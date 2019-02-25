aa9df7fd8ab56d1ad6f11ccf5274af3096285c33
The Magician <magic-modules@google.com>
2019-02-25 13:46:08 -0800

    Add managed SSL certs to beta. (#458)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider_compute_gen.go
+++ google-beta/provider_compute_gen.go
@@ -35,6 +35,7 @@ var GeneratedComputeResourcesMap = map[string]*schema.Resource{
 	"google_compute_router":                  resourceComputeRouter(),
 	"google_compute_snapshot":                resourceComputeSnapshot(),
 	"google_compute_ssl_certificate":         resourceComputeSslCertificate(),
+	"google_compute_managed_ssl_certificate": resourceComputeManagedSslCertificate(),
 	"google_compute_ssl_policy":              resourceComputeSslPolicy(),
 	"google_compute_subnetwork":              resourceComputeSubnetwork(),
 	"google_compute_target_http_proxy":       resourceComputeTargetHttpProxy(),
