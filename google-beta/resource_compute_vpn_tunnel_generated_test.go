312d4151daa1ed4ab7e3d64d925e7a5fa0613e30
emily <emilyye@google.com>
2019-03-20 00:38:32 +0000

    Add post-create for VpnTunnels Labels
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_vpn_tunnel_generated_test.go
+++ google-beta/resource_compute_vpn_tunnel_generated_test.go
@@ -112,6 +112,105 @@ resource "google_compute_route" "route1" {
 `, context)
 }
 
+func TestAccComputeVpnTunnel_vpnTunnelBetaExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeVpnTunnelDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeVpnTunnel_vpnTunnelBetaExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeVpnTunnel_vpnTunnelBetaExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_vpn_tunnel" "tunnel1" {
+  provider = "google-beta"
+  name          = "tunnel1-%{random_suffix}"
+  peer_ip       = "15.0.0.120"
+  shared_secret = "a secret message"
+
+  target_vpn_gateway = "${google_compute_vpn_gateway.target_gateway.self_link}"
+
+  depends_on = [
+    "google_compute_forwarding_rule.fr_esp",
+    "google_compute_forwarding_rule.fr_udp500",
+    "google_compute_forwarding_rule.fr_udp4500",
+  ]
+
+  labels {
+    foo = "bar"
+  }
+}
+
+resource "google_compute_vpn_gateway" "target_gateway" {
+  provider = "google-beta"
+  name    = "vpn1-%{random_suffix}"
+  network = "${google_compute_network.network1.self_link}"
+}
+
+resource "google_compute_network" "network1" {
+  provider = "google-beta"
+  name       = "network1-%{random_suffix}"
+}
+
+resource "google_compute_address" "vpn_static_ip" {
+  provider = "google-beta"
+  name   = "vpn-static-ip-%{random_suffix}"
+}
+
+resource "google_compute_forwarding_rule" "fr_esp" {
+  provider = "google-beta"
+  name        = "fr-esp-%{random_suffix}"
+  ip_protocol = "ESP"
+  ip_address  = "${google_compute_address.vpn_static_ip.address}"
+  target      = "${google_compute_vpn_gateway.target_gateway.self_link}"
+}
+
+resource "google_compute_forwarding_rule" "fr_udp500" {
+  provider = "google-beta"
+  name        = "fr-udp500-%{random_suffix}"
+  ip_protocol = "UDP"
+  port_range  = "500"
+  ip_address  = "${google_compute_address.vpn_static_ip.address}"
+  target      = "${google_compute_vpn_gateway.target_gateway.self_link}"
+}
+
+resource "google_compute_forwarding_rule" "fr_udp4500" {
+  provider = "google-beta"
+  name        = "fr-udp4500-%{random_suffix}"
+  ip_protocol = "UDP"
+  port_range  = "4500"
+  ip_address  = "${google_compute_address.vpn_static_ip.address}"
+  target      = "${google_compute_vpn_gateway.target_gateway.self_link}"
+}
+
+resource "google_compute_route" "route1" {
+  provider = "google-beta"
+  name       = "route1-%{random_suffix}"
+  network    = "${google_compute_network.network1.name}"
+  dest_range = "15.0.0.0/24"
+  priority   = 1000
+
+  next_hop_vpn_tunnel = "${google_compute_vpn_tunnel.tunnel1.self_link}"
+}
+
+provider "google-beta"{
+  region = "us-central1"
+  zone   = "us-central1-a"
+}
+`, context)
+}
+
 func testAccCheckComputeVpnTunnelDestroy(s *terraform.State) error {
 	for name, rs := range s.RootModule().Resources {
 		if rs.Type != "google_compute_vpn_tunnel" {
