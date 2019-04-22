30ade2b26cac1de2cdd6c538e037511b989dfc93
Sam Levenick <slevenick@google.com>
2019-04-22 22:59:15 +0000

    Add logConfig to subnetwork resource
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_subnetwork_generated_test.go
+++ google-beta/resource_compute_subnetwork_generated_test.go
@@ -68,6 +68,55 @@ resource "google_compute_network" "custom-test" {
 `, context)
 }
 
+func TestAccComputeSubnetwork_subnetworkLoggingConfigBetaExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeSubnetworkDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeSubnetwork_subnetworkLoggingConfigBetaExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeSubnetwork_subnetworkLoggingConfigBetaExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_subnetwork" "subnet-with-logging" {
+  provider      = "google-beta" 
+  name          = "log-test-subnetwork-%{random_suffix}"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = "${google_compute_network.custom-test.self_link}"
+
+  enable_flow_logs = true
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling = 0.5
+    metadata = "INCLUDE_ALL_METADATA"
+  }
+}
+
+resource "google_compute_network" "custom-test" {
+  provider                = "google-beta"
+  name                    = "log-test-network-%{random_suffix}"
+  auto_create_subnetworks = false
+}
+
+provider "google-beta"{
+  region = "us-central1"
+  zone   = "us-central1-a"
+}
+`, context)
+}
+
 func testAccCheckComputeSubnetworkDestroy(s *terraform.State) error {
 	for name, rs := range s.RootModule().Resources {
 		if rs.Type != "google_compute_subnetwork" {
