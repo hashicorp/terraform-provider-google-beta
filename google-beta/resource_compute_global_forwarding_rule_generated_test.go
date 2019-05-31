ff0356274d274fd5efcc8f8dc09cf8184f1d7fd7
Sam Levenick <slevenick@google.com>
2019-05-31 20:13:11 +0000

    Add support for INTERNAL_SELF_MANAGED backend service
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_global_forwarding_rule_generated_test.go
+++ google-beta/resource_compute_global_forwarding_rule_generated_test.go
@@ -101,6 +101,130 @@ resource "google_compute_http_health_check" "default" {
 `, context)
 }
 
+func TestAccComputeGlobalForwardingRule_globalForwardingRuleInternalExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeGlobalForwardingRuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeGlobalForwardingRule_globalForwardingRuleInternalExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeGlobalForwardingRule_globalForwardingRuleInternalExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_global_forwarding_rule" "default" {
+  provider              = "google-beta"
+  name                  = "global-rule-%{random_suffix}"
+  target                = "${google_compute_target_http_proxy.default.self_link}"
+  port_range            = "80"
+  load_balancing_scheme = "INTERNAL_SELF_MANAGED"
+  ip_address            = "0.0.0.0"
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  provider    = "google-beta"
+  name        = "target-proxy-%{random_suffix}"
+  description = "a description"
+  url_map     = "${google_compute_url_map.default.self_link}"
+}
+
+resource "google_compute_url_map" "default" {
+  provider         = "google-beta"
+  name            = "url-map-target-proxy-%{random_suffix}"
+  description     = "a description"
+  default_service = "${google_compute_backend_service.default.self_link}"
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = "${google_compute_backend_service.default.self_link}"
+
+    path_rule {
+      paths   = ["/*"]
+      service = "${google_compute_backend_service.default.self_link}"
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  provider              = "google-beta"
+  name                  = "backend-%{random_suffix}"
+  port_name             = "http"
+  protocol              = "HTTP"
+  timeout_sec           = 10
+  load_balancing_scheme = "INTERNAL_SELF_MANAGED"
+
+  backend {
+    group = "${google_compute_instance_group_manager.foobar.instance_group}"
+    balancing_mode = "RATE"
+    capacity_scaler = 0.4
+    max_rate_per_instance = 50
+  }
+
+  health_checks = ["${google_compute_health_check.default.self_link}"]
+}
+
+data "google_compute_image" "debian_image" {
+  provider = "google-beta"
+  family   = "debian-9"
+  project  = "debian-cloud"
+}
+
+resource "google_compute_instance_group_manager" "foobar" {
+  provider           = "google-beta"
+  name               = "igm-internal"
+  version {
+    instance_template  = "${google_compute_instance_template.foobar.self_link}"
+    name               = "primary"
+  }
+  base_instance_name = "foobar"
+  zone               = "us-central1-f"
+  target_size        = 1
+}
+
+resource "google_compute_instance_template" "foobar" {
+  provider     = "google-beta"
+  name         = "instance-template-internal"
+  machine_type = "n1-standard-1"
+
+  network_interface {
+    network = "default"
+  }
+
+  disk {
+    source_image = "${data.google_compute_image.debian_image.self_link}"
+    auto_delete  = true
+    boot         = true
+  }
+}
+
+resource "google_compute_health_check" "default" {
+  provider           = "google-beta"
+  name               = "check-backend-%{random_suffix}"
+  check_interval_sec = 1
+  timeout_sec        = 1
+
+  tcp_health_check {
+    port = "80"
+  }
+}
+`, context)
+}
+
 func testAccCheckComputeGlobalForwardingRuleDestroy(s *terraform.State) error {
 	for name, rs := range s.RootModule().Resources {
 		if rs.Type != "google_compute_global_forwarding_rule" {
