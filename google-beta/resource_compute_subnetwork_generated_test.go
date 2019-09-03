ce8003191cbaf187cadb2d4694cab94061bfd4b7
The Magician <magic-modules@google.com>
2019-09-03 15:01:04 -0700

    Add purpose and role to subnetwork for L7 load balancing (#1051)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_subnetwork_generated_test.go
+++ google-beta/resource_compute_subnetwork_generated_test.go
@@ -117,6 +117,50 @@ provider "google-beta"{
 `, context)
 }
 
+func TestAccComputeSubnetwork_subnetworkInternalL7lbExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeSubnetworkDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeSubnetwork_subnetworkInternalL7lbExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeSubnetwork_subnetworkInternalL7lbExample(context map[string]interface{}) string {
+	return Nprintf(`
+provider "google-beta" {
+  region = "us-central1"
+  zone   = "us-central1-a"
+}
+
+resource "google_compute_subnetwork" "network-for-l7lb" {
+  provider      = "google-beta"
+  name          = "l7lb-test-subnetwork%{random_suffix}"
+  ip_cidr_range = "10.0.0.0/22"
+  region        = "us-central1"
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
+  network       = "${google_compute_network.custom-test.self_link}"
+}
+
+resource "google_compute_network" "custom-test" {
+  provider      = "google-beta"
+  name                    = "l7lb-test-network%{random_suffix}"
+  auto_create_subnetworks = false
+}
+`, context)
+}
+
 func testAccCheckComputeSubnetworkDestroy(s *terraform.State) error {
 	for name, rs := range s.RootModule().Resources {
 		if rs.Type != "google_compute_subnetwork" {
