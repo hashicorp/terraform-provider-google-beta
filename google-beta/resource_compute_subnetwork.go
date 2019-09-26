6bb47792c0ee42f8b3266746fb4515b43e7913bc
Nathan McKinley <nmckinley@google.com>
2019-09-25 22:47:18 -0700

    Upgrade to terraform-plugin-sdk.

--- google-beta/resource_compute_subnetwork.go
+++ google-beta/resource_compute_subnetwork.go
@@ -23,9 +23,9 @@ import (
 	"time"
 
 	"github.com/apparentlymart/go-cidr/cidr"
-	"github.com/hashicorp/terraform/helper/customdiff"
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/customdiff"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"google.golang.org/api/compute/v1"
 )
 
