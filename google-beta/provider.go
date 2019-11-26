cfe3b7ae63eca36bf6ede4803a0d5816bd3d09a2
The Magician <magic-modules@google.com>
2019-11-26 15:05:25 -0800

    Auto-generate BgpPeer (#1396)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -489,9 +489,9 @@ func Provider() terraform.ResourceProvider {
 	return provider
 }
 
-// Generated resources: 101
+// Generated resources: 102
 // Generated IAM resources: 39
-// Total generated resources: 140
+// Total generated resources: 141
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -557,6 +557,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_route":                             resourceComputeRoute(),
 			"google_compute_router":                            resourceComputeRouter(),
 			"google_compute_router_nat":                        resourceComputeRouterNat(),
+			"google_compute_router_peer":                       resourceComputeRouterBgpPeer(),
 			"google_compute_snapshot":                          resourceComputeSnapshot(),
 			"google_compute_ssl_certificate":                   resourceComputeSslCertificate(),
 			"google_compute_managed_ssl_certificate":           resourceComputeManagedSslCertificate(),
@@ -668,7 +669,6 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_compute_project_metadata_item":         resourceComputeProjectMetadataItem(),
 			"google_compute_region_instance_group_manager": resourceComputeRegionInstanceGroupManager(),
 			"google_compute_router_interface":              resourceComputeRouterInterface(),
-			"google_compute_router_peer":                   resourceComputeRouterPeer(),
 			"google_compute_security_policy":               resourceComputeSecurityPolicy(),
 			"google_compute_shared_vpc_host_project":       resourceComputeSharedVpcHostProject(),
 			"google_compute_shared_vpc_service_project":    resourceComputeSharedVpcServiceProject(),
