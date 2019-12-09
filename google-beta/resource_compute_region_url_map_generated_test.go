a5a15b8257433dd4197d475a936c80e2a3f05896
The Magician <magic-modules@google.com>
2019-12-09 15:18:12 -0800

    Add path_rules to RegionUrlMap (#1489)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_region_url_map_generated_test.go
+++ google-beta/resource_compute_region_url_map_generated_test.go
@@ -122,6 +122,230 @@ resource "google_compute_region_health_check" "default" {
 `, context)
 }
 
+func TestAccComputeRegionUrlMap_regionUrlMapL7IlbPathExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeRegionUrlMapDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRegionUrlMap_regionUrlMapL7IlbPathExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeRegionUrlMap_regionUrlMapL7IlbPathExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_region_url_map" "regionurlmap" {
+  provider = "google-beta"
+  name        = "regionurlmap%{random_suffix}"
+  description = "a description"
+  default_service = google_compute_region_backend_service.home.self_link
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_region_backend_service.home.self_link
+
+    path_rule {
+      paths   = ["/home"]
+      route_action {
+        cors_policy {
+          allow_credentials = true
+          allow_headers = ["Allowed content"]
+          allow_methods = ["GET"]
+          allow_origins = ["Allowed origin"]
+          expose_headers = ["Exposed header"]
+          max_age = 30
+          disabled = false
+        }
+        fault_injection_policy {
+          abort {
+            http_status = 234
+            percentage = 5.6
+          }
+          delay {
+            fixed_delay {
+              seconds = 0
+              nanos = 50000
+            }
+            percentage = 7.8
+          }
+        }
+        request_mirror_policy {
+          backend_service = google_compute_region_backend_service.home.self_link
+        }
+        retry_policy {
+          num_retries = 4
+          per_try_timeout {
+            seconds = 30
+          }
+          retry_conditions = ["5xx", "deadline-exceeded"]
+        }
+        timeout {
+          seconds = 20
+          nanos = 750000000
+        }
+        url_rewrite {
+          host_rewrite = "A replacement header"
+          path_prefix_rewrite = "A replacement path"
+        }
+        weighted_backend_services {
+          backend_service = google_compute_region_backend_service.home.self_link
+          weight = 400
+          header_action {
+            request_headers_to_remove = ["RemoveMe"]
+            request_headers_to_add {
+              header_name = "AddMe"
+              header_value = "MyValue"
+              replace = true
+            }
+            response_headers_to_remove = ["RemoveMe"]
+            response_headers_to_add {
+              header_name = "AddMe"
+              header_value = "MyValue"
+              replace = false
+            }
+          }
+        }
+      }
+    }
+  }
+
+  test {
+    service = google_compute_region_backend_service.home.self_link
+    host    = "hi.com"
+    path    = "/home"
+  }
+}
+
+resource "google_compute_region_backend_service" "home" {
+  provider = "google-beta"
+  name        = "home%{random_suffix}"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_region_health_check.default.self_link]
+  load_balancing_scheme = "INTERNAL_MANAGED"
+}
+
+resource "google_compute_region_health_check" "default" {
+  provider = "google-beta"
+  name               = "health-check%{random_suffix}"
+  http_health_check {
+    port = 80
+  }
+}
+`, context)
+}
+
+func TestAccComputeRegionUrlMap_regionUrlMapL7IlbPathPartialExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeRegionUrlMapDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRegionUrlMap_regionUrlMapL7IlbPathPartialExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeRegionUrlMap_regionUrlMapL7IlbPathPartialExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_region_url_map" "regionurlmap" {
+  provider = "google-beta"
+  name        = "regionurlmap%{random_suffix}"
+  description = "a description"
+  default_service = google_compute_region_backend_service.home.self_link
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_region_backend_service.home.self_link
+
+    path_rule {
+      paths   = ["/home"]
+      route_action {
+        retry_policy {
+          num_retries = 4
+          per_try_timeout {
+            seconds = 30
+          }
+          retry_conditions = ["5xx", "deadline-exceeded"]
+        }
+        timeout {
+          seconds = 20
+          nanos = 750000000
+        }
+        url_rewrite {
+          host_rewrite = "A replacement header"
+          path_prefix_rewrite = "A replacement path"
+        }
+        weighted_backend_services {
+          backend_service = google_compute_region_backend_service.home.self_link
+          weight = 400
+          header_action {
+            response_headers_to_add {
+              header_name = "AddMe"
+              header_value = "MyValue"
+              replace = false
+            }
+          }
+        }
+      }
+    }
+  }
+
+  test {
+    service = google_compute_region_backend_service.home.self_link
+    host    = "hi.com"
+    path    = "/home"
+  }
+}
+
+resource "google_compute_region_backend_service" "home" {
+  provider = "google-beta"
+  name        = "home%{random_suffix}"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_region_health_check.default.self_link]
+  load_balancing_scheme = "INTERNAL_MANAGED"
+}
+
+resource "google_compute_region_health_check" "default" {
+  provider = "google-beta"
+  name               = "health-check%{random_suffix}"
+  http_health_check {
+    port = 80
+  }
+}
+`, context)
+}
+
 func testAccCheckComputeRegionUrlMapDestroy(s *terraform.State) error {
 	for name, rs := range s.RootModule().Resources {
 		if rs.Type != "google_compute_region_url_map" {
