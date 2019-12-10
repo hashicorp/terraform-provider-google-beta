2bc67e8605683e1ed66e89b9daced13c0a383632
Ty Larrabee <tysen@google.com>
2019-12-10 01:14:36 +0000

    Add route_rules to RegionUrlMap
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_region_url_map_generated_test.go
+++ google-beta/resource_compute_region_url_map_generated_test.go
@@ -346,6 +346,199 @@ resource "google_compute_region_health_check" "default" {
 `, context)
 }
 
+func TestAccComputeRegionUrlMap_regionUrlMapL7IlbRouteExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeRegionUrlMapDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRegionUrlMap_regionUrlMapL7IlbRouteExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeRegionUrlMap_regionUrlMapL7IlbRouteExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_region_url_map" "regionurlmap" {
+  provider        = "google-beta"
+  name            = "regionurlmap%{random_suffix}"
+  description     = "a description"
+  default_service = google_compute_region_backend_service.home.self_link
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_region_backend_service.home.self_link
+
+    route_rules {
+      priority = 1
+      header_action {
+        request_headers_to_remove = ["RemoveMe2"]
+        request_headers_to_add {
+          header_name = "AddSomethingElse"
+          header_value = "MyOtherValue"
+          replace = true
+        }
+        response_headers_to_remove = ["RemoveMe3"]
+        response_headers_to_add {
+          header_name = "AddMe"
+          header_value = "MyValue"
+          replace = false
+        }
+      }
+      match_rules {
+        full_path_match = "a full path"
+        header_matches {
+          header_name = "someheader"
+          exact_match = "match this exactly"
+          invert_match = true
+        }
+        ignore_case = true
+        metadata_filters {
+          filter_match_criteria = "MATCH_ANY"
+          filter_labels {
+            name = "PLANET"
+            value = "MARS"
+          }
+        }
+        query_parameter_matches {
+          name = "a query parameter"
+          present_match = true
+        }
+      }
+      url_redirect {
+        host_redirect = "A host"
+        https_redirect = false
+        path_redirect = "some/path"
+        redirect_response_code = "TEMPORARY_REDIRECT"
+        strip_query = true
+      }
+    }
+  }
+
+  test {
+    service = google_compute_region_backend_service.home.self_link
+    host    = "hi.com"
+    path    = "/home"
+  }
+}
+
+resource "google_compute_region_backend_service" "home" {
+  provider    = "google-beta"
+  name        = "home%{random_suffix}"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_region_health_check.default.self_link]
+  load_balancing_scheme = "INTERNAL_MANAGED"
+}
+
+resource "google_compute_region_health_check" "default" {
+  provider = "google-beta"
+  name     = "health-check%{random_suffix}"
+  http_health_check {
+    port = 80
+  }
+}
+`, context)
+}
+
+func TestAccComputeRegionUrlMap_regionUrlMapL7IlbRoutePartialExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeRegionUrlMapDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRegionUrlMap_regionUrlMapL7IlbRoutePartialExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeRegionUrlMap_regionUrlMapL7IlbRoutePartialExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_region_url_map" "regionurlmap" {
+  provider = "google-beta"
+  name        = "regionurlmap%{random_suffix}"
+  description = "a description"
+  default_service = google_compute_region_backend_service.home.self_link
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name = "allpaths"
+    default_service = google_compute_region_backend_service.home.self_link
+
+    route_rules {
+      priority = 1
+      service = google_compute_region_backend_service.home.self_link
+      header_action {
+        request_headers_to_remove = ["RemoveMe2"]
+      }
+      match_rules {
+        full_path_match = "a full path"
+        header_matches {
+          header_name = "someheader"
+          exact_match = "match this exactly"
+          invert_match = true
+        }
+        query_parameter_matches {
+          name = "a query parameter"
+          present_match = true
+        }
+      }
+    }
+  }
+
+  test {
+    service = google_compute_region_backend_service.home.self_link
+    host    = "hi.com"
+    path    = "/home"
+  }
+}
+
+resource "google_compute_region_backend_service" "home" {
+  provider = "google-beta"
+  name        = "home%{random_suffix}"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_region_health_check.default.self_link]
+  load_balancing_scheme = "INTERNAL_MANAGED"
+}
+
+resource "google_compute_region_health_check" "default" {
+  provider = "google-beta"
+  name               = "health-check%{random_suffix}"
+  http_health_check {
+    port = 80
+  }
+}
+`, context)
+}
+
 func testAccCheckComputeRegionUrlMapDestroy(s *terraform.State) error {
 	for name, rs := range s.RootModule().Resources {
 		if rs.Type != "google_compute_region_url_map" {
