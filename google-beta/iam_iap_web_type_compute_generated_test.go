419be019bf438316222975e2c456e70f8ccd814a
Dana Hoffman <danahoffman@google.com>
2019-12-17 22:04:09 +0000

    add IAM conditions support to the remaining IAP resources
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/iam_iap_web_type_compute_generated_test.go
+++ google-beta/iam_iap_web_type_compute_generated_test.go
@@ -29,6 +29,9 @@ func TestAccIapWebTypeComputeIamBindingGenerated(t *testing.T) {
 		"random_suffix": acctest.RandString(10),
 		"role":          "roles/iap.httpsResourceAccessor",
 		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
 	}
 
 	resource.Test(t, resource.TestCase{
@@ -65,6 +68,9 @@ func TestAccIapWebTypeComputeIamMemberGenerated(t *testing.T) {
 		"random_suffix": acctest.RandString(10),
 		"role":          "roles/iap.httpsResourceAccessor",
 		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
 	}
 
 	resource.Test(t, resource.TestCase{
@@ -92,6 +98,9 @@ func TestAccIapWebTypeComputeIamPolicyGenerated(t *testing.T) {
 		"random_suffix": acctest.RandString(10),
 		"role":          "roles/iap.httpsResourceAccessor",
 		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
 	}
 
 	resource.Test(t, resource.TestCase{
@@ -100,6 +109,163 @@ func TestAccIapWebTypeComputeIamPolicyGenerated(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccIapWebTypeComputeIamPolicy_basicGenerated(context),
+			},
+			{
+				ResourceName:      "google_iap_web_type_compute_iam_policy.foo",
+				ImportStateId:     fmt.Sprintf("projects/%s/iap_web/compute", fmt.Sprintf("tf-test%s", context["random_suffix"])),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccIapWebTypeComputeIamBindingGenerated_withCondition(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+		"role":          "roles/iap.httpsResourceAccessor",
+		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIapWebTypeComputeIamBinding_withConditionGenerated(context),
+			},
+			{
+				ResourceName:      "google_iap_web_type_compute_iam_binding.foo",
+				ImportStateId:     fmt.Sprintf("projects/%s/iap_web/compute roles/iap.httpsResourceAccessor %s", fmt.Sprintf("tf-test%s", context["random_suffix"]), context["condition_title"]),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccIapWebTypeComputeIamBindingGenerated_withAndWithoutCondition(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+		"role":          "roles/iap.httpsResourceAccessor",
+		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIapWebTypeComputeIamBinding_withAndWithoutConditionGenerated(context),
+			},
+			{
+				ResourceName:      "google_iap_web_type_compute_iam_binding.foo",
+				ImportStateId:     fmt.Sprintf("projects/%s/iap_web/compute roles/iap.httpsResourceAccessor", fmt.Sprintf("tf-test%s", context["random_suffix"])),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				ResourceName:      "google_iap_web_type_compute_iam_binding.foo2",
+				ImportStateId:     fmt.Sprintf("projects/%s/iap_web/compute roles/iap.httpsResourceAccessor %s", fmt.Sprintf("tf-test%s", context["random_suffix"]), context["condition_title"]),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccIapWebTypeComputeIamMemberGenerated_withCondition(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+		"role":          "roles/iap.httpsResourceAccessor",
+		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIapWebTypeComputeIamMember_withConditionGenerated(context),
+			},
+			{
+				ResourceName:      "google_iap_web_type_compute_iam_member.foo",
+				ImportStateId:     fmt.Sprintf("projects/%s/iap_web/compute roles/iap.httpsResourceAccessor user:admin@hashicorptest.com %s", fmt.Sprintf("tf-test%s", context["random_suffix"]), context["condition_title"]),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccIapWebTypeComputeIamMemberGenerated_withAndWithoutCondition(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+		"role":          "roles/iap.httpsResourceAccessor",
+		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIapWebTypeComputeIamMember_withAndWithoutConditionGenerated(context),
+			},
+			{
+				ResourceName:      "google_iap_web_type_compute_iam_member.foo",
+				ImportStateId:     fmt.Sprintf("projects/%s/iap_web/compute roles/iap.httpsResourceAccessor user:admin@hashicorptest.com", fmt.Sprintf("tf-test%s", context["random_suffix"])),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				ResourceName:      "google_iap_web_type_compute_iam_member.foo2",
+				ImportStateId:     fmt.Sprintf("projects/%s/iap_web/compute roles/iap.httpsResourceAccessor user:admin@hashicorptest.com %s", fmt.Sprintf("tf-test%s", context["random_suffix"]), context["condition_title"]),
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccIapWebTypeComputeIamPolicyGenerated_withCondition(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+		"role":          "roles/iap.httpsResourceAccessor",
+		"org_id":        getTestOrgFromEnv(t),
+
+		"condition_title": "expires_after_2019_12_31",
+		"condition_expr":  `request.time < timestamp(\"2020-01-01T00:00:00Z\")`,
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIapWebTypeComputeIamPolicy_withConditionGenerated(context),
 			},
 			{
 				ResourceName:      "google_iap_web_type_compute_iam_policy.foo",
@@ -197,6 +363,154 @@ resource "google_iap_web_type_compute_iam_binding" "foo" {
   project = "${google_project_service.project_service.project}"
   role = "%{role}"
   members = ["user:admin@hashicorptest.com", "user:paddy@hashicorp.com"]
+}
+`, context)
+}
+
+func testAccIapWebTypeComputeIamBinding_withConditionGenerated(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_project" "project" {
+  project_id = "tf-test%{random_suffix}"
+  name       = "tf-test%{random_suffix}"
+  org_id     = "%{org_id}"
+}
+
+resource "google_project_service" "project_service" {
+  project = google_project.project.project_id
+  service = "iap.googleapis.com"
+}
+
+resource "google_iap_web_type_compute_iam_binding" "foo" {
+  project = "${google_project_service.project_service.project}"
+  role = "%{role}"
+  members = ["user:admin@hashicorptest.com"]
+  condition {
+    title       = "%{condition_title}"
+    description = "Expiring at midnight of 2019-12-31"
+    expression  = "%{condition_expr}"
+  }
+}
+`, context)
+}
+
+func testAccIapWebTypeComputeIamBinding_withAndWithoutConditionGenerated(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_project" "project" {
+  project_id = "tf-test%{random_suffix}"
+  name       = "tf-test%{random_suffix}"
+  org_id     = "%{org_id}"
+}
+
+resource "google_project_service" "project_service" {
+  project = google_project.project.project_id
+  service = "iap.googleapis.com"
+}
+
+resource "google_iap_web_type_compute_iam_binding" "foo" {
+  project = "${google_project_service.project_service.project}"
+  role = "%{role}"
+  members = ["user:admin@hashicorptest.com"]
+}
+
+resource "google_iap_web_type_compute_iam_binding" "foo2" {
+  project = "${google_project_service.project_service.project}"
+  role = "%{role}"
+  members = ["user:admin@hashicorptest.com"]
+  condition {
+    title       = "%{condition_title}"
+    description = "Expiring at midnight of 2019-12-31"
+    expression  = "%{condition_expr}"
+  }
+}
+`, context)
+}
+
+func testAccIapWebTypeComputeIamMember_withConditionGenerated(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_project" "project" {
+  project_id = "tf-test%{random_suffix}"
+  name       = "tf-test%{random_suffix}"
+  org_id     = "%{org_id}"
+}
+
+resource "google_project_service" "project_service" {
+  project = google_project.project.project_id
+  service = "iap.googleapis.com"
+}
+
+resource "google_iap_web_type_compute_iam_member" "foo" {
+  project = "${google_project_service.project_service.project}"
+  role = "%{role}"
+  member = "user:admin@hashicorptest.com"
+  condition {
+    title       = "%{condition_title}"
+    description = "Expiring at midnight of 2019-12-31"
+    expression  = "%{condition_expr}"
+  }
+}
+`, context)
+}
+
+func testAccIapWebTypeComputeIamMember_withAndWithoutConditionGenerated(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_project" "project" {
+  project_id = "tf-test%{random_suffix}"
+  name       = "tf-test%{random_suffix}"
+  org_id     = "%{org_id}"
+}
+
+resource "google_project_service" "project_service" {
+  project = google_project.project.project_id
+  service = "iap.googleapis.com"
+}
+
+resource "google_iap_web_type_compute_iam_member" "foo" {
+  project = "${google_project_service.project_service.project}"
+  role = "%{role}"
+  member = "user:admin@hashicorptest.com"
+}
+
+resource "google_iap_web_type_compute_iam_member" "foo2" {
+  project = "${google_project_service.project_service.project}"
+  role = "%{role}"
+  member = "user:admin@hashicorptest.com"
+  condition {
+    title       = "%{condition_title}"
+    description = "Expiring at midnight of 2019-12-31"
+    expression  = "%{condition_expr}"
+  }
+}
+`, context)
+}
+
+func testAccIapWebTypeComputeIamPolicy_withConditionGenerated(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_project" "project" {
+  project_id = "tf-test%{random_suffix}"
+  name       = "tf-test%{random_suffix}"
+  org_id     = "%{org_id}"
+}
+
+resource "google_project_service" "project_service" {
+  project = google_project.project.project_id
+  service = "iap.googleapis.com"
+}
+
+data "google_iam_policy" "foo" {
+  binding {
+    role = "%{role}"
+    members = ["user:admin@hashicorptest.com"]
+    condition {
+      title       = "%{condition_title}"
+      description = "Expiring at midnight of 2019-12-31"
+      expression  = "%{condition_expr}"
+    }
+  }
+}
+
+resource "google_iap_web_type_compute_iam_policy" "foo" {
+  project = "${google_project_service.project_service.project}"
+  policy_data = "${data.google_iam_policy.foo.policy_data}"
 }
 `, context)
 }
