2241e3771902f51cb579778f2adebb7f9a7a3199
emily <emilyye@google.com>
2020-01-05 23:17:27 +0000

    Add bootstrapped test networks for service networking tests
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_sql_database_instance_test.go
+++ google-beta/resource_sql_database_instance_test.go
@@ -623,8 +623,8 @@ func TestAccSqlDatabaseInstance_withPrivateNetwork(t *testing.T) {
 	t.Parallel()
 
 	databaseName := "tf-test-" + acctest.RandString(10)
-	networkName := "tf-test-" + acctest.RandString(10)
 	addressName := "tf-test-" + acctest.RandString(10)
+	networkName := BootstrapSharedServiceNetworkingConsumerNetwork(t, "sql-instance-private")
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -779,9 +779,8 @@ resource "google_sql_database_instance" "instance-failover" {
 
 func testAccSqlDatabaseInstance_withPrivateNetwork(databaseName, networkName, addressRangeName string) string {
 	return fmt.Sprintf(`
-resource "google_compute_network" "foobar" {
+data "google_compute_network" "servicenet" {
   name                    = "%s"
-  auto_create_subnetworks = false
 }
 
 resource "google_compute_global_address" "foobar" {
@@ -789,11 +788,11 @@ resource "google_compute_global_address" "foobar" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = google_compute_network.foobar.self_link
+  network       = data.google_compute_network.servicenet.self_link
 }
 
 resource "google_service_networking_connection" "foobar" {
-  network                 = google_compute_network.foobar.self_link
+  network                 = data.google_compute_network.servicenet.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.foobar.name]
 }
@@ -806,7 +805,7 @@ resource "google_sql_database_instance" "instance" {
     tier = "db-f1-micro"
     ip_configuration {
       ipv4_enabled    = "false"
-      private_network = google_compute_network.foobar.self_link
+      private_network = data.google_compute_network.servicenet.self_link
     }
   }
 }
