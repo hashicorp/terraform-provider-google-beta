26404d2e5810a27c1ba766d97927934147035af8
The Magician <magic-modules@google.com>
2020-03-09 16:10:09 -0700

    Add node group autoscaling policy (#3230) (#1841)
    * Add node group autoscaling policy
    
    * add default_from_api
    
    * Update api.yaml
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_node_group_generated_test.go
+++ google-beta/resource_compute_node_group_generated_test.go
@@ -71,6 +71,56 @@ resource "google_compute_node_group" "nodes" {
 `, context)
 }
 
+func TestAccComputeNodeGroup_nodeGroupAutoscalingPolicyExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(10),
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeNodeGroupDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeNodeGroup_nodeGroupAutoscalingPolicyExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeNodeGroup_nodeGroupAutoscalingPolicyExample(context map[string]interface{}) string {
+	return Nprintf(`
+data "google_compute_node_types" "central1a" {
+  provider = google-beta
+  zone = "us-central1-a"
+}
+
+resource "google_compute_node_template" "soletenant-tmpl" {
+  provider = google-beta
+  name      = "tf-test-soletenant-tmpl%{random_suffix}"
+  region    = "us-central1"
+  node_type = data.google_compute_node_types.central1a.names[0]
+}
+
+resource "google_compute_node_group" "nodes" {
+  provider = google-beta
+  name        = "tf-test-soletenant-group%{random_suffix}"
+  zone        = "us-central1-a"
+  description = "example google_compute_node_group for Terraform Google Provider"
+
+  size          = 1
+  node_template = google_compute_node_template.soletenant-tmpl.self_link
+  autoscaling_policy {
+    mode = "ON"
+    min_nodes = 1
+    max_nodes = 10
+  }
+}
+`, context)
+}
+
 func testAccCheckComputeNodeGroupDestroy(s *terraform.State) error {
 	for name, rs := range s.RootModule().Resources {
 		if rs.Type != "google_compute_node_group" {
