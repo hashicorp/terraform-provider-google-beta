b0d10f8c957cddd82466b555af9c98c1325ddd5c
The Magician <magic-modules@google.com>
2020-03-11 10:38:01 -0700

    add app engine flex version (#3110) (#1849)
    * added flexible app version
    
    * add sweeper file for flexible_app_version
    
    * sweeper changes, add website link, etc
    
    * add exclude to inspec.yaml
    
    * add exclude to ansible.yaml
    
    * missed
    
    * sweeper review comments
    
    * add test, other changes
    
    * update test to flask, ignore read on beta_settings for now
    
    * add copyright to app.yaml
    
    * rm django zip
    
    * update after review comments
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -569,9 +569,9 @@ func Provider() terraform.ResourceProvider {
 	return provider
 }
 
-// Generated resources: 131
+// Generated resources: 132
 // Generated IAM resources: 54
-// Total generated resources: 185
+// Total generated resources: 186
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -587,6 +587,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_app_engine_domain_mapping":                             resourceAppEngineDomainMapping(),
 			"google_app_engine_firewall_rule":                              resourceAppEngineFirewallRule(),
 			"google_app_engine_standard_app_version":                       resourceAppEngineStandardAppVersion(),
+			"google_app_engine_flexible_app_version":                       resourceAppEngineFlexibleAppVersion(),
 			"google_app_engine_application_url_dispatch_rules":             resourceAppEngineApplicationUrlDispatchRules(),
 			"google_app_engine_service_split_traffic":                      resourceAppEngineServiceSplitTraffic(),
 			"google_bigquery_dataset":                                      resourceBigQueryDataset(),
