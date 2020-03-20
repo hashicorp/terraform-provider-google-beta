5d15ffc57158cc5daa6d8bd133d710958ac9f46e
The Magician <magic-modules@google.com>
2020-03-20 16:48:26 -0700

    Allow import of logging_metric in a non-default project. (#3270) (#1876)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_security_scanner_scan_config.go
+++ google-beta/resource_security_scanner_scan_config.go
@@ -502,7 +502,7 @@ func resourceSecurityScannerScanConfigImport(d *schema.ResourceData, meta interf
 	config := meta.(*Config)
 
 	// current import_formats can't import fields with forward slashes in their value
-	if err := parseImportId([]string{"(?P<name>.+)"}, d, config); err != nil {
+	if err := parseImportId([]string{"(?P<project>[^ ]+) (?P<name>[^ ]+)", "(?P<name>[^ ]+)"}, d, config); err != nil {
 		return nil, err
 	}
 
