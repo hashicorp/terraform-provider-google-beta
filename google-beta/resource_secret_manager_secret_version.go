cb568bf8d4c2b43d9bf1a3e8729e4bdc05e6344c
The Magician <magic-modules@google.com>
2020-03-25 09:00:21 -0700

    Catch googleapi errors from flattenprop (#3292) (#1888)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_secret_manager_secret_version.go
+++ google-beta/resource_secret_manager_secret_version.go
@@ -24,6 +24,7 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"google.golang.org/api/googleapi"
 )
 
 func resourceSecretManagerSecretVersionUpdate(d *schema.ResourceData, meta interface{}) error {
@@ -180,6 +181,9 @@ func resourceSecretManagerSecretVersionRead(d *schema.ResourceData, meta interfa
 	// Terraform must set the top level schema field, but since this object contains collapsed properties
 	// it's difficult to know what the top level should be. Instead we just loop over the map returned from flatten.
 	if flattenedProp := flattenSecretManagerSecretVersionPayload(res["payload"], d, config); flattenedProp != nil {
+		if gerr, ok := flattenedProp.(*googleapi.Error); ok {
+			return fmt.Errorf("Error reading SecretVersion: %s", gerr)
+		}
 		casted := flattenedProp.([]interface{})[0]
 		if casted != nil {
 			for k, v := range casted.(map[string]interface{}) {
