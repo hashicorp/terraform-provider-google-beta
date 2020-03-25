84ff47bd76e03c72864ebb0207d41a220854e5fa
Modular Magician <magic-modules@google.com>
2020-03-25 16:00:07 +0000

    Catch googleapi errors from flattenprop (#3292)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_billing_budget.go
+++ google-beta/resource_billing_budget.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"google.golang.org/api/googleapi"
 )
 
 func resourceBillingBudget() *schema.Resource {
@@ -273,6 +274,9 @@ func resourceBillingBudgetRead(d *schema.ResourceData, meta interface{}) error {
 	// Terraform must set the top level schema field, but since this object contains collapsed properties
 	// it's difficult to know what the top level should be. Instead we just loop over the map returned from flatten.
 	if flattenedProp := flattenBillingBudgetBudget(res["budget"], d, config); flattenedProp != nil {
+		if gerr, ok := flattenedProp.(*googleapi.Error); ok {
+			return fmt.Errorf("Error reading Budget: %s", gerr)
+		}
 		casted := flattenedProp.([]interface{})[0]
 		if casted != nil {
 			for k, v := range casted.(map[string]interface{}) {
