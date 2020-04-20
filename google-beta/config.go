71c70005cc1b77318cbf9bd3ae15b3fa59c5ee22
Modular Magician <magic-modules@google.com>
2020-04-20 19:28:46 +0000

    Service Directory resources (#3371)
    * service directory resources
    
    * * custom imports
    * interpolation from name to id
    * update tests
    
    * validate functions & import formats
    
    * fix CheckDestroys
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/config.go
+++ google-beta/config.go
@@ -118,6 +118,7 @@ type Config struct {
 	SecretManagerBasePath        string
 	SecurityCenterBasePath       string
 	SecurityScannerBasePath      string
+	ServiceDirectoryBasePath     string
 	ServiceManagementBasePath    string
 	ServiceUsageBasePath         string
 	SourceRepoBasePath           string
@@ -263,6 +264,7 @@ var RuntimeConfigDefaultBasePath = "https://runtimeconfig.googleapis.com/v1beta1
 var SecretManagerDefaultBasePath = "https://secretmanager.googleapis.com/v1beta1/"
 var SecurityCenterDefaultBasePath = "https://securitycenter.googleapis.com/v1/"
 var SecurityScannerDefaultBasePath = "https://websecurityscanner.googleapis.com/v1beta/"
+var ServiceDirectoryDefaultBasePath = "https://servicedirectory.googleapis.com/v1beta1/"
 var ServiceManagementDefaultBasePath = "https://servicemanagement.googleapis.com/v1/"
 var ServiceUsageDefaultBasePath = "https://serviceusage.googleapis.com/v1beta1/"
 var SourceRepoDefaultBasePath = "https://sourcerepo.googleapis.com/v1/"
@@ -782,6 +784,7 @@ func ConfigureBasePaths(c *Config) {
 	c.SecretManagerBasePath = SecretManagerDefaultBasePath
 	c.SecurityCenterBasePath = SecurityCenterDefaultBasePath
 	c.SecurityScannerBasePath = SecurityScannerDefaultBasePath
+	c.ServiceDirectoryBasePath = ServiceDirectoryDefaultBasePath
 	c.ServiceManagementBasePath = ServiceManagementDefaultBasePath
 	c.ServiceUsageBasePath = ServiceUsageDefaultBasePath
 	c.SourceRepoBasePath = SourceRepoDefaultBasePath
