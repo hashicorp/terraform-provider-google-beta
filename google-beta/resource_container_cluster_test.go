c8a02658717cdfab2cdd7cd5486881d5391c6c5a
The Magician <magic-modules@google.com>
2020-08-05 16:56:54 -0700

    Remove test that tests legacy behavior which has been removed. (#3829) (#2341)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_container_cluster_test.go
+++ google-beta/resource_container_cluster_test.go
@@ -2031,9 +2031,6 @@ resource "google_container_cluster" "primary" {
 
   enable_legacy_abac      = true
 
-  logging_service    = "logging.googleapis.com"
-  monitoring_service = "monitoring.googleapis.com"
-
   resource_labels = {
     created-by = "terraform"
   }
@@ -2063,9 +2060,6 @@ resource "google_container_cluster" "primary" {
   ]
 
   enable_legacy_abac      = false
-
-  logging_service    = "none"
-  monitoring_service = "none"
 
   resource_labels = {
     created-by = "terraform-update"
