1fc78dbc7781036a0a9bec4fc12de86fa759dbda
Modular Magician <magic-modules@google.com>
2020-08-19 23:10:51 +0000

    Fix test resource names to match example (#3889)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_region_health_check_generated_test.go
+++ google-beta/resource_compute_region_health_check_generated_test.go
@@ -517,7 +517,7 @@ func TestAccComputeRegionHealthCheck_regionHealthCheckGrpcExample(t *testing.T) 
 
 func testAccComputeRegionHealthCheck_regionHealthCheckGrpcExample(context map[string]interface{}) string {
 	return Nprintf(`
-resource "google_compute_region_health_check" "grpc-health-check" {
+resource "google_compute_region_health_check" "grpc-region-health-check" {
   name = "tf-test-grpc-region-health-check%{random_suffix}"
 
   timeout_sec        = 1
@@ -556,7 +556,7 @@ func TestAccComputeRegionHealthCheck_regionHealthCheckGrpcFullExample(t *testing
 
 func testAccComputeRegionHealthCheck_regionHealthCheckGrpcFullExample(context map[string]interface{}) string {
 	return Nprintf(`
-resource "google_compute_region_health_check" "grpc-health-check" {
+resource "google_compute_region_health_check" "grpc-region-health-check" {
   name = "tf-test-grpc-region-health-check%{random_suffix}"
 
   timeout_sec        = 1
