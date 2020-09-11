b0dd62c183f9a101c5ef8fafbd66ae96701c06f2
The Magician <magic-modules@google.com>
2020-09-11 14:28:40 -0700

    Add Regional SSL Certificate datasource (#3969) (#2476)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -662,6 +662,7 @@ func Provider() terraform.ResourceProvider {
 			"google_compute_node_types":                           dataSourceGoogleComputeNodeTypes(),
 			"google_compute_regions":                              dataSourceGoogleComputeRegions(),
 			"google_compute_region_instance_group":                dataSourceGoogleComputeRegionInstanceGroup(),
+			"google_compute_region_ssl_certificate":               dataSourceGoogleRegionComputeSslCertificate(),
 			"google_compute_resource_policy":                      dataSourceGoogleComputeResourcePolicy(),
 			"google_compute_router":                               dataSourceGoogleComputeRouter(),
 			"google_compute_ssl_certificate":                      dataSourceGoogleComputeSslCertificate(),
