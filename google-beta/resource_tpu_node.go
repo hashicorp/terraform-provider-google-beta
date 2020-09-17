edc0ab880883577470e6620a7f8cdd7545a5f7e3
The Magician <magic-modules@google.com>
2020-09-17 15:00:44 -0700

    Enforce v2 sdk in all resources and other generated files. (#3989) (#2501)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_tpu_node.go
+++ google-beta/resource_tpu_node.go
@@ -23,7 +23,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // compareTpuNodeSchedulingConfig diff suppresses for the default
