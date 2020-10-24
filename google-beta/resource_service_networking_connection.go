961b35893bd4fede4b529cf1db57e1c63a4dbd5a
Nick Ma <nickma@google.com>
2020-10-24 03:04:08 +0000

    refactor: service_networking shared utils

--- google-beta/resource_service_networking_connection.go
+++ google-beta/resource_service_networking_connection.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/url"
-	"regexp"
 	"strings"
 	"time"
 
@@ -305,48 +304,4 @@ func parseConnectionId(id string) (*connectionId, error) {
 		Network: network,
 		Service: service,
 	}, nil
-}
-
-// NOTE(craigatgoogle): An out of band aspect of this API is that it uses a unique formatting of network
-// different from the standard self_link URI. It requires a call to the resource manager to get the project
-// number for the current project.
-func retrieveServiceNetworkingNetworkName(d *schema.ResourceData, config *Config, network, userAgent string) (string, error) {
-	networkFieldValue, err := ParseNetworkFieldValue(network, d, config)
-	if err != nil {
-		return "", errwrap.Wrapf("Failed to retrieve network field value, err: {{err}}", err)
-	}
-
-	pid := networkFieldValue.Project
-	if pid == "" {
-		return "", fmt.Errorf("Could not determine project")
-	}
-	log.Printf("[DEBUG] Retrieving project number by doing a GET with the project id, as required by service networking")
-	project, err := config.NewResourceManagerClient(userAgent).Projects.Get(pid).Do()
-	if err != nil {
-		// note: returning a wrapped error is part of this method's contract!
-		// https://blog.golang.org/go1.13-errors
-		return "", fmt.Errorf("Failed to retrieve project, pid: %s, err: %w", pid, err)
-	}
-
-	networkName := networkFieldValue.Name
-	if networkName == "" {
-		return "", fmt.Errorf("Failed to parse network")
-	}
-
-	// return the network name formatting unique to this API
-	return fmt.Sprintf("projects/%v/global/networks/%v", project.ProjectNumber, networkName), nil
-
-}
-
-const parentServicePattern = "^services/.+$"
-
-// NOTE(craigatgoogle): An out of band aspect of this API is that it requires the service name to be
-// formatted as "services/<serviceName>"
-func formatParentService(service string) string {
-	r := regexp.MustCompile(parentServicePattern)
-	if !r.MatchString(service) {
-		return fmt.Sprintf("services/%s", service)
-	} else {
-		return service
-	}
 }
--- google-beta/service_networking_operation.go
+++ google-beta/service_networking_operation.go
@@ -1,6 +1,7 @@
 package google
 
 import (
+	"encoding/json"
 	"time"
 
 	"google.golang.org/api/servicenetworking/v1"
@@ -14,6 +15,21 @@ type ServiceNetworkingOperationWaiter struct {
 func (w *ServiceNetworkingOperationWaiter) QueryOp() (interface{}, error) {
 	return w.Service.Operations.Get(w.Op.Name).Do()
 }
+
+func serviceNetworkOperationWaitTimeWithResponse(config *Config, op *servicenetworking.Operation, response *servicenetworking.Subnetwork, activity, userAgent string, timeout time.Duration) error {
+	w := &ServiceNetworkingOperationWaiter{
+		Service: config.NewServiceNetworkingClient(userAgent),
+	}
+
+	if err := w.SetOp(op); err != nil {
+		return err
+	}
+	if err := OperationWait(w, activity, timeout, config.PollInterval); err != nil {
+		return err
+	}
+	return json.Unmarshal([]byte(w.CommonOperationWaiter.Op.Response), response)
+}
+
 
 func serviceNetworkingOperationWaitTime(config *Config, op *servicenetworking.Operation, activity, userAgent string, timeout time.Duration) error {
 	w := &ServiceNetworkingOperationWaiter{
