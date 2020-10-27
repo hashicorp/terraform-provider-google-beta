afc703bad5d4fa5a658193eede027897036300b1
The Magician <magic-modules@google.com>
2020-10-27 14:56:37 -0700

    Add skip_initial_version_creation to kms crypto key (#3907) (#2645)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_firebase_web_app.go
+++ google-beta/resource_firebase_web_app.go
@@ -251,8 +251,8 @@ func resourceFirebaseWebAppUpdate(d *schema.ResourceData, meta interface{}) erro
 
 func resourceFirebaseWebAppDelete(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[WARNING] Firebase WebApp resources"+
-		" cannot be deleted from GCP. The resource %s will be removed from Terraform"+
-		" state, but will still be present on the server.", d.Id())
+		" cannot be deleted from Google Cloud. The resource %s will be removed from Terraform"+
+		" state, but will still be present on Google Cloud.", d.Id())
 	d.SetId("")
 
 	return nil
