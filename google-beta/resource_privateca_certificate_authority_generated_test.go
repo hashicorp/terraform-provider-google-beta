4b7dc8d6c77864f0be9b6c333baca6d298585d2a
The Magician <magic-modules@google.com>
2021-01-19 14:26:08 -0800

    Misc improvements for Certificate Authority (#4407) (#2883)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_privateca_certificate_authority_generated_test.go
+++ google-beta/resource_privateca_certificate_authority_generated_test.go
@@ -68,6 +68,7 @@ resource "google_privateca_certificate_authority" "default" {
   key_spec {
     algorithm = "RSA_PKCS1_4096_SHA256"
   }
+  disable_on_delete = true
 }
 `, context)
 }
@@ -132,6 +133,78 @@ resource "google_privateca_certificate_authority" "default" {
   key_spec {
     algorithm = "EC_P256_SHA256"
   }
+  disable_on_delete = true
+}
+`, context)
+}
+
+func TestAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityCmekExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"kms_key_name":  BootstrapKMSKeyWithPurposeInLocation(t, "ASYMMETRIC_SIGN", "us-central1").CryptoKey.Name,
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProvidersOiCS,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"random": {},
+		},
+		CheckDestroy: testAccCheckPrivatecaCertificateAuthorityDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityCmekExample(context),
+			},
+		},
+	})
+}
+
+func testAccPrivatecaCertificateAuthority_privatecaCertificateAuthorityCmekExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_project_service_identity" "privateca_sa" {
+  provider = google-beta
+  service  = "privateca.googleapis.com"
+}
+
+resource "google_kms_crypto_key_iam_binding" "privateca_sa_keyuser" {
+  provider      = google-beta
+  crypto_key_id = "%{kms_key_name}"
+  role          = "roles/cloudkms.signerVerifier"
+
+  members = [
+    "serviceAccount:${google_project_service_identity.privateca_sa.email}",
+  ]
+}
+
+resource "google_privateca_certificate_authority" "default" {
+  provider                 = google-beta
+  certificate_authority_id = "tf-test%{random_suffix}"
+  location                 = "us-central1"
+
+  key_spec {
+    cloud_kms_key_version = "%{kms_key_name}/cryptoKeyVersions/1"
+  }
+
+  config  {
+    subject_config  {
+      common_name = "Example Authority"
+      subject {
+        organization = "Example, Org."
+      }
+    }
+
+    reusable_config {
+      reusable_config= "root-unconstrained"
+    }
+  }
+
+  depends_on = [
+    google_kms_crypto_key_iam_binding.privateca_sa_keyuser,
+  ]
+
+  disable_on_delete = true
 }
 `, context)
 }
