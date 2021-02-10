c78c3127922b0b60e29f4c8db179e9f7280fce8a
The Magician <magic-modules@google.com>
2021-02-10 10:26:05 -0800

    secretmanager: point beta provider to v1 of the api (#4479) (#2946)
    The v1 api is a superset of v1beta1. New features are added to v1 and not v1beta1.
    
    See https://github.com/hashicorp/terraform-provider-google/issues/8415
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_secret_manager_secret_sweeper_test.go
+++ google-beta/resource_secret_manager_secret_sweeper_test.go
@@ -61,7 +61,7 @@ func testSweepSecretManagerSecret(region string) error {
 		},
 	}
 
-	listTemplate := strings.Split("https://secretmanager.googleapis.com/v1beta1/projects/{{project}}/secrets", "?")[0]
+	listTemplate := strings.Split("https://secretmanager.googleapis.com/v1/projects/{{project}}/secrets", "?")[0]
 	listUrl, err := replaceVars(d, config, listTemplate)
 	if err != nil {
 		log.Printf("[INFO][SWEEPER_LOG] error preparing sweeper list url: %s", err)
@@ -103,7 +103,7 @@ func testSweepSecretManagerSecret(region string) error {
 			continue
 		}
 
-		deleteTemplate := "https://secretmanager.googleapis.com/v1beta1/projects/{{project}}/secrets/{{secret_id}}"
+		deleteTemplate := "https://secretmanager.googleapis.com/v1/projects/{{project}}/secrets/{{secret_id}}"
 		deleteUrl, err := replaceVars(d, config, deleteTemplate)
 		if err != nil {
 			log.Printf("[INFO][SWEEPER_LOG] error preparing delete url: %s", err)
