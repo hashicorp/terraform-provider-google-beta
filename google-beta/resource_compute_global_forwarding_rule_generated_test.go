39a28c02119b5f503045a5be49b1d36ed69e0fc3
The Magician <magic-modules@google.com>
2021-02-12 09:04:11 -0800

    Added support for Private Services Connect for Google APIs (#4358) (#2956)
    * updated to mmv1 folder
    
    * replaced data source with resources in tests
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_global_forwarding_rule_generated_test.go
+++ google-beta/resource_compute_global_forwarding_rule_generated_test.go
@@ -238,6 +238,56 @@ resource "google_compute_health_check" "default" {
 `, context)
 }
 
+func TestAccComputeGlobalForwardingRule_globalForwardingRulePrivateServicesConnectExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProvidersOiCS,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"random": {},
+		},
+		CheckDestroy: testAccCheckComputeGlobalForwardingRuleDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeGlobalForwardingRule_globalForwardingRulePrivateServicesConnectExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeGlobalForwardingRule_globalForwardingRulePrivateServicesConnectExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_global_address" "default" {
+  provider      = google-beta
+  name          = "tf-test-global-psconnect-ip%{random_suffix}"
+  address_type  = "INTERNAL"
+  purpose       = "PRIVATE_SERVICE_CONNECT"
+  network       = google_compute_network.network.id
+  address       = "100.100.100.106"
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  provider      = google-beta
+  name          = "globalrule%{random_suffix}"
+  target        = "all-apis"
+  network       = google_compute_network.network.id
+  ip_address    = google_compute_global_address.default.id
+  load_balancing_scheme = ""
+}
+
+resource "google_compute_network" "network" {
+  provider      = google-beta
+  name          = "tf-test%{random_suffix}"
+  auto_create_subnetworks = false
+}
+`, context)
+}
+
 func testAccCheckComputeGlobalForwardingRuleDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
