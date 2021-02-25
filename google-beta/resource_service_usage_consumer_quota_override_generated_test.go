2416f2bf680176b3d0ce98d145dd0c54f2bf095d
Modular Magician <magic-modules@google.com>
2021-02-25 17:36:54 +0000

    Add Service Usage Regional Example (#4507)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_service_usage_consumer_quota_override_generated_test.go
+++ google-beta/resource_service_usage_consumer_quota_override_generated_test.go
@@ -67,6 +67,54 @@ resource "google_service_usage_consumer_quota_override" "override" {
 `, context)
 }
 
+func TestAccServiceUsageConsumerQuotaOverride_regionConsumerQuotaOverrideExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id":        getTestOrgFromEnv(t),
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProvidersOiCS,
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"random": {},
+		},
+		CheckDestroy: testAccCheckServiceUsageConsumerQuotaOverrideDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccServiceUsageConsumerQuotaOverride_regionConsumerQuotaOverrideExample(context),
+			},
+		},
+	})
+}
+
+func testAccServiceUsageConsumerQuotaOverride_regionConsumerQuotaOverrideExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_project" "my_project" {
+  provider   = google-beta
+  name       = "tf-test-project"
+  project_id = "quota%{random_suffix}"
+  org_id     = "%{org_id}"
+  auto_create_network = false
+}
+
+resource "google_service_usage_consumer_quota_override" "override" {
+  provider       = google-beta
+  dimensions = {
+    region = "us-central1"
+  }
+  project        = google_project.my_project.project_id
+  service        = "compute.googleapis.com"
+  metric         = "compute.googleapis.com%2Fn2_cpus"
+  limit          = "%2Fproject%2Fregion"
+  override_value = "10"
+  force          = true
+}
+`, context)
+}
+
 func testAccCheckServiceUsageConsumerQuotaOverrideDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
