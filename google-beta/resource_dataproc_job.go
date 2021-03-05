3d3992066188de0987674f7413d19391e6fd83e5
The Magician <magic-modules@google.com>
2021-03-05 12:31:30 -0800

    Ga clients (#4560) (#3030)
    * Composer to ga client
    
    * Dataproc, remove unused filestore client
    
    * Move resourcemanager v2beta1 to v2
    
    * Move security policy, vpc host, vpc project to v1 client
    
    * Revert composer, updating certain fields is not supported in GA
    
    * Remove unused DNS beta endpoint
    
    * Use single dataproc client
    
    * Fix test import
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_dataproc_job.go
+++ google-beta/resource_dataproc_job.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"google.golang.org/api/dataproc/v1"
+	dataproc "google.golang.org/api/dataproc/v1beta2"
 )
 
 func resourceDataprocJob() *schema.Resource {
