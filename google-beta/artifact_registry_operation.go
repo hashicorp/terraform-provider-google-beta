6b5a6bf552a4773dd6267fc88bfa71a41f93d25f
The Magician <magic-modules@google.com>
2021-03-18 20:12:06 -0700

    Allow other repo types for AR (#4604) (#3068)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: upodroid <cy@borg.dev>

--- google-beta/artifact_registry_operation.go
+++ google-beta/artifact_registry_operation.go
@@ -31,7 +31,7 @@ func (w *ArtifactRegistryOperationWaiter) QueryOp() (interface{}, error) {
 		return nil, fmt.Errorf("Cannot query operation, it's unset or nil.")
 	}
 	// Returns the proper get.
-	url := fmt.Sprintf("https://artifactregistry.googleapis.com/v1beta1/%s", w.CommonOperationWaiter.Op.Name)
+	url := fmt.Sprintf("https://artifactregistry.googleapis.com/v1beta2/%s", w.CommonOperationWaiter.Op.Name)
 
 	return sendRequest(w.Config, "GET", w.Project, url, w.UserAgent, nil)
 }
--- google-beta/resource_artifact_registry_repository_sweeper_test.go
+++ google-beta/resource_artifact_registry_repository_sweeper_test.go
@@ -61,7 +61,7 @@ func testSweepArtifactRegistryRepository(region string) error {
 		},
 	}
 
-	listTemplate := strings.Split("https://artifactregistry.googleapis.com/v1beta1/projects/{{project}}/locations/{{location}}/repositories", "?")[0]
+	listTemplate := strings.Split("https://artifactregistry.googleapis.com/v1beta2/projects/{{project}}/locations/{{location}}/repositories", "?")[0]
 	listUrl, err := replaceVars(d, config, listTemplate)
 	if err != nil {
 		log.Printf("[INFO][SWEEPER_LOG] error preparing sweeper list url: %s", err)
@@ -103,7 +103,7 @@ func testSweepArtifactRegistryRepository(region string) error {
 			continue
 		}
 
-		deleteTemplate := "https://artifactregistry.googleapis.com/v1beta1/projects/{{project}}/locations/{{location}}/repositories/{{repository_id}}"
+		deleteTemplate := "https://artifactregistry.googleapis.com/v1beta2/projects/{{project}}/locations/{{location}}/repositories/{{repository_id}}"
 		deleteUrl, err := replaceVars(d, config, deleteTemplate)
 		if err != nil {
 			log.Printf("[INFO][SWEEPER_LOG] error preparing delete url: %s", err)
