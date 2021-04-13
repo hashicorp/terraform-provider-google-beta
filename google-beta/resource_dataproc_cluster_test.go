fda7a3ce21a7459be2ea6de667b3c28136bdae5d
Modular Magician <magic-modules@google.com>
2021-04-13 00:02:20 +0000

    Dataproc, container cluster, backend bucket test fixes (#4679)
    * ANACONDA is removed now
    
    * Remove telemetry pinned version, default cdn_cache fields in backen bucket
    
    * Ordering
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_dataproc_cluster_test.go
+++ google-beta/resource_dataproc_cluster_test.go
@@ -565,7 +565,7 @@ func TestAccDataprocCluster_withOptionalComponents(t *testing.T) {
 				Config: testAccDataprocCluster_withOptionalComponents(rnd),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckDataprocClusterExists(t, "google_dataproc_cluster.with_opt_components", &cluster),
-					testAccCheckDataprocClusterHasOptionalComponents(&cluster, "ANACONDA", "ZOOKEEPER"),
+					testAccCheckDataprocClusterHasOptionalComponents(&cluster, "ZOOKEEPER", "DOCKER"),
 				),
 			},
 		},
@@ -1429,7 +1429,7 @@ resource "google_dataproc_cluster" "with_opt_components" {
 
   cluster_config {
     software_config {
-      optional_components = ["ANACONDA", "ZOOKEEPER"]
+      optional_components = ["DOCKER", "ZOOKEEPER"]
     }
   }
 }
