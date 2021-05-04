caac3c5bf99e87b73d0eaaa191b165f1c208b041
The Magician <magic-modules@google.com>
2021-05-04 10:00:06 -0700

    Added support for the integration with Secret Manager in Cloud Run. (#4739) (#3225)
    * Added support for the integration with Secret Manager in Cloud Run Managed.
    The integration enables binding secret values to environment variables or
    to the content of files.
    
    * Revert "Added support for the integration with Secret Manager in Cloud Run Managed."
    
    This reverts commit 2170b507ee540108a8f08641e4997c9d449f2e28.
    
    * Added support for the integration with Secret Manager in Cloud Run Managed.
    
    The integration enables binding secret values to environment variables or
    to the content of files.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_cloud_run_service_generated_test.go
+++ google-beta/resource_cloud_run_service_generated_test.go
@@ -262,6 +262,207 @@ resource "google_cloud_run_service" "default" {
 `, context)
 }
 
+func TestAccCloudRunService_cloudRunServiceSecretEnvironmentVariablesExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project":       getTestProjectFromEnv(),
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckCloudRunServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_cloudRunServiceSecretEnvironmentVariablesExample(context),
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_cloudRunServiceSecretEnvironmentVariablesExample(context map[string]interface{}) string {
+	return Nprintf(`
+data "google_project" "project" {
+  provider = google-beta
+}
+
+resource "google_secret_manager_secret" "secret" {
+  provider = google-beta
+
+  secret_id = "secret%{random_suffix}"
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-data" {
+  provider = google-beta
+
+  secret = google_secret_manager_secret.secret.name
+  secret_data = "secret-data"
+}
+
+resource "google_secret_manager_secret_iam_member" "secret-access" {
+  provider = google-beta
+
+  secret_id = google_secret_manager_secret.secret.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  depends_on = [google_secret_manager_secret.secret]
+}
+
+resource "google_cloud_run_service" "default" {
+  provider = google-beta
+
+  name     = "tf-test-cloudrun-srv%{random_suffix}"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+        env {
+          name = "SECRET_ENV_VAR"
+	  value_from {
+            secret_key_ref {
+              name = google_secret_manager_secret.secret.secret_id
+              key = "1"
+            }
+          }
+        }
+      }
+    }
+  }
+
+  metadata {
+    annotations = {
+      generated-by = "magic-modules"
+      "run.googleapis.com/launch-stage" = "ALPHA"
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+  autogenerate_revision_name = true
+
+  lifecycle {
+    ignore_changes = [
+        metadata.0.annotations,
+    ]
+  }
+
+  depends_on = [google_secret_manager_secret_version.secret-version-data]
+}
+`, context)
+}
+
+func TestAccCloudRunService_cloudRunServiceSecretVolumesExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"project":       getTestProjectFromEnv(),
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckCloudRunServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_cloudRunServiceSecretVolumesExample(context),
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_cloudRunServiceSecretVolumesExample(context map[string]interface{}) string {
+	return Nprintf(`
+data "google_project" "project" {
+  provider = google-beta
+}
+
+resource "google_secret_manager_secret" "secret" {
+  provider = google-beta
+
+  secret_id = "secret%{random_suffix}"
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret-version-data" {
+  provider = google-beta
+
+  secret = google_secret_manager_secret.secret.name
+  secret_data = "secret-data"
+}
+
+resource "google_secret_manager_secret_iam_member" "secret-access" {
+  provider = google-beta
+
+  secret_id = google_secret_manager_secret.secret.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  depends_on = [google_secret_manager_secret.secret]
+}
+
+resource "google_cloud_run_service" "default" {
+  provider = google-beta
+
+  name     = "tf-test-cloudrun-srv%{random_suffix}"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+	volume_mounts {
+	  name = "a-volume"
+	  mount_path = "/secrets"
+	}
+      }
+      volumes {
+        name = "a-volume"
+	secret {
+	  secret_name = google_secret_manager_secret.secret.secret_id
+	  items {
+            key = "1"
+	    path = "my-secret"
+	  }
+	}
+      }
+    }
+  }
+
+  metadata {
+    annotations = {
+      generated-by = "magic-modules"
+      "run.googleapis.com/launch-stage" = "ALPHA"
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+  autogenerate_revision_name = true
+
+  lifecycle {
+    ignore_changes = [
+        metadata.0.annotations,
+    ]
+  }
+
+  depends_on = [google_secret_manager_secret_version.secret-version-data]
+}
+`, context)
+}
+
 func testAccCheckCloudRunServiceDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
