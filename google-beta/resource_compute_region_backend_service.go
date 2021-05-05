9c6da13954252518dddd01660ae9fdcbf4e90699
The Magician <magic-modules@google.com>
2021-05-05 14:09:15 -0700

    Marked BackendService cdn_policy serve_while_stale as default_from_api (#4763) (#3230)
    * Marked serve_while_stale as default_from_api
    
    * Added default_from_api for cdn_policy.*_ttl fields for backend services.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_region_backend_service.go
+++ google-beta/resource_compute_region_backend_service.go
@@ -248,17 +248,20 @@ The possible values are: USE_ORIGIN_HEADERS, FORCE_CACHE_ALL and CACHE_ALL_STATI
 						},
 						"client_ttl": {
 							Type:        schema.TypeInt,
+							Computed:    true,
 							Optional:    true,
 							Description: `Specifies the maximum allowed TTL for cached content served by this origin.`,
 						},
 						"default_ttl": {
 							Type:     schema.TypeInt,
+							Computed: true,
 							Optional: true,
 							Description: `Specifies the default TTL for cached content served by this origin for responses
 that do not have an existing valid TTL (max-age or s-max-age).`,
 						},
 						"max_ttl": {
 							Type:        schema.TypeInt,
+							Computed:    true,
 							Optional:    true,
 							Description: `Specifies the maximum allowed TTL for cached content served by this origin.`,
 						},
@@ -292,6 +295,7 @@ can be specified as values, and you cannot specify a status code more than once.
 						},
 						"serve_while_stale": {
 							Type:        schema.TypeInt,
+							Computed:    true,
 							Optional:    true,
 							Description: `Serve existing content from the cache (if available) when revalidating content with the origin, or when an error is encountered when refreshing the cache.`,
 						},
