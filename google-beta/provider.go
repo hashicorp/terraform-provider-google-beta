7a407d0f33f1b54a9290d2d71194b30012e357c3
Modular Magician <magic-modules@google.com>
2021-05-27 17:49:34 +0000

    DialogFlow fulfillment support (#4808)
    * DialogFlow fulfillment support
    
    * Apply Code Review suggestions
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -839,9 +839,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 222
+// Generated resources: 223
 // Generated IAM resources: 117
-// Total generated resources: 339
+// Total generated resources: 340
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1041,6 +1041,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_dialogflow_agent":                                      resourceDialogflowAgent(),
 			"google_dialogflow_intent":                                     resourceDialogflowIntent(),
 			"google_dialogflow_entity_type":                                resourceDialogflowEntityType(),
+			"google_dialogflow_fulfillment":                                resourceDialogflowFulfillment(),
 			"google_dns_managed_zone":                                      resourceDNSManagedZone(),
 			"google_dns_policy":                                            resourceDNSPolicy(),
 			"google_dns_record_set":                                        resourceDNSResourceDnsRecordSet(),
