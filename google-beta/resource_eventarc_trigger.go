a2fbac1e58a18f467cf36fb1e829c4e77f03d679
Modular Magician <magic-modules@google.com>
2021-06-04 18:29:27 +0000

    Add support for user-project-overrides to tpgtools (#4837)
    * add support for user-project overrides
    
    * de-indent
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_eventarc_trigger.go
+++ google-beta/resource_eventarc_trigger.go
@@ -247,7 +247,12 @@ func resourceEventarcTriggerCreate(d *schema.ResourceData, meta interface{}) err
 	if err != nil {
 		return err
 	}
-	client := CreateEventarcClient(config, userAgent, "")
+	billingProject := project
+	// err == nil indicates that the billing_project value was found
+	if bp, err := getBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+	client := CreateEventarcClient(config, userAgent, billingProject)
 	res, err := client.ApplyTrigger(context.Background(), obj, createDirective...)
 	if err != nil {
 		// The resource didn't actually create
@@ -282,7 +287,12 @@ func resourceEventarcTriggerRead(d *schema.ResourceData, meta interface{}) error
 	if err != nil {
 		return err
 	}
-	client := CreateEventarcClient(config, userAgent, "")
+	billingProject := project
+	// err == nil indicates that the billing_project value was found
+	if bp, err := getBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+	client := CreateEventarcClient(config, userAgent, billingProject)
 	res, err := client.GetTrigger(context.Background(), obj)
 	if err != nil {
 		// Resource not found
@@ -348,7 +358,12 @@ func resourceEventarcTriggerUpdate(d *schema.ResourceData, meta interface{}) err
 	if err != nil {
 		return err
 	}
-	client := CreateEventarcClient(config, userAgent, "")
+	billingProject := project
+	// err == nil indicates that the billing_project value was found
+	if bp, err := getBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+	client := CreateEventarcClient(config, userAgent, billingProject)
 	res, err := client.ApplyTrigger(context.Background(), obj, directive...)
 	if err != nil {
 		return fmt.Errorf("Error updating Trigger: %s", err)
@@ -382,7 +397,12 @@ func resourceEventarcTriggerDelete(d *schema.ResourceData, meta interface{}) err
 	if err != nil {
 		return err
 	}
-	client := CreateEventarcClient(config, userAgent, "")
+	billingProject := project
+	// err == nil indicates that the billing_project value was found
+	if bp, err := getBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+	client := CreateEventarcClient(config, userAgent, billingProject)
 	if err := client.DeleteTrigger(context.Background(), obj); err != nil {
 		return fmt.Errorf("Error deleting Trigger: %s", err)
 	}
