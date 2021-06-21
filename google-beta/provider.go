5c1b1c0bc9a86e3bfb57ff6bedd654609901237a
Modular Magician <magic-modules@google.com>
2021-06-21 18:50:23 +0000

    Add Vertex AI Dataset (#4863)
    * initial Dataset
    
    Co-authored-by: upodroid <cy@borg.dev>
    
    * pass d to OperationWaiters
    
    * fetch region from self_link
    
    * fix regex and add test
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -707,6 +707,14 @@ func Provider() *schema.Provider {
 					"GOOGLE_TPU_CUSTOM_ENDPOINT",
 				}, DefaultBasePaths[TPUBasePathKey]),
 			},
+			"vertex_ai_custom_endpoint": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validateCustomEndpoint,
+				DefaultFunc: schema.MultiEnvDefaultFunc([]string{
+					"GOOGLE_VERTEX_AI_CUSTOM_ENDPOINT",
+				}, DefaultBasePaths[VertexAIBasePathKey]),
+			},
 			"vpc_access_custom_endpoint": {
 				Type:         schema.TypeString,
 				Optional:     true,
@@ -860,9 +868,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 225
+// Generated resources: 226
 // Generated IAM resources: 117
-// Total generated resources: 342
+// Total generated resources: 343
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1211,6 +1219,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_tags_tag_value_iam_policy":                             ResourceIamPolicy(TagsTagValueIamSchema, TagsTagValueIamUpdaterProducer, TagsTagValueIdParseFunc),
 			"google_tags_tag_binding":                                      resourceTagsTagBinding(),
 			"google_tpu_node":                                              resourceTPUNode(),
+			"google_vertex_ai_dataset":                                     resourceVertexAIDataset(),
 			"google_vpc_access_connector":                                  resourceVPCAccessConnector(),
 			"google_workflows_workflow":                                    resourceWorkflowsWorkflow(),
 		},
@@ -1496,6 +1505,7 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData, p *schema.Pr
 	config.StorageBasePath = d.Get("storage_custom_endpoint").(string)
 	config.TagsBasePath = d.Get("tags_custom_endpoint").(string)
 	config.TPUBasePath = d.Get("tpu_custom_endpoint").(string)
+	config.VertexAIBasePath = d.Get("vertex_ai_custom_endpoint").(string)
 	config.VPCAccessBasePath = d.Get("vpc_access_custom_endpoint").(string)
 	config.WorkflowsBasePath = d.Get("workflows_custom_endpoint").(string)
 
