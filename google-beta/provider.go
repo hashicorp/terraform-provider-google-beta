4e4fe5f949437cd880035ce196b1582e14facd8b
The Magician <magic-modules@google.com>
2021-07-09 11:03:46 -0500

    Add Vertex AI Featurestores (#4906) (#3416)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: upodroid <cy@borg.dev>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -870,9 +870,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 229
+// Generated resources: 231
 // Generated IAM resources: 117
-// Total generated resources: 346
+// Total generated resources: 348
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1225,6 +1225,8 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_tags_tag_binding":                                      resourceTagsTagBinding(),
 			"google_tpu_node":                                              resourceTPUNode(),
 			"google_vertex_ai_dataset":                                     resourceVertexAIDataset(),
+			"google_vertex_ai_featurestore":                                resourceVertexAIFeaturestore(),
+			"google_vertex_ai_featurestore_entitytype":                     resourceVertexAIFeaturestoreEntitytype(),
 			"google_vpc_access_connector":                                  resourceVPCAccessConnector(),
 			"google_workflows_workflow":                                    resourceWorkflowsWorkflow(),
 		},
