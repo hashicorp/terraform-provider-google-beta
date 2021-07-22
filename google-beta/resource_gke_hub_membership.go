99dff8c18b51b6fce4a22c20a902d55278e6b48e
Modular Magician <magic-modules@google.com>
2021-07-22 16:39:54 +0000

    Promote `google_gke_hub_membership` to GA (#4982)
    * initial ga work
    
    * promote the right resource
    
    * remove description from ga
    
    * deprecate description
    
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_gke_hub_membership.go
+++ google-beta/resource_gke_hub_membership.go
@@ -69,7 +69,8 @@ with length <2000 characters. For example: 'https://container.googleapis.com/v1/
 			"description": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				Description: `The name of this entity type to be displayed on the console.`,
+				Deprecated:  "This field is unavailable in the GA provider and will be removed from the beta provider in a future release.",
+				Description: `The name of this entity type to be displayed on the console. This field is unavailable in v1 of the API.`,
 			},
 			"endpoint": {
 				Type:        schema.TypeList,
