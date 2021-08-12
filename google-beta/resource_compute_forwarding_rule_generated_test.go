06fe7259d8e0b2a7ec7e88edd593fe661e9e4482
The Magician <magic-modules@google.com>
2021-08-12 17:13:21 -0500

    L3 default support for Network Load Balancer (#5059) (#3516)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_forwarding_rule_generated_test.go
+++ google-beta/resource_compute_forwarding_rule_generated_test.go
@@ -176,6 +176,56 @@ resource "google_compute_target_pool" "default" {
 `, context)
 }
 
+func TestAccComputeForwardingRule_forwardingRuleL3DefaultExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeForwardingRuleDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeForwardingRule_forwardingRuleL3DefaultExample(context),
+			},
+		},
+	})
+}
+
+func testAccComputeForwardingRule_forwardingRuleL3DefaultExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_forwarding_rule" "fwd_rule" {
+  provider        = google-beta
+  name            = "tf-test-l3-forwarding-rule%{random_suffix}"
+  backend_service = google_compute_region_backend_service.service.id
+  ip_protocol     = "L3_DEFAULT"
+  all_ports       = true
+}
+
+resource "google_compute_region_backend_service" "service" {
+  provider              = google-beta
+  region                = "us-central1"
+  name                  = "service%{random_suffix}"
+  health_checks         = [google_compute_region_health_check.health_check.id]
+  protocol              = "UNSPECIFIED"
+  load_balancing_scheme = "EXTERNAL"
+}
+
+resource "google_compute_region_health_check" "health_check" {
+  provider           = google-beta
+  name               = "tf-test-health-check%{random_suffix}"
+  region             = "us-central1"
+
+  tcp_health_check {
+    port = 80
+  }
+}
+`, context)
+}
+
 func TestAccComputeForwardingRule_forwardingRuleInternallbExample(t *testing.T) {
 	t.Parallel()
 
