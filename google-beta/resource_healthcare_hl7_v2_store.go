8879d7730e331e7f8677ffdc3387011535fd27ff
The Magician <magic-modules@google.com>
2021-08-24 14:28:39 -0500

    add update_mask_fields to healthcare_hl7_v2_store parser_config (#5122) (#3560)
    * add update_mask_fields to healthcare_hl7_v2_store parser_config
    
    * make parser_config default from api
    
    * add tests
    
    * make test changes beta-only
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_healthcare_hl7_v2_store.go
+++ google-beta/resource_healthcare_hl7_v2_store.go
@@ -139,6 +139,7 @@ Fields/functions available for filtering are:
 			},
 			"parser_config": {
 				Type:        schema.TypeList,
+				Computed:    true,
 				Optional:    true,
 				Description: `A nested object resource`,
 				MaxItems:    1,
@@ -357,7 +358,9 @@ func resourceHealthcareHl7V2StoreUpdate(d *schema.ResourceData, meta interface{}
 	updateMask := []string{}
 
 	if d.HasChange("parser_config") {
-		updateMask = append(updateMask, "parserConfig")
+		updateMask = append(updateMask, "parser_config.allow_null_header",
+			"parser_config.segment_terminator",
+			"parser_config.schema")
 	}
 
 	if d.HasChange("labels") {
