0cb6e4d22039766cc605e67c00b15c323991826e
The Magician <magic-modules@google.com>
2021-09-09 13:07:41 -0500

    Add support for ILB as next-hop with tags (#5162) (#3609)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_route_generated_test.go
+++ google-beta/resource_compute_route_generated_test.go
@@ -42,7 +42,7 @@ func TestAccComputeRoute_routeBasicExample(t *testing.T) {
 				ResourceName:            "google_compute_route.default",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"network", "next_hop_instance", "next_hop_vpn_tunnel", "next_hop_ilb"},
+				ImportStateVerifyIgnore: []string{"network", "next_hop_instance", "next_hop_vpn_tunnel"},
 			},
 		},
 	})
@@ -83,7 +83,7 @@ func TestAccComputeRoute_routeIlbExample(t *testing.T) {
 				ResourceName:            "google_compute_route.route-ilb",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"network", "next_hop_instance", "next_hop_vpn_tunnel", "next_hop_ilb"},
+				ImportStateVerifyIgnore: []string{"network", "next_hop_instance", "next_hop_vpn_tunnel"},
 			},
 		},
 	})
@@ -136,6 +136,122 @@ resource "google_compute_route" "route-ilb" {
   network      = google_compute_network.default.name
   next_hop_ilb = google_compute_forwarding_rule.default.id
   priority     = 2000
+}
+`, context)
+}
+
+func TestAccComputeRoute_routeIlbVipExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeRouteDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRoute_routeIlbVipExample(context),
+			},
+			{
+				ResourceName:            "google_compute_route.route-ilb",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"network", "next_hop_instance", "next_hop_vpn_tunnel"},
+			},
+		},
+	})
+}
+
+func testAccComputeRoute_routeIlbVipExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_compute_network" "producer" {
+  provider                = google-beta
+  name                    = "producer%{random_suffix}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "producer" {
+  provider      = google-beta
+  name          = "producer%{random_suffix}-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.producer.id
+}
+
+resource "google_compute_network" "consumer" {
+  provider                = google-beta
+  name                    = "consumer%{random_suffix}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "consumer" {
+  provider      = google-beta
+  name          = "consumer%{random_suffix}-subnet"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.consumer.id
+}
+
+resource "google_compute_network_peering" "peering1" {
+  provider     = google-beta
+  name         = "peering-producer%{random_suffix}-to-consumer%{random_suffix}"
+  network      = google_compute_network.consumer.id
+  peer_network = google_compute_network.producer.id
+}
+
+resource "google_compute_network_peering" "peering2" {
+  provider     = google-beta
+  name         = "peering-consumer%{random_suffix}-to-producer%{random_suffix}"
+  network      = google_compute_network.producer.id
+  peer_network = google_compute_network.consumer.id
+}
+
+resource "google_compute_health_check" "hc" {
+  provider           = google-beta
+  name               = "tf-test-proxy-health-check%{random_suffix}"
+  check_interval_sec = 1
+  timeout_sec        = 1
+
+  tcp_health_check {
+    port = "80"
+  }
+}
+
+resource "google_compute_region_backend_service" "backend" {
+  provider      = google-beta
+  name          = "tf-test-compute-backend%{random_suffix}"
+  region        = "us-central1"
+  health_checks = [google_compute_health_check.hc.id]
+}
+
+resource "google_compute_forwarding_rule" "default" {
+  provider = google-beta
+  name     = "tf-test-compute-forwarding-rule%{random_suffix}"
+  region   = "us-central1"
+
+  load_balancing_scheme = "INTERNAL"
+  backend_service       = google_compute_region_backend_service.backend.id
+  all_ports             = true
+  network               = google_compute_network.producer.name
+  subnetwork            = google_compute_subnetwork.producer.name
+}
+
+resource "google_compute_route" "route-ilb" {
+  provider     = google-beta
+  name         = "tf-test-route-ilb%{random_suffix}"
+  dest_range   = "0.0.0.0/0"
+  network      = google_compute_network.consumer.name
+  next_hop_ilb = google_compute_forwarding_rule.default.ip_address
+  priority     = 2000
+  tags         = ["tag1", "tag2"]
+
+  depends_on = [
+    google_compute_network_peering.peering1,
+    google_compute_network_peering.peering2
+  ]
 }
 `, context)
 }
