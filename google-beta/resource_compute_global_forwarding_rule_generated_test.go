a28aac2e99e192ca2cb324dc04b5f1b838aecd7d
Modular Magician <magic-modules@google.com>
2021-09-17 00:00:03 +0000

    Added end-to-end external HTTP LB example with custom headers (#5183)
    Co-authored-by: Scott Suarez <ScottMSuarez@gmail.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_global_forwarding_rule_generated_test.go
+++ google-beta/resource_compute_global_forwarding_rule_generated_test.go
@@ -23,6 +23,184 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
+func TestAccComputeGlobalForwardingRule_externalHttpLbMigBackendCustomHeaderExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeGlobalForwardingRuleDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeGlobalForwardingRule_externalHttpLbMigBackendCustomHeaderExample(context),
+			},
+			{
+				ResourceName:            "google_compute_global_forwarding_rule.google_compute_global_forwarding_rule",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"network", "port_range", "target"},
+			},
+		},
+	})
+}
+
+func testAccComputeGlobalForwardingRule_externalHttpLbMigBackendCustomHeaderExample(context map[string]interface{}) string {
+	return Nprintf(`
+# External HTTP load balancer with a CDN-enabled managed instance group backend
+# and custom request and response headers
+
+# VPC
+resource "google_compute_network" "xlb_network" {
+  name                    = "tf-test-l7-xlb-network%{random_suffix}"
+  provider                = google
+  auto_create_subnetworks = false
+}
+
+# backend subnet
+resource "google_compute_subnetwork" "xlb_subnet" {
+  name          = "tf-test-l7-xlb-subnet%{random_suffix}"
+  provider      = google
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.xlb_network.id
+}
+
+# forwarding rule
+resource "google_compute_global_forwarding_rule" "google_compute_global_forwarding_rule" {
+  name                  = "tf-test-l7-xlb-forwarding-rule%{random_suffix}"
+  provider              = google
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.default.id
+}
+
+# http proxy
+resource "google_compute_target_http_proxy" "default" {
+  name     = "tf-test-l7-xlb-target-http-proxy%{random_suffix}"
+  provider = google
+  url_map  = google_compute_url_map.default.id
+}
+
+# url map
+resource "google_compute_url_map" "default" {
+  name            = "tf-test-l7-xlb-url-map%{random_suffix}"
+  provider        = google
+  default_service = google_compute_backend_service.default.id
+}
+
+# backend service with custom request and response headers
+resource "google_compute_backend_service" "default" {
+  name                     = "tf-test-l7-xlb-backend-service%{random_suffix}"
+  provider                 = google-beta
+  protocol                 = "HTTP"
+  port_name                = "my-port"
+  load_balancing_scheme    = "EXTERNAL"
+  timeout_sec              = 10
+  enable_cdn               = true
+  custom_request_headers   = ["X-Client-Geo-Location: {client_region_subdivision}, {client_city}"]
+  custom_response_headers  = ["X-Cache-Hit: {cdn_cache_status}"]
+  health_checks            = [google_compute_health_check.default.id]
+  backend {
+    group           = google_compute_instance_group_manager.mig.instance_group
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+}
+
+# instance template
+resource "google_compute_instance_template" "instance_template" {
+  name         = "tf-test-l7-xlb-mig-template%{random_suffix}"
+  provider     = google
+  machine_type = "e2-small"
+  tags         = ["allow-health-check"]
+
+  network_interface {
+    network    = google_compute_network.xlb_network.id
+    subnetwork = google_compute_subnetwork.xlb_subnet.id
+    access_config {
+      # add external ip to fetch packages
+    }
+  }
+  disk {
+    source_image = "debian-cloud/debian-10"
+    auto_delete  = true
+    boot         = true
+  }
+
+  # install nginx and serve a simple web page
+  metadata = {
+    startup-script = <<-EOF1
+      #! /bin/bash
+      set -euo pipefail
+
+      export DEBIAN_FRONTEND=noninteractive
+      apt-get update
+      apt-get install -y nginx-light jq
+
+      NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+      IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+      METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
+
+      cat <<EOF > /var/www/html/index.html
+      <pre>
+      Name: $NAME
+      IP: $IP
+      Metadata: $METADATA
+      </pre>
+      EOF
+    EOF1
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# health check
+resource "google_compute_health_check" "default" {
+  name     = "tf-test-l7-xlb-hc%{random_suffix}"
+  provider = google
+  http_health_check {
+    port_specification = "USE_SERVING_PORT"
+  }
+}
+
+# MIG
+resource "google_compute_instance_group_manager" "mig" {
+  name     = "tf-test-l7-xlb-mig1%{random_suffix}"
+  provider = google
+  zone     = "us-central1-c"
+  named_port {
+    name = "http"
+    port = 8080
+  }
+  version {
+    instance_template = google_compute_instance_template.instance_template.id
+    name              = "primary"
+  }
+  base_instance_name = "vm"
+  target_size        = 2
+}
+
+# allow access from health check ranges
+resource "google_compute_firewall" "fw_health_check" {
+  name          = "tf-test-l7-xlb-fw-allow-hc%{random_suffix}"
+  provider      = google
+  direction     = "INGRESS"
+  network       = google_compute_network.xlb_network.id
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  allow {
+    protocol = "tcp"
+  }
+  target_tags = ["allow-health-check"]
+}
+`, context)
+}
+
 func TestAccComputeGlobalForwardingRule_globalForwardingRuleHttpExample(t *testing.T) {
 	t.Parallel()
 
