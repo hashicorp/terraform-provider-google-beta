ac873420beacef8406098f67b1a5cd25762f1da2
The Magician <magic-modules@google.com>
2021-09-17 13:10:30 -0500

    update comment to generate downstreams (#5212) (#3633)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_dataproc_cluster_test.go
+++ google-beta/resource_dataproc_cluster_test.go
@@ -14,6 +14,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	"google.golang.org/api/googleapi"
+
+	dataproc "google.golang.org/api/dataproc/v1beta2"
 )
 
 func TestDataprocExtractInitTimeout(t *testing.T) {
