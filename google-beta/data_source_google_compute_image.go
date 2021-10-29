a7ac1076ff6af26b9f476168f94fdf28e8b10d7f
The Magician <magic-modules@google.com>
2021-10-29 10:07:33 -0700

    Stop using goimports for third_party files, only use gofmt (#5392) (#3798)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/data_source_google_compute_image.go
+++ google-beta/data_source_google_compute_image.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
-	"google.golang.org/api/compute/v0.beta"
+	compute "google.golang.org/api/compute/v0.beta"
 )
 
 func dataSourceGoogleComputeImage() *schema.Resource {
