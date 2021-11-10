9f3d30c3431f0ae99b76cfd1644bbf0c18376271
Modular Magician <magic-modules@google.com>
2021-11-10 18:21:29 +0000

    Reimplemented compute service attachment using the DCL (#5342)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_cloudbuild_worker_pool.go
+++ google-beta/resource_cloudbuild_worker_pool.go
@@ -69,12 +69,11 @@ func resourceCloudbuildWorkerPool() *schema.Resource {
 			},
 
 			"project": {
-				Type:             schema.TypeString,
-				Computed:         true,
-				Optional:         true,
-				ForceNew:         true,
-				DiffSuppressFunc: compareSelfLinkOrResourceName,
-				Description:      "The project for the resource",
+				Type:        schema.TypeString,
+				Computed:    true,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "The project for the resource",
 			},
 
 			"worker_config": {
