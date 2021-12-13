62a4ef25c6bc4d34ee4e55e86006e8bfe1135eab
Modular Magician <magic-modules@google.com>
2021-12-13 21:26:01 +0000

    Add support for Regional L7 XLB. (#5531)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_compute_forwarding_rule_generated_test.go
+++ google-beta/resource_compute_forwarding_rule_generated_test.go
@@ -952,6 +952,232 @@ resource "google_compute_subnetwork" "proxy" {
 `, context)
 }
 
+func TestAccComputeForwardingRule_forwardingRuleRegionalHttpXlbExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckComputeForwardingRuleDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeForwardingRule_forwardingRuleRegionalHttpXlbExample(context),
+			},
+			{
+				ResourceName:            "google_compute_forwarding_rule.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"backend_service", "network", "subnetwork", "region", "port_range", "target", "ip_address"},
+			},
+		},
+	})
+}
+
+func testAccComputeForwardingRule_forwardingRuleRegionalHttpXlbExample(context map[string]interface{}) string {
+	return Nprintf(`
+// Forwarding rule for Regional External Load Balancing
+resource "google_compute_forwarding_rule" "default" {
+  provider = google-beta
+  depends_on = [google_compute_subnetwork.proxy]
+  name   = "tf-test-website-forwarding-rule%{random_suffix}"
+  region = "us-central1"
+
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = "80"
+  target                = google_compute_region_target_http_proxy.default.id
+  network               = google_compute_network.default.id
+  ip_address            = google_compute_address.default.id
+  network_tier          = "STANDARD"
+}
+
+resource "google_compute_region_target_http_proxy" "default" {
+  provider = google-beta
+
+  region  = "us-central1"
+  name    = "tf-test-website-proxy%{random_suffix}"
+  url_map = google_compute_region_url_map.default.id
+}
+
+resource "google_compute_region_url_map" "default" {
+  provider = google-beta
+
+  region          = "us-central1"
+  name            = "tf-test-website-map%{random_suffix}"
+  default_service = google_compute_region_backend_service.default.id
+}
+
+resource "google_compute_region_backend_service" "default" {
+  provider = google-beta
+
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+
+  backend {
+    group = google_compute_region_instance_group_manager.rigm.instance_group
+    balancing_mode = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+
+  region      = "us-central1"
+  name        = "tf-test-website-backend%{random_suffix}"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_region_health_check.default.id]
+}
+
+data "google_compute_image" "debian_image" {
+  provider = google-beta
+  family   = "debian-9"
+  project  = "debian-cloud"
+}
+
+resource "google_compute_region_instance_group_manager" "rigm" {
+  provider = google-beta
+  region   = "us-central1"
+  name     = "tf-test-website-rigm%{random_suffix}"
+  version {
+    instance_template = google_compute_instance_template.instance_template.id
+    name              = "primary"
+  }
+  base_instance_name = "internal-glb"
+  target_size        = 1
+}
+
+resource "google_compute_instance_template" "instance_template" {
+  provider     = google-beta
+  name         = "template-tf-test-website-backend%{random_suffix}"
+  machine_type = "e2-medium"
+
+  network_interface {
+    network = google_compute_network.default.id
+    subnetwork = google_compute_subnetwork.default.id
+  }
+
+  disk {
+    source_image = data.google_compute_image.debian_image.self_link
+    auto_delete  = true
+    boot         = true
+  }
+
+  tags = ["allow-ssh", "load-balanced-backend"]
+}
+
+resource "google_compute_region_health_check" "default" {
+  depends_on = [google_compute_firewall.fw4]
+  provider = google-beta
+
+  region = "us-central1"
+  name   = "tf-test-website-hc%{random_suffix}"
+  http_health_check {
+    port_specification = "USE_SERVING_PORT"
+  }
+}
+
+resource "google_compute_address" "default" {
+  name = "tf-test-website-ip%{random_suffix}-1"
+  provider = google-beta
+  region = "us-central1"
+  network_tier = "STANDARD"
+}
+
+resource "google_compute_firewall" "fw1" {
+  provider = google-beta
+  name = "tf-test-website-fw%{random_suffix}-1"
+  network = google_compute_network.default.id
+  source_ranges = ["10.1.2.0/24"]
+  allow {
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "udp"
+  }
+  allow {
+    protocol = "icmp"
+  }
+  direction = "INGRESS"
+}
+
+resource "google_compute_firewall" "fw2" {
+  depends_on = [google_compute_firewall.fw1]
+  provider = google-beta
+  name = "tf-test-website-fw%{random_suffix}-2"
+  network = google_compute_network.default.id
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+  target_tags = ["allow-ssh"]
+  direction = "INGRESS"
+}
+
+resource "google_compute_firewall" "fw3" {
+  depends_on = [google_compute_firewall.fw2]
+  provider = google-beta
+  name = "tf-test-website-fw%{random_suffix}-3"
+  network = google_compute_network.default.id
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  allow {
+    protocol = "tcp"
+  }
+  target_tags = ["load-balanced-backend"]
+  direction = "INGRESS"
+}
+
+resource "google_compute_firewall" "fw4" {
+  depends_on = [google_compute_firewall.fw3]
+  provider = google-beta
+  name = "tf-test-website-fw%{random_suffix}-4"
+  network = google_compute_network.default.id
+  source_ranges = ["10.129.0.0/26"]
+  target_tags = ["load-balanced-backend"]
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+  allow {
+    protocol = "tcp"
+    ports = ["443"]
+  }
+  allow {
+    protocol = "tcp"
+    ports = ["8000"]
+  }
+  direction = "INGRESS"
+}
+
+resource "google_compute_network" "default" {
+  provider = google-beta
+  name                    = "tf-test-website-net%{random_suffix}"
+  auto_create_subnetworks = false
+  routing_mode = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "default" {
+  provider = google-beta
+  name          = "tf-test-website-net%{random_suffix}-default"
+  ip_cidr_range = "10.1.2.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+}
+
+resource "google_compute_subnetwork" "proxy" {
+  provider = google-beta
+  name          = "tf-test-website-net%{random_suffix}-proxy"
+  ip_cidr_range = "10.129.0.0/26"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+  purpose       = "REGIONAL_MANAGED_PROXY"
+  role          = "ACTIVE"
+}
+`, context)
+}
+
 func testAccCheckComputeForwardingRuleDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
