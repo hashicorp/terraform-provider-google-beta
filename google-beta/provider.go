2c2fe60d4096205eab8b109412871031e3982255
The Magician <magic-modules@google.com>
2021-12-23 11:13:24 -0800

    Revert "Apigee Nat Address Resource (#5018)" (#5576) (#3943)
    This reverts commit ed444936dee897e0e6b2027ce7aa81df5ed4b8f2.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -900,9 +900,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 243
+// Generated resources: 242
 // Generated IAM resources: 123
-// Total generated resources: 366
+// Total generated resources: 365
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -945,7 +945,6 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_apigee_envgroup":                                       resourceApigeeEnvgroup(),
 			"google_apigee_instance_attachment":                            resourceApigeeInstanceAttachment(),
 			"google_apigee_envgroup_attachment":                            resourceApigeeEnvgroupAttachment(),
-			"google_apigee_nat_address":                                    resourceApigeeNatAddress(),
 			"google_app_engine_domain_mapping":                             resourceAppEngineDomainMapping(),
 			"google_app_engine_firewall_rule":                              resourceAppEngineFirewallRule(),
 			"google_app_engine_standard_app_version":                       resourceAppEngineStandardAppVersion(),
