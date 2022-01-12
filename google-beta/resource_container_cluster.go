f064736d96d4017fd9ddb4ff168ddfd6383cd8fa
The Magician <magic-modules@google.com>
2022-01-11 16:11:20 -0800

    Promote `dns_config` of `google_container_cluster` to GA (#5495) (#3978)
    Co-authored-by: upodroid <cy@borg.dev>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: upodroid <cy@borg.dev>

--- google-beta/resource_container_cluster.go
+++ google-beta/resource_container_cluster.go
@@ -1873,7 +1873,6 @@ func resourceContainerClusterRead(d *schema.ResourceData, meta interface{}) erro
 	if err := d.Set("dns_config", flattenDnsConfig(cluster.NetworkConfig.DnsConfig)); err != nil {
 		return err
 	}
-
 	if err := d.Set("logging_config", flattenContainerClusterLoggingConfig(cluster.LoggingConfig)); err != nil {
 		return err
 	}
