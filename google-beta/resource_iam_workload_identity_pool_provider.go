f7d814c3baa6395066e05bb1827979c0d20f15bd
The Magician <magic-modules@google.com>
2022-01-21 14:23:55 -0800

    Update all generated resources to 20m timeouts (#5638) (#4002)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_iam_workload_identity_pool_provider.go
+++ google-beta/resource_iam_workload_identity_pool_provider.go
@@ -65,9 +65,9 @@ func resourceIAMBetaWorkloadIdentityPoolProvider() *schema.Resource {
 		},
 
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(4 * time.Minute),
-			Update: schema.DefaultTimeout(4 * time.Minute),
-			Delete: schema.DefaultTimeout(4 * time.Minute),
+			Create: schema.DefaultTimeout(20 * time.Minute),
+			Update: schema.DefaultTimeout(20 * time.Minute),
+			Delete: schema.DefaultTimeout(20 * time.Minute),
 		},
 
 		Schema: map[string]*schema.Schema{
