83137403e0b6320f6f07aef635f3da5864a494fe
Modular Magician <magic-modules@google.com>
2022-02-23 22:12:24 +0000

    Add endpointAttachment in Apigee (#5717)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -901,9 +901,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 244
+// Generated resources: 245
 // Generated IAM resources: 126
-// Total generated resources: 370
+// Total generated resources: 371
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -946,6 +946,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_apigee_envgroup":                                       resourceApigeeEnvgroup(),
 			"google_apigee_instance_attachment":                            resourceApigeeInstanceAttachment(),
 			"google_apigee_envgroup_attachment":                            resourceApigeeEnvgroupAttachment(),
+			"google_apigee_endpoint_attachment":                            resourceApigeeEndpointAttachment(),
 			"google_app_engine_domain_mapping":                             resourceAppEngineDomainMapping(),
 			"google_app_engine_firewall_rule":                              resourceAppEngineFirewallRule(),
 			"google_app_engine_standard_app_version":                       resourceAppEngineStandardAppVersion(),
