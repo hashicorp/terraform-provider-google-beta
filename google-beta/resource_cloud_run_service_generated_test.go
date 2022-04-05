f0a69d4c3a6f9d33456aa32cdfed9f87f4dfd742
Modular Magician <magic-modules@google.com>
2022-04-05 20:45:47 +0000

    Adding Eventarc example for cgc docs (#5852)
    Co-authored-by: Shuya Ma <87669292+shuyama1@users.noreply.github.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_cloud_run_service_generated_test.go
+++ google-beta/resource_cloud_run_service_generated_test.go
@@ -458,6 +458,143 @@ resource "google_cloud_run_service" "default" {
 `, context)
 }
 
+func TestAccCloudRunService_eventarcBasicTfExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckCloudRunServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_eventarcBasicTfExample(context),
+			},
+			{
+				ResourceName:            "google_cloud_run_service.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "location"},
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_eventarcBasicTfExample(context map[string]interface{}) string {
+	return Nprintf(`
+# Used to retrieve project_number later
+data "google_project" "project" {
+}
+        
+# Enable Cloud Run API
+resource "google_project_service" "run" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+    
+# Enable Eventarc API
+resource "google_project_service" "eventarc" {
+  service            = "eventarc.googleapis.com"
+  disable_on_destroy = false
+}
+
+
+  
+# Deploy Cloud Run service
+resource "google_cloud_run_service" "default" {
+  name     = "tf-test-cloudrun-hello-tf%{random_suffix}"
+  location = "us-east1"
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+  
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+  
+  depends_on = [google_project_service.run]
+}
+      
+# Make Cloud Run service publicly accessible
+resource "google_cloud_run_service_iam_member" "allUsers" {
+  service  = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+
+  
+      
+# Create a Pub/Sub trigger
+resource "google_eventarc_trigger" "tf-test-trigger-pubsub-tf%{random_suffix}" {
+name     = "tf-test-trigger-pubsub-tf%{random_suffix}"
+location = google_cloud_run_service.default.location
+matching_criteria {
+  attribute = "type"   
+  value     = "google.cloud.pubsub.topic.v1.messagePublished"
+}
+destination {
+  cloud_run_service {
+    service = google_cloud_run_service.default.name
+    region  = google_cloud_run_service.default.location
+  }
+}
+
+depends_on = [google_project_service.eventarc]
+}
+
+
+# Give default Compute service account eventarc.eventReceiver role
+resource "google_project_iam_binding" "project" {
+  project = data.google_project.project.id
+  role    = "roles/eventarc.eventReceiver"
+
+  members = [
+    "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+  ]
+}
+    
+# Create an AuditLog for Cloud Storage trigger
+resource "google_eventarc_trigger" "tf-test-trigger-auditlog-tf%{random_suffix}" {
+  name     = "tf-test-trigger-auditlog-tf%{random_suffix}"
+  location = google_cloud_run_service.default.location
+  project  = data.google_project.project.id
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.audit.log.v1.written"
+  }
+  matching_criteria {
+    attribute = "serviceName"
+    value     = "storage.googleapis.com"
+  }
+matching_criteria {
+attribute = "methodName"
+value     = "storage.objects.create"
+}
+destination {
+cloud_run_service {
+  service = google_cloud_run_service.default.name
+  region  = google_cloud_run_service.default.location
+}
+}
+service_account = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+
+depends_on = [google_project_service.eventarc]
+}
+
+`, context)
+}
+
 func testAccCheckCloudRunServiceDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
