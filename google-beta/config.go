4e9106c3bd4d08d5fa98839844351774484c070c
Modular Magician <magic-modules@google.com>
2022-04-14 21:24:36 +0000

    Add support for Cloud Deploy resources (#5710)
    * add cloud deploy resources
    
    Co-authored-by: upodroid <cy@borg.dev>
    
    * add new test
    
    * apply suggestions from sam
    
    * remove custom flattener
    
    * fix import id, add new tests and fix bad rebase
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/config.go
+++ google-beta/config.go
@@ -177,6 +177,7 @@ type Config struct {
 	BinaryAuthorizationBasePath  string
 	CloudAssetBasePath           string
 	CloudBuildBasePath           string
+	CloudDeployBasePath          string
 	CloudFunctionsBasePath       string
 	Cloudfunctions2BasePath      string
 	CloudIdentityBasePath        string
@@ -287,6 +288,7 @@ const BillingBasePathKey = "Billing"
 const BinaryAuthorizationBasePathKey = "BinaryAuthorization"
 const CloudAssetBasePathKey = "CloudAsset"
 const CloudBuildBasePathKey = "CloudBuild"
+const CloudDeployBasePathKey = "CloudDeploy"
 const CloudFunctionsBasePathKey = "CloudFunctions"
 const Cloudfunctions2BasePathKey = "Cloudfunctions2"
 const CloudIdentityBasePathKey = "CloudIdentity"
@@ -380,6 +382,7 @@ var DefaultBasePaths = map[string]string{
 	BinaryAuthorizationBasePathKey:  "https://binaryauthorization.googleapis.com/v1/",
 	CloudAssetBasePathKey:           "https://cloudasset.googleapis.com/v1/",
 	CloudBuildBasePathKey:           "https://cloudbuild.googleapis.com/v1/",
+	CloudDeployBasePathKey:          "https://clouddeploy.googleapis.com/v1/",
 	CloudFunctionsBasePathKey:       "https://cloudfunctions.googleapis.com/v1/",
 	Cloudfunctions2BasePathKey:      "https://cloudfunctions.googleapis.com/v2beta/",
 	CloudIdentityBasePathKey:        "https://cloudidentity.googleapis.com/v1beta1/",
@@ -1249,6 +1252,7 @@ func ConfigureBasePaths(c *Config) {
 	c.BinaryAuthorizationBasePath = DefaultBasePaths[BinaryAuthorizationBasePathKey]
 	c.CloudAssetBasePath = DefaultBasePaths[CloudAssetBasePathKey]
 	c.CloudBuildBasePath = DefaultBasePaths[CloudBuildBasePathKey]
+	c.CloudDeployBasePath = DefaultBasePaths[CloudDeployBasePathKey]
 	c.CloudFunctionsBasePath = DefaultBasePaths[CloudFunctionsBasePathKey]
 	c.Cloudfunctions2BasePath = DefaultBasePaths[Cloudfunctions2BasePathKey]
 	c.CloudIdentityBasePath = DefaultBasePaths[CloudIdentityBasePathKey]
