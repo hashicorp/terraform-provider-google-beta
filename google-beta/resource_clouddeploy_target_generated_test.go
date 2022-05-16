f5f749d2c434097dcc5c3e250e87a1b3d9f6c7d0
Modular Magician <magic-modules@google.com>
2022-05-16 21:19:16 +0000

    hardcode protocol_version (#6035)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_clouddeploy_target_generated_test.go
+++ google-beta/resource_clouddeploy_target_generated_test.go
@@ -91,9 +91,9 @@ resource "google_clouddeploy_target" "primary" {
   name     = "tf-test-target%{random_suffix}"
 
   annotations = {
-    my_second_annotation = "example-annotation-2"
-
     my_first_annotation = "example-annotation-1"
+
+    my_second_annotation = "example-annotation-2"
   }
 
   description = "basic description"
@@ -241,9 +241,9 @@ resource "google_clouddeploy_target" "primary" {
   name     = "tf-test-target%{random_suffix}"
 
   annotations = {
-    my_second_annotation = "updated-example-annotation-2"
-
     my_third_annotation = "example-annotation-3"
+
+    my_second_annotation = "updated-example-annotation-2"
   }
 
   description = "updated description"
