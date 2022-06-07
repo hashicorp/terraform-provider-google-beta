4c64dacdf119f8b60f1707f3e62c4f0851869511
Modular Magician <magic-modules@google.com>
2022-06-07 18:55:59 +0000

    Prepared storage_pubsub_notifications snippet for inclusion on C.G.C (#6031)
    Co-authored-by: Riley Karson <rileykarson@google.com>
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_cgc_snippet_generated_test.go
+++ google-beta/resource_cgc_snippet_generated_test.go
@@ -1016,3 +1016,60 @@ resource "google_storage_bucket_iam_member" "member" {
 }
 `, context)
 }
+
+func TestAccCGCSnippet_storagePubsubNotificationsExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProvidersOiCS,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCGCSnippet_storagePubsubNotificationsExample(context),
+			},
+			{
+				ResourceName:      "google_pubsub_topic.topic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCGCSnippet_storagePubsubNotificationsExample(context map[string]interface{}) string {
+	return Nprintf(`
+// Create a Pub/Sub notification.
+resource "google_storage_notification" "notification" {
+  bucket         = google_storage_bucket.bucket.name
+  payload_format = "JSON_API_V1"
+  topic          = google_pubsub_topic.topic.id
+  depends_on = [google_pubsub_topic_iam_binding.binding]
+}
+
+// Enable notifications by giving the correct IAM permission to the unique service account.
+data "google_storage_project_service_account" "gcs_account" {
+}
+
+// Create a Pub/Sub topic.
+resource "google_pubsub_topic_iam_binding" "binding" {
+  topic   = google_pubsub_topic.topic.id
+  role    = "roles/pubsub.publisher"
+  members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
+}
+
+// Create a new storage bucket.
+resource "google_storage_bucket" "bucket" {
+  name     = "tf-test-example-bucket-name%{random_suffix}"
+  location = "US"
+  uniform_bucket_level_access = true
+}
+
+resource "google_pubsub_topic" "topic" {
+  name = "tf_test_your_topic_name%{random_suffix}"
+}
+`, context)
+}
