499ef9a91f7e5ebadc7ea26dcbbb428a3e2b5c43
Modular Magician <magic-modules@google.com>
2022-07-07 17:08:15 +0000

    feat: add Cloud Run system packages example (#6204)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_cloud_run_service_generated_test.go
+++ google-beta/resource_cloud_run_service_generated_test.go
@@ -993,6 +993,65 @@ resource "google_compute_global_forwarding_rule" "https_default" {
 `, context)
 }
 
+func TestAccCloudRunService_cloudRunSystemPackagesExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckCloudRunServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_cloudRunSystemPackagesExample(context),
+			},
+			{
+				ResourceName:            "google_cloud_run_service.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "location"},
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_cloudRunSystemPackagesExample(context map[string]interface{}) string {
+	return Nprintf(`
+# Example of how to deploy a Cloud Run application with system packages
+
+resource "google_cloud_run_service" "default" {
+  name     = "tf-test-graphviz-example%{random_suffix}"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        # Replace with the URL of your graphviz image
+        #   gcr.io/<YOUR_GCP_PROJECT_ID>/graphviz
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Make Cloud Run service publicly accessible
+resource "google_cloud_run_service_iam_member" "allow_unauthenticated" {
+  service  = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+`, context)
+}
+
 func testAccCheckCloudRunServiceDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
