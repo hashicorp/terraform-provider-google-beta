27c29a913d39d2db23e3f6e1c46f78675f977c9f
The Magician <magic-modules@google.com>
2022-07-18 12:06:42 -0700

    Add static outbound Cloud Run sample (#6264) (#4495)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_cloud_run_service_generated_test.go
+++ google-beta/resource_cloud_run_service_generated_test.go
@@ -384,6 +384,115 @@ resource "google_cloud_run_service" "default" {
 `, context)
 }
 
+func TestAccCloudRunService_cloudRunServiceStaticOutboundExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckCloudRunServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_cloudRunServiceStaticOutboundExample(context),
+			},
+			{
+				ResourceName:            "google_cloud_run_service.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "location"},
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_cloudRunServiceStaticOutboundExample(context map[string]interface{}) string {
+	return Nprintf(`
+# Example of setting up a Cloud Run service with a static outbound IP
+
+resource "google_compute_network" "default" {
+  name = "tf-test-cr-static-ip-network%{random_suffix}"
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "tf-test-cr-static-ip%{random_suffix}"
+  ip_cidr_range = "10.124.0.0/28"
+  network       = google_compute_network.default.id
+  region        = "us-central1"
+}
+
+resource "google_project_service" "vpc" {
+  service = "vpcaccess.googleapis.com"
+}
+
+resource "google_vpc_access_connector" "default" {
+  provider = google-beta
+  name     = "tf-test-cr-conn%{random_suffix}"
+
+  subnet {
+    name = google_compute_subnetwork.default.name
+  }
+
+  # Wait for VPC API enablement
+  # before creating this resource
+  depends_on = [
+    google_project_service.vpc
+  ]
+}
+
+resource "google_compute_router" "default" {
+  name    = "tf-test-cr-static-ip-router%{random_suffix}"
+  network = google_compute_network.default.name
+  region  = google_compute_subnetwork.default.region
+}
+
+resource "google_compute_address" "default" {
+  name   = "tf-test-cr-static-ip-addr%{random_suffix}"
+  region = google_compute_subnetwork.default.region
+}
+
+resource "google_compute_router_nat" "default" {
+  name   = "tf-test-cr-static-nat%{random_suffix}"
+  router = google_compute_router.default.name
+  region = google_compute_subnetwork.default.region
+
+  nat_ip_allocate_option = "MANUAL_ONLY"
+  nat_ips                = [google_compute_address.default.self_link]
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.default.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = "tf-test-cr-static-ip-service%{random_suffix}"
+  location = google_compute_subnetwork.default.region
+
+  template {
+    spec {
+      containers {
+        # TODO<developer>: replace with a URL to your container
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+
+  metadata {
+    annotations = {
+      "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.default.name
+      "run.googleapis.com/vpc-access-egress"    = "all-traffic"
+      "run.googleapis.com/ingress"              = "all"
+    }
+  }
+}
+`, context)
+}
+
 func TestAccCloudRunService_cloudRunServiceScheduledExample(t *testing.T) {
 	t.Parallel()
 
