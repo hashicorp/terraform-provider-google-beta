0c8a5bf689c2d2077ea70d918b8db37b05de6dbd
The Magician <magic-modules@google.com>
2022-07-25 10:18:16 -0700

    docs: add sample for Cloud Run Secure Services tutorial (#6235) (#4525)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_cloud_run_service_generated_test.go
+++ google-beta/resource_cloud_run_service_generated_test.go
@@ -1472,6 +1472,119 @@ resource "google_cloud_run_service_iam_member" "allow_unauthenticated" {
 `, context)
 }
 
+func TestAccCloudRunService_cloudRunServiceSecureServicesExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": randString(t, 10),
+	}
+
+	vcrTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProvidersOiCS,
+		CheckDestroy: testAccCheckCloudRunServiceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCloudRunService_cloudRunServiceSecureServicesExample(context),
+			},
+			{
+				ResourceName:            "google_cloud_run_service.renderer",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"name", "location"},
+			},
+		},
+	})
+}
+
+func testAccCloudRunService_cloudRunServiceSecureServicesExample(context map[string]interface{}) string {
+	return Nprintf(`
+resource "google_cloud_run_service" "renderer" {
+  name     = "renderer%{random_suffix}"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        # Replace with the URL of your Secure Services > Renderer image.
+        #   gcr.io/<PROJECT_ID>/renderer
+        image = "gcr.io/cloudrun/hello"
+      }
+      service_account_name = google_service_account.renderer.email
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service" "editor" {
+  name     = "editor%{random_suffix}"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        # Replace with the URL of your Secure Services > Editor image.
+        #   gcr.io/<PROJECT_ID>/editor
+        image = "gcr.io/cloudrun/hello"
+        env {
+          name  = "EDITOR_UPSTREAM_RENDER_URL"
+          value = google_cloud_run_service.renderer.status[0].url
+        }
+      }
+      service_account_name = google_service_account.editor.email
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+resource "google_service_account" "renderer" {
+  account_id   = "renderer-identity"
+  display_name = "Service identity of the Renderer (Backend) service."
+}
+
+resource "google_service_account" "editor" {
+  account_id   = "editor-identity"
+  display_name = "Service identity of the Editor (Frontend) service."
+}
+
+resource "google_cloud_run_service_iam_member" "editor_invokes_renderer" {
+  location = google_cloud_run_service.renderer.location
+  service  = google_cloud_run_service.renderer.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.editor.email}"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.editor.location
+  project  = google_cloud_run_service.editor.project
+  service  = google_cloud_run_service.editor.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+output "backend_url" {
+  value = google_cloud_run_service.renderer.status[0].url
+}
+
+output "frontend_url" {
+  value = google_cloud_run_service.editor.status[0].url
+}
+`, context)
+}
+
 func TestAccCloudRunService_cloudRunServiceTasksExample(t *testing.T) {
 	t.Parallel()
 
