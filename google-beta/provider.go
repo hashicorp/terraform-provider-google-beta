ce9930aa20897be2c6bb237a1d46a919d14f09cf
The Magician <magic-modules@google.com>
2022-11-01 16:15:55 -0700

    Add new Resource Cryptokeyversions (#6553) (#4831)
    * Add new resource cryptokeyVersion, and the tests.
    
    * Resource cryptoKeyVersion -- Fix errors that prevent creation
    
    * CryptoKeyVersion resource -- [WIP] commit
    
    * CryptoKeyVersions -- Fix tests for create and create with skip_initial_version
    
    * CryptoKeyVersions -- Fix after running gofmt
    
    * CryptoKeyVersions -- modification based on PR feedback, except add update test
    
    * CryptoKeyVersion -- Add update test
    
    * gofmt fix the test file
    
    * Remove unused fields and update tests
    
    * Add custom import, yet tests aren't fully passing because of custom_delete
    
    * Attempt to fix custom import and delete
    
    * Delete kms_crypto_key_version.erb
    
    * Fix Custom delete issues
    
    * Enable importStateVerify to all tests
    
    * Fix make lint problem
    
    * Delete go.mod
    
    * (1) Remove enum values for output fields
    (2) fix indentation problems
    (3) Fix state verify for crypto_Key
    (4) Add remove test for cryptoKeyVersions
    (5) WIP Fix custom_delete issues
    
    * (1) Call destroy at cryptoKeyVersion deletion
    (2) Do not destroy cryptoKeyVersion when deleting crypto_key
    
    * CryptoKeyVersions Delete, put error handler first
    
    * Uncomment clearCryptoKeyVersions when deleting cryptoKey
    
    * Align with master branch, check for state before deleting cryptokeyversion
    
    * Remove state check in cryptoKeyVersions deletion
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -1006,9 +1006,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 275
+// Generated resources: 276
 // Generated IAM resources: 183
-// Total generated resources: 458
+// Total generated resources: 459
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1358,6 +1358,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_identity_platform_tenant":                              resourceIdentityPlatformTenant(),
 			"google_kms_key_ring":                                          resourceKMSKeyRing(),
 			"google_kms_crypto_key":                                        resourceKMSCryptoKey(),
+			"google_kms_crypto_key_version":                                resourceKMSCryptoKeyVersion(),
 			"google_kms_key_ring_import_job":                               resourceKMSKeyRingImportJob(),
 			"google_kms_secret_ciphertext":                                 resourceKMSSecretCiphertext(),
 			"google_logging_metric":                                        resourceLoggingMetric(),
