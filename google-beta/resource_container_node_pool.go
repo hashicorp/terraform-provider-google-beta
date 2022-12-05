cc8aaf91e905176125e91b793cc5390d31f39890
Modular Magician <magic-modules@google.com>
2022-12-05 17:27:07 +0000

    fix permadiff thrashing (#6908)
    Co-authored-by: Edward Sun <sunedward@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_container_node_pool.go
+++ google-beta/resource_container_node_pool.go
@@ -1575,6 +1575,10 @@ func nodePoolUpdate(d *schema.ResourceData, meta interface{}, nodePoolInfo *Node
 				if v, ok := upgradeSettingsConfig["max_surge"]; ok {
 					upgradeSettings.MaxSurge = int64(v.(int))
 				}
+				// max_unavailable not be preserved if only max_surge is updated
+				if v, ok := upgradeSettingsConfig["max_unavailable"]; ok {
+					upgradeSettings.MaxUnavailable = int64(v.(int))
+				}
 			}
 
 			if d.HasChange(prefix + "upgrade_settings.0.max_unavailable") {
@@ -1583,6 +1587,10 @@ func nodePoolUpdate(d *schema.ResourceData, meta interface{}, nodePoolInfo *Node
 				}
 				if v, ok := upgradeSettingsConfig["max_unavailable"]; ok {
 					upgradeSettings.MaxUnavailable = int64(v.(int))
+				}
+				// max_surge not be preserved if only max_unavailable is updated
+				if v, ok := upgradeSettingsConfig["max_surge"]; ok {
+					upgradeSettings.MaxSurge = int64(v.(int))
 				}
 			}
 
