58ac48d765b10366b21f0046ef99e59abdc6f9ad
Modular Magician <magic-modules@google.com>
2023-01-18 15:49:36 +0000

    Add support for enabling/disabling Cloud Data Lineage integration in google-beta. (#7102)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_composer_environment_test.go
+++ google-beta/resource_composer_environment_test.go
@@ -1525,6 +1525,9 @@ resource "google_composer_environment" "test" {
 
       software_config {
         image_version = "composer-2-airflow-2"
+        cloud_data_lineage_integration {
+          enabled = true
+        }
       }
 
       workloads_config {
@@ -1784,6 +1787,9 @@ resource "google_composer_environment" "test" {
 
       software_config {
         image_version = "composer-2-airflow-2"
+        cloud_data_lineage_integration {
+          enabled = false
+        }
       }
 
       workloads_config {
