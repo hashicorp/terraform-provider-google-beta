d9ddfef020eebc0927f68ce1e2c71f997f48b839
The Magician <magic-modules@google.com>
2023-01-18 17:38:31 -0800

    Support location in google tags tag binding (#6716) (#5115)
    * working on tags issue
    
    * added handwritten resource for location_tags
    
    * reverting_ruby_version
    
    * fixing_the_test_of_location_tag_binding
    
    * fixing_the_test_og_tags_location_tag_binding
    
    * fixing the test case of location_tag_binding
    
    * fixing the test case of location tag binding
    
    * fix_the_test_case_of_location_tag_binding
    
    * fixing_the_test_and_added_different_destroy_producer
    
    * fixing_the_test_case_of_location_tag_binding
    
    * reverting some changes in test case
    
    * adding new test file for location tag binding
    
    * added a seprate test file for location tag binding
    
    * added autogen exception in test file of location tag binding
    
    * making_GA_only_resource_tags_location_tag_binding
    
    * adding the test in common test file
    
    * changing the test config in acceptance test of location_tag_binding
    
    * minor fixes
    
    * fixing the linting error
    
    * minor fixes
    
    * minor fixes
    
    * committing to run pipeline
    
    * reverting-ruby-version
    
    * running pipline for debugging
    
    * changin the IAM permission in tags test
    
    * added permission for cloud run instance
    
    * added permission for cloud run instance
    
    * added permission for cloud run instance in acceptance test
    
    * adding IAM policy to test case of location_tags_binding
    
    * changing the IAM  role in test case of location_tag_binding
    
    * adding the iam binding for cloudrun service in the test case of location_tag_binding
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * minor fixes
    
    * re-running the pipeline
    
    * minor linting fixes
    
    * added the docs
    
    * handling pagination in Read method of resource
    
    * fixing linting errors
    
    * minor fixes
    
    * fixing the minor errors
    
    * fixing the minor errors
    
    * changing the id format
    
    * fixing import function
    
    * updating the docs
    
    * fixing linting error
    
    * fixing linting error
    
    * fixing the import statement
    
    * minor fixes and adding import_state_verify
    
    * minor fix
    
    * addressing the review comments
    
    * addressing the review comments
    
    * commiting to run pipeline
    
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/config.go
+++ google-beta/config.go
@@ -278,6 +278,7 @@ type Config struct {
 	CloudIoTBasePath          string
 	ServiceNetworkingBasePath string
 	BigtableAdminBasePath     string
+	TagsLocationBasePath      string
 
 	// dcl
 	ContainerAwsBasePath   string
@@ -393,6 +394,7 @@ const ServiceNetworkingBasePathKey = "ServiceNetworking"
 const BigtableAdminBasePathKey = "BigtableAdmin"
 const ContainerAwsBasePathKey = "ContainerAws"
 const ContainerAzureBasePathKey = "ContainerAzure"
+const TagsLocationBasePathKey = "TagsLocation"
 
 // Generated product base paths
 var DefaultBasePaths = map[string]string{
@@ -502,6 +504,7 @@ var DefaultBasePaths = map[string]string{
 	BigtableAdminBasePathKey:        "https://bigtableadmin.googleapis.com/v2/",
 	ContainerAwsBasePathKey:         "https://{{location}}-gkemulticloud.googleapis.com/v1/",
 	ContainerAzureBasePathKey:       "https://{{location}}-gkemulticloud.googleapis.com/v1/",
+	TagsLocationBasePathKey:         "https://{{location}}-cloudresourcemanager.googleapis.com/v3/",
 }
 
 var DefaultClientScopes = []string{
@@ -1389,4 +1392,5 @@ func ConfigureBasePaths(c *Config) {
 	c.ServiceNetworkingBasePath = DefaultBasePaths[ServiceNetworkingBasePathKey]
 	c.BigQueryBasePath = DefaultBasePaths[BigQueryBasePathKey]
 	c.BigtableAdminBasePath = DefaultBasePaths[BigtableAdminBasePathKey]
+	c.TagsLocationBasePath = DefaultBasePaths[TagsLocationBasePathKey]
 }
