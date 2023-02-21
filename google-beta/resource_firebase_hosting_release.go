b8082bf611bf54f70fafb5b162c1973fb9509f1a
Modular Magician <magic-modules@google.com>
2023-02-21 20:35:53 +0000

    Make resource definition functions public (#7324)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_firebase_hosting_release.go
+++ google-beta/resource_firebase_hosting_release.go
@@ -23,7 +23,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func resourceFirebaseHostingRelease() *schema.Resource {
+func ResourceFirebaseHostingRelease() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceFirebaseHostingReleaseCreate,
 		Read:   resourceFirebaseHostingReleaseRead,
