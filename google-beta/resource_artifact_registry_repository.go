722c0e1753f725cb966053c5d1370a7e219e2ffe
The Magician <magic-modules@google.com>
2023-03-17 14:03:25 -0700

    Update Repository.yaml (#7491) (#5340)
    Correct documentation on output name.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_artifact_registry_repository.go
+++ google-beta/resource_artifact_registry_repository.go
@@ -273,7 +273,7 @@ Repository. Upstream policies cannot be set on a standard repository.`,
 				Type:     schema.TypeString,
 				Computed: true,
 				Description: `The name of the repository, for example:
-"projects/p1/locations/us-central1/repositories/repo1"`,
+"repo1"`,
 			},
 			"update_time": {
 				Type:        schema.TypeString,
