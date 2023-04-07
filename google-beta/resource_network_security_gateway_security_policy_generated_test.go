d345bbc319f87e94b03c16f77138e365464f0edc
The Magician <magic-modules@google.com>
2023-04-07 14:43:46 -0700

    Make network security gateway names singular (#7666) (#5453)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_network_security_gateway_security_policy_generated_test.go
+++ google-beta/resource_network_security_gateway_security_policy_generated_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccNetworkSecurityGatewaySecurityPolicies_networkSecurityGatewaySecurityPoliciesBasicExample(t *testing.T) {
+func TestAccNetworkSecurityGatewaySecurityPolicy_networkSecurityGatewaySecurityPolicyBasicExample(t *testing.T) {
 	t.Parallel()
 
 	context := map[string]interface{}{
@@ -33,13 +33,13 @@ func TestAccNetworkSecurityGatewaySecurityPolicies_networkSecurityGatewaySecurit
 	VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: ProtoV5ProviderBetaFactories(t),
-		CheckDestroy:             testAccCheckNetworkSecurityGatewaySecurityPoliciesDestroyProducer(t),
+		CheckDestroy:             testAccCheckNetworkSecurityGatewaySecurityPolicyDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccNetworkSecurityGatewaySecurityPolicies_networkSecurityGatewaySecurityPoliciesBasicExample(context),
+				Config: testAccNetworkSecurityGatewaySecurityPolicy_networkSecurityGatewaySecurityPolicyBasicExample(context),
 			},
 			{
-				ResourceName:            "google_network_security_gateway_security_policies.default",
+				ResourceName:            "google_network_security_gateway_security_policy.default",
 				ImportState:             true,
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"name", "location"},
@@ -48,9 +48,9 @@ func TestAccNetworkSecurityGatewaySecurityPolicies_networkSecurityGatewaySecurit
 	})
 }
 
-func testAccNetworkSecurityGatewaySecurityPolicies_networkSecurityGatewaySecurityPoliciesBasicExample(context map[string]interface{}) string {
+func testAccNetworkSecurityGatewaySecurityPolicy_networkSecurityGatewaySecurityPolicyBasicExample(context map[string]interface{}) string {
 	return Nprintf(`
-resource "google_network_security_gateway_security_policies" "default" {
+resource "google_network_security_gateway_security_policy" "default" {
   provider    = google-beta
   name        = "tf-test-my-gateway-security-policy%{random_suffix}"
   location    = "us-central1"
@@ -59,10 +59,10 @@ resource "google_network_security_gateway_security_policies" "default" {
 `, context)
 }
 
-func testAccCheckNetworkSecurityGatewaySecurityPoliciesDestroyProducer(t *testing.T) func(s *terraform.State) error {
+func testAccCheckNetworkSecurityGatewaySecurityPolicyDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
-			if rs.Type != "google_network_security_gateway_security_policies" {
+			if rs.Type != "google_network_security_gateway_security_policy" {
 				continue
 			}
 			if strings.HasPrefix(name, "data.") {
@@ -84,7 +84,7 @@ func testAccCheckNetworkSecurityGatewaySecurityPoliciesDestroyProducer(t *testin
 
 			_, err = SendRequest(config, "GET", billingProject, url, config.UserAgent, nil)
 			if err == nil {
-				return fmt.Errorf("NetworkSecurityGatewaySecurityPolicies still exists at %s", url)
+				return fmt.Errorf("NetworkSecurityGatewaySecurityPolicy still exists at %s", url)
 			}
 		}
 
