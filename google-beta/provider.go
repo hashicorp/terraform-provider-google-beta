0d18fdb192dee93f76acddc4550e0d5e8ed43d60
The Magician <magic-modules@google.com>
2023-05-02 09:35:13 -0700

    Add google_gkeonprem_vmware_node_pool resource (#7824) (#5579)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider.go
+++ google-beta/provider.go
@@ -801,9 +801,9 @@ func Provider() *schema.Provider {
 	return provider
 }
 
-// Generated resources: 333
+// Generated resources: 334
 // Generated IAM resources: 219
-// Total generated resources: 552
+// Total generated resources: 553
 func ResourceMap() map[string]*schema.Resource {
 	resourceMap, _ := ResourceMapWithErrors()
 	return resourceMap
@@ -1164,6 +1164,7 @@ func ResourceMapWithErrors() (map[string]*schema.Resource, error) {
 			"google_gke_hub_membership_iam_member":                         ResourceIamMember(GKEHubMembershipIamSchema, GKEHubMembershipIamUpdaterProducer, GKEHubMembershipIdParseFunc),
 			"google_gke_hub_membership_iam_policy":                         ResourceIamPolicy(GKEHubMembershipIamSchema, GKEHubMembershipIamUpdaterProducer, GKEHubMembershipIdParseFunc),
 			"google_gkeonprem_vmware_cluster":                              ResourceGkeonpremVmwareCluster(),
+			"google_gkeonprem_vmware_node_pool":                            ResourceGkeonpremVmwareNodePool(),
 			"google_healthcare_consent_store":                              ResourceHealthcareConsentStore(),
 			"google_healthcare_consent_store_iam_binding":                  ResourceIamBinding(HealthcareConsentStoreIamSchema, HealthcareConsentStoreIamUpdaterProducer, HealthcareConsentStoreIdParseFunc),
 			"google_healthcare_consent_store_iam_member":                   ResourceIamMember(HealthcareConsentStoreIamSchema, HealthcareConsentStoreIamUpdaterProducer, HealthcareConsentStoreIdParseFunc),
