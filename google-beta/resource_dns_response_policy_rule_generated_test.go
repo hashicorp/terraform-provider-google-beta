967af6a7aaa620375e0df6b2f78a268c4aab6451
Modular Magician <magic-modules@google.com>
2023-07-12 15:27:37 +0000

    Promote google_dns_response_policy & google_dns_response_policy_rule to GA (#8308)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_dns_response_policy_rule_generated_test.go
+++ google-beta/resource_dns_response_policy_rule_generated_test.go
@@ -39,7 +39,7 @@ func TestAccDNSResponsePolicyRule_dnsResponsePolicyRuleBasicExample(t *testing.T
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		CheckDestroy:             testAccCheckDNSResponsePolicyRuleDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
@@ -58,24 +58,18 @@ func TestAccDNSResponsePolicyRule_dnsResponsePolicyRuleBasicExample(t *testing.T
 func testAccDNSResponsePolicyRule_dnsResponsePolicyRuleBasicExample(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 resource "google_compute_network" "network-1" {
-  provider = google-beta
-
   name                    = "tf-test-network-1%{random_suffix}"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_network" "network-2" {
-  provider = google-beta
-  
   name                    = "tf-test-network-2%{random_suffix}"
   auto_create_subnetworks = false
 }
 
 resource "google_dns_response_policy" "response-policy" {
-  provider = google-beta
-
   response_policy_name = "tf-test-example-response-policy%{random_suffix}"
-  
+
   networks {
     network_url = google_compute_network.network-1.id
   }
@@ -85,8 +79,6 @@ resource "google_dns_response_policy" "response-policy" {
 }
 
 resource "google_dns_response_policy_rule" "example-response-policy-rule" {
-  provider = google-beta
-
   response_policy = google_dns_response_policy.response-policy.response_policy_name
   rule_name       = "tf-test-example-rule%{random_suffix}"
   dns_name        = "dns.example.com."
@@ -98,7 +90,7 @@ resource "google_dns_response_policy_rule" "example-response-policy-rule" {
       ttl     = 300
       rrdatas = ["192.0.2.91"]
     }
-  }  
+  }
 
 }
 `, context)
