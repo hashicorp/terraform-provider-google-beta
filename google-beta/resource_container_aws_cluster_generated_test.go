1d6502254ca36feb594cd5d710e978bc429ac8b8
Modular Magician <magic-modules@google.com>
2023-07-13 23:01:06 +0000

    Revert "Generate DCL tests to service packages (#8257)" (#8340)
    This reverts commit c6ecf9c6afba568ac50d9c0923f268e4c0461c54.
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/resource_container_aws_cluster_generated_test.go
+++ google-beta/resource_container_aws_cluster_generated_test.go
@@ -16,7 +16,7 @@
 //
 // ----------------------------------------------------------------------------
 
-package containeraws_test
+package google
 
 import (
 	"context"
@@ -48,12 +48,12 @@ func TestAccContainerAwsCluster_BasicHandWritten(t *testing.T) {
 		"project_name":   envvar.GetTestProjectFromEnv(),
 		"project_number": envvar.GetTestProjectNumberFromEnv(),
 		"service_acct":   envvar.GetTestServiceAccountFromEnv(t),
-		"random_suffix":  acctest.RandString(t, 10),
+		"random_suffix":  RandString(t, 10),
 	}
 
-	acctest.VcrTest(t, resource.TestCase{
+	VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		ProtoV5ProviderFactories: ProtoV5ProviderFactories(t),
 		CheckDestroy:             testAccCheckContainerAwsClusterDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
@@ -92,13 +92,13 @@ func TestAccContainerAwsCluster_BetaBasicHandWritten(t *testing.T) {
 		"project_name":   envvar.GetTestProjectFromEnv(),
 		"project_number": envvar.GetTestProjectNumberFromEnv(),
 		"service_acct":   envvar.GetTestServiceAccountFromEnv(t),
-		"random_suffix":  acctest.RandString(t, 10),
+		"random_suffix":  RandString(t, 10),
 	}
 
-	acctest.VcrTest(t, resource.TestCase{
+	VcrTest(t, resource.TestCase{
 		PreCheck: func() { acctest.AccTestPreCheck(t) },
 
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		ProtoV5ProviderFactories: ProtoV5ProviderBetaFactories(t),
 		CheckDestroy:             testAccCheckContainerAwsClusterDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
@@ -519,7 +519,7 @@ func testAccCheckContainerAwsClusterDestroyProducer(t *testing.T) func(s *terraf
 				continue
 			}
 
-			config := acctest.GoogleProviderConfig(t)
+			config := GoogleProviderConfig(t)
 
 			billingProject := ""
 			if config.BillingProject != "" {
