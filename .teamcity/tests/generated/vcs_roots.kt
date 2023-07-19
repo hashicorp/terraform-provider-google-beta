565b5558b1d24bdee41d0ffba862c8131d4be500
The Magician <magic-modules@google.com>
2023-07-19 14:36:55 -0700

    Stop creating new, second VCS Root. Instead use a single manually-created root in the whole project. (#8394) (#5917)
    * Stop creating new VCS Root, instead use manually-created root made to create original TeamCity/GitHub connection
    
    * Remove vcs_root.erb
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- .teamcity/tests/generated/vcs_roots.kt
+++ .teamcity/tests/generated/vcs_roots.kt
@@ -9,7 +9,7 @@ import org.junit.Test
 class VcsTests {
     @Test
     fun buildsHaveCleanCheckOut() {
-        val project = GoogleBeta("public", TestConfiguration())
+        val project = GoogleBeta("public",  TestVcsRootId(), TestConfiguration())
         project.buildTypes.forEach { bt ->
             assertTrue("Build '${bt.id}' doesn't use clean checkout", bt.vcs.cleanCheckout)
         }
