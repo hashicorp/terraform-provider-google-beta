9e6879805c5fa035ef50fe2ffcaae1bfc47a3746
The Magician <magic-modules@google.com>
2023-08-10 16:44:25 -0700

    Merge pull request #8599 from GoogleCloudPlatform/main_sync (#6049)
    Sync 5.0.0 with main
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- .teamcity/tests/generated/vcs_roots.kt
+++ .teamcity/tests/generated/vcs_roots.kt
@@ -14,7 +14,7 @@ import org.junit.Test
 class VcsTests {
     @Test
     fun buildsHaveCleanCheckOut() {
-        val project = GoogleBeta("public",  TestVcsRootId(), "refs/heads/main", TestConfiguration())
+        val project = GoogleBeta("default",  TestVcsRootId(), "refs/heads/main", TestConfiguration())
         project.buildTypes.forEach { bt ->
             assertTrue("Build '${bt.id}' doesn't use clean checkout", bt.vcs.cleanCheckout)
         }
