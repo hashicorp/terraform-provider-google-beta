7d07bc661fe7501be3000dc8f60350834704fdf1
Modular Magician <magic-modules@google.com>
2023-08-18 22:27:47 +0000

    adding security_policy field to TargetInstance (#8357)
    * adding security_policy field to TargetInstance
    
    * making sure the target_instance uses the same zone as instance
    
    * fixing test by adding ddos protection policy rule
    
    * fixing review issues
    
    * making securityPolicy field updatable and add hw test for it
    
    * adding lifecyle block and context var for hw test
    
    * separating regions for tests
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_target_instance_generated_test.go
+++ google-beta/services/compute/resource_compute_target_instance_generated_test.go
@@ -149,6 +149,114 @@ resource "google_compute_instance" "target-vm" {
 `, context)
 }
 
+func TestAccComputeTargetInstance_targetInstanceWithSecurityPolicyExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckComputeTargetInstanceDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeTargetInstance_targetInstanceWithSecurityPolicyExample(context),
+			},
+			{
+				ResourceName:            "google_compute_target_instance.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"instance", "zone"},
+			},
+		},
+	})
+}
+
+func testAccComputeTargetInstance_targetInstanceWithSecurityPolicyExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_network" "default" {
+  provider                = google-beta
+  name                    = "tf-test-custom-default-network%{random_suffix}"
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+}
+      
+resource "google_compute_subnetwork" "default" {
+  provider                   = google-beta
+  name                       = "tf-test-custom-default-subnet%{random_suffix}"
+  ip_cidr_range              = "10.1.2.0/24"
+  network                    = google_compute_network.default.id
+  private_ipv6_google_access = "DISABLE_GOOGLE_ACCESS"
+  purpose                    = "PRIVATE"
+  region                     = "southamerica-west1"
+  stack_type                 = "IPV4_ONLY"
+}
+
+data "google_compute_image" "vmimage" {
+  provider = google-beta
+  family   = "debian-11"
+  project  = "debian-cloud"
+}
+
+resource "google_compute_instance" "target-vm" {
+  provider     = google-beta
+  name         = "tf-test-target-vm%{random_suffix}"
+  machine_type = "e2-medium"
+  zone         = "southamerica-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.vmimage.self_link
+    }
+  }
+
+  network_interface {       
+    network = google_compute_network.default.self_link
+    subnetwork = google_compute_subnetwork.default.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_region_security_policy" "policyddosprotection" {
+  provider    = google-beta
+  region      = "southamerica-west1"
+  name        = "tf-test-policyddos%{random_suffix}"
+  description = "ddos protection security policy to set target instance"
+  type        = "CLOUD_ARMOR_NETWORK"
+  ddos_protection_config {
+    ddos_protection = "ADVANCED_PREVIEW"
+  }
+}
+
+resource "google_compute_network_edge_security_service" "edge_sec_service" {
+  provider        = google-beta
+  region          = "southamerica-west1"
+  name            = "tf-test-edgesec%{random_suffix}"
+  security_policy = google_compute_region_security_policy.policyddosprotection.self_link
+}
+
+resource "google_compute_region_security_policy" "regionsecuritypolicy" {
+  provider    = google-beta
+  name        = "tf-test-region-secpolicy%{random_suffix}"
+  region      = "southamerica-west1"
+  description = "basic security policy for target instance"
+  type        = "CLOUD_ARMOR_NETWORK"
+  depends_on  = [google_compute_network_edge_security_service.edge_sec_service]
+}
+
+resource "google_compute_target_instance" "default" {
+  provider        = google-beta
+  name            = "tf-test-target-instance%{random_suffix}"
+  zone            = "southamerica-west1-a"
+  instance        = google_compute_instance.target-vm.id
+  security_policy = google_compute_region_security_policy.regionsecuritypolicy.self_link
+}
+`, context)
+}
+
 func testAccCheckComputeTargetInstanceDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
