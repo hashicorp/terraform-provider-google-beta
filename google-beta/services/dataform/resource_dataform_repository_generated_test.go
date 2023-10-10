a1a2224e629c88ff287a2e152c3ea8007cbf99aa
The Magician <magic-modules@google.com>
2023-10-10 19:07:26 -0400

    Dataform Repository Resource Adding requested fields (#9202) (#6480)
    * Dataform Repository Resource Adding Service Account and SshAuthenticationConfig
    
    * Adding separate test exmple file and enhancing descriptions for repository resource
    
    * Adding example in repository resource
    
    * Fixing test for Dataform Repository resource
    [upstream:95fe4c502e9f9aba11f71ec9c17ce1998cedbac8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/dataform/resource_dataform_repository_generated_test.go
+++ google-beta/services/dataform/resource_dataform_repository_generated_test.go
@@ -97,6 +97,78 @@ resource "google_dataform_repository" "dataform_respository" {
 `, context)
 }
 
+func TestAccDataformRepository_dataformRepositorySshExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckDataformRepositoryDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataformRepository_dataformRepositorySshExample(context),
+			},
+			{
+				ResourceName:            "google_dataform_repository.dataform_respository",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"region"},
+			},
+		},
+	})
+}
+
+func testAccDataformRepository_dataformRepositorySshExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_sourcerepo_repository" "git_repository" {
+  provider = google-beta
+  name = "my/repository%{random_suffix}"
+}
+
+resource "google_secret_manager_secret" "secret" {
+  provider = google-beta
+  secret_id = "secret"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "secret_version" {
+  provider = google-beta
+  secret = google_secret_manager_secret.secret.id
+
+  secret_data = "tf-test-secret-data%{random_suffix}"
+}
+
+resource "google_dataform_repository" "dataform_respository" {
+  provider = google-beta
+  name = "tf_test_dataform_repository%{random_suffix}"
+
+  git_remote_settings {
+      url = google_sourcerepo_repository.git_repository.url
+      default_branch = "main"
+      ssh_authentication_config {
+        user_private_key_secret_version = google_secret_manager_secret_version.secret_version.id
+        host_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAklOUpkDHrfHY17SbrmTIpNLTGK9Tjom/BWDSU"
+      }
+  }
+
+  workspace_compilation_overrides {
+    default_database = "database"
+    schema_suffix = "_suffix"
+    table_prefix = "prefix_"
+  }
+
+  service_account = "1234567890-compute@developer.gserviceaccount.com"
+}
+`, context)
+}
+
 func testAccCheckDataformRepositoryDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
