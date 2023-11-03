47e467fc543d54a2f9d3c78074c8460739f1a1f1
The Magician <magic-modules@google.com>
2023-11-03 08:50:41 -0700

    Enabling management of default Dialogflow resources without terraform import: `is_default_x` fields (#9336) (#6600)
    * Enabling management of Default Start Flow + Default Welcome Intent + Default Negative Intent, without terraform import
    
    Fixes https://github.com/hashicorp/terraform-provider-google/issues/16308
    
    * Basic tests for default flow + intents
    
    * Tests asserting resource IDs
    
    * Making Flow + Intent import respect is_default_X; fixing description on examples
    
    * Bah, making isFallback output-only might break users that'd imported the Default Negative Intent :(
    
    * Update mmv1/templates/terraform/pre_create/dialogflowcx_set_location_skip_default_obj.go.erb
    
    
    
    * Adding callout notes about multiple is_default_x resources
    
    * More words around the pre-create + pre-delete code
    
    * Adding more tests around Default Start Flow
    
    * Adding more tests around Default Welcome Intent and Default Negative Intent
    
    ---------
    
    
    [upstream:58d0735cfa46a3668a0c5af3181ce6443ab22d91]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/firebasehosting/resource_firebase_hosting_custom_domain.go
+++ google-beta/services/firebasehosting/resource_firebase_hosting_custom_domain.go
@@ -528,6 +528,7 @@ let Hosting serve secure content on its behalf.`,
 			"wait_dns_verification": {
 				Type:     schema.TypeBool,
 				Optional: true,
+				ForceNew: true,
 				Default:  false,
 				Description: `If true, Terraform will wait for DNS records to be fully resolved on the 'CustomDomain'.
 If false, Terraform will not wait for DNS records on the 'CustomDomain'. Any issues in
