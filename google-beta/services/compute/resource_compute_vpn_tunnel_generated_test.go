f35541f7020bfb340e74aff25a7bb091ecbd3229
The Magician <magic-modules@google.com>
2023-11-13 03:42:37 -0800

    Promote `labels` field on `google_compute_vpn_tunnel` to GA (#9441) (#6629)
    * Promote `labels` field on `google_compute_vpn_tunnel` to GA
    
    * Update GA vpn tunnel generated tests to include labels, remove beta-specific tests
    [upstream:e80655649171a7fbe99ae7d251ec09bda6ab91f5]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_vpn_tunnel_generated_test.go
+++ google-beta/services/compute/resource_compute_vpn_tunnel_generated_test.go
@@ -69,6 +69,10 @@ resource "google_compute_vpn_tunnel" "tunnel1" {
     google_compute_forwarding_rule.fr_udp500,
     google_compute_forwarding_rule.fr_udp4500,
   ]
+
+  labels = {
+    foo = "bar"
+  }
 }
 
 resource "google_compute_vpn_gateway" "target_gateway" {
@@ -114,114 +118,6 @@ resource "google_compute_route" "route1" {
   priority   = 1000
 
   next_hop_vpn_tunnel = google_compute_vpn_tunnel.tunnel1.id
-}
-`, context)
-}
-
-func TestAccComputeVpnTunnel_vpnTunnelBetaExample(t *testing.T) {
-	t.Parallel()
-
-	context := map[string]interface{}{
-		"provider_name":  "google-beta.us-central1",
-		"provider_alias": "alias  = \"us-central1\"",
-		"random_suffix":  acctest.RandString(t, 10),
-	}
-
-	acctest.VcrTest(t, resource.TestCase{
-		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
-		CheckDestroy:             testAccCheckComputeVpnTunnelDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccComputeVpnTunnel_vpnTunnelBetaExample(context),
-			},
-			{
-				ResourceName:            "google_compute_vpn_tunnel.tunnel1",
-				ImportState:             true,
-				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"target_vpn_gateway", "vpn_gateway", "peer_external_gateway", "peer_gcp_gateway", "router", "shared_secret", "region", "labels", "terraform_labels"},
-			},
-		},
-	})
-}
-
-func testAccComputeVpnTunnel_vpnTunnelBetaExample(context map[string]interface{}) string {
-	return acctest.Nprintf(`
-resource "google_compute_vpn_tunnel" "tunnel1" {
-  provider      = %{provider_name}
-  name          = "tf-test-tunnel-1%{random_suffix}"
-  peer_ip       = "15.0.0.120"
-  shared_secret = "a secret message"
-
-  target_vpn_gateway = google_compute_vpn_gateway.target_gateway.id
-
-  depends_on = [
-    google_compute_forwarding_rule.fr_esp,
-    google_compute_forwarding_rule.fr_udp500,
-    google_compute_forwarding_rule.fr_udp4500,
-  ]
-
-  labels = {
-    foo = "bar"
-  }
-}
-
-resource "google_compute_vpn_gateway" "target_gateway" {
-  provider = %{provider_name}
-  name     = "tf-test-vpn-1%{random_suffix}"
-  network  = google_compute_network.network1.id
-}
-
-resource "google_compute_network" "network1" {
-  provider = %{provider_name}
-  name     = "tf-test-network-1%{random_suffix}"
-}
-
-resource "google_compute_address" "vpn_static_ip" {
-  provider = %{provider_name}
-  name     = "tf-test-vpn-static-ip%{random_suffix}"
-}
-
-resource "google_compute_forwarding_rule" "fr_esp" {
-  provider    = %{provider_name}
-  name        = "tf-test-fr-esp%{random_suffix}"
-  ip_protocol = "ESP"
-  ip_address  = google_compute_address.vpn_static_ip.address
-  target      = google_compute_vpn_gateway.target_gateway.id
-}
-
-resource "google_compute_forwarding_rule" "fr_udp500" {
-  provider    = %{provider_name}
-  name        = "tf-test-fr-udp500%{random_suffix}"
-  ip_protocol = "UDP"
-  port_range  = "500"
-  ip_address  = google_compute_address.vpn_static_ip.address
-  target      = google_compute_vpn_gateway.target_gateway.id
-}
-
-resource "google_compute_forwarding_rule" "fr_udp4500" {
-  provider    = %{provider_name}
-  name        = "tf-test-fr-udp4500%{random_suffix}"
-  ip_protocol = "UDP"
-  port_range  = "4500"
-  ip_address  = google_compute_address.vpn_static_ip.address
-  target      = google_compute_vpn_gateway.target_gateway.id
-}
-
-resource "google_compute_route" "route1" {
-  provider   = %{provider_name}
-  name       = "route1%{random_suffix}"
-  network    = google_compute_network.network1.name
-  dest_range = "15.0.0.0/24"
-  priority   = 1000
-
-  next_hop_vpn_tunnel = google_compute_vpn_tunnel.tunnel1.id
-}
-
-provider "google-beta" {
-  region = "us-central1"
-  zone   = "us-central1-a"
-  %{provider_alias}
 }
 `, context)
 }
