1abeee76527389a4b72a066fbcd6a68a8e0f48e1
Modular Magician <magic-modules@google.com>
2023-11-20 20:24:16 +0000

    Update `SKIP_PROJECT_SWEEPER` value in TeamCity configuration files (#9500) [upstream:337e6ac22304a75cf0c1dfe0f7a7fa68fec4ba0e]
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- .teamcity/components/generated/project.kt
+++ .teamcity/components/generated/project.kt
@@ -62,7 +62,7 @@ fun GoogleBeta(environment: String, projDescription: String, manualVcsRoot: Abso
             // Controls the VCS root, and allows custom builds to point at different branches
             param("BRANCH_NAME", branchRef)
             // Skip the sweeper for project resources in the Beta tests only
-            param("env.SKIP_PROJECT_SWEEPER", 1)
+            param("env.SKIP_PROJECT_SWEEPER", "1")
             // Not used, but making `environment` a param makes the value visible to non-admins in TeamCity
             param("ENVIRONMENT", environment)
         }
