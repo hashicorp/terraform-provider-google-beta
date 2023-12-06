1c91f24284a8947e08cb64bb98eac13a1983e14c
The Magician <magic-modules@google.com>
2023-12-06 11:18:55 -0800

    Add Subnet resource to Vmwareengine (#9560) (#6715)
    [upstream:3547ac8d589d09f0b66f4c1fe3e1dfd9783c87d0]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -295,6 +295,7 @@ var handwrittenDatasources = map[string]*schema.Resource{
 	"google_vmwareengine_network_policy":                  vmwareengine.DataSourceVmwareengineNetworkPolicy(),
 	"google_vmwareengine_nsx_credentials":                 vmwareengine.DataSourceVmwareengineNsxCredentials(),
 	"google_vmwareengine_private_cloud":                   vmwareengine.DataSourceVmwareenginePrivateCloud(),
+	"google_vmwareengine_subnet":                          vmwareengine.DataSourceVmwareengineSubnet(),
 
 	// ####### END handwritten datasources ###########
 }
@@ -412,9 +413,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 402
+// Generated resources: 403
 // Generated IAM resources: 249
-// Total generated resources: 651
+// Total generated resources: 652
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                         accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                   accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1056,6 +1057,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_vmwareengine_network_peering":                            vmwareengine.ResourceVmwareengineNetworkPeering(),
 	"google_vmwareengine_network_policy":                             vmwareengine.ResourceVmwareengineNetworkPolicy(),
 	"google_vmwareengine_private_cloud":                              vmwareengine.ResourceVmwareenginePrivateCloud(),
+	"google_vmwareengine_subnet":                                     vmwareengine.ResourceVmwareengineSubnet(),
 	"google_vpc_access_connector":                                    vpcaccess.ResourceVPCAccessConnector(),
 	"google_workflows_workflow":                                      workflows.ResourceWorkflowsWorkflow(),
 	"google_workstations_workstation":                                workstations.ResourceWorkstationsWorkstation(),
