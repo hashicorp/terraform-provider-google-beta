6e5ca257274bc3f2807e4df2df348b869fccf6d3
Modular Magician <magic-modules@google.com>
2023-12-18 21:26:45 +0000

    Add missing IAM resources dependencies in `TestAccComputeRegionInstanceTemplate_sourceImageEncryptionKey` (#9644) [upstream:0fb627b59276d60c2f80fb050129de3d7e0c337f]
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_region_instance_template_test.go
+++ google-beta/services/compute/resource_compute_region_instance_template_test.go
@@ -3231,6 +3231,10 @@ resource "google_compute_snapshot" "snapshot" {
     kms_key_self_link       = data.google_kms_crypto_key.key.id
     kms_key_service_account = google_service_account.test.email
   }
+
+  depends_on = [
+    google_kms_crypto_key_iam_member.crypto_key
+  ]
 }
 
 resource "google_compute_region_instance_template" "template" {
@@ -3251,6 +3255,10 @@ resource "google_compute_region_instance_template" "template" {
   network_interface {
     network = "default"
   }
+
+  depends_on = [
+    google_kms_crypto_key_iam_member.crypto_key
+  ]
 }
 `, context)
 }
@@ -3290,6 +3298,10 @@ resource "google_compute_image" "image" {
     kms_key_self_link       = data.google_kms_crypto_key.key.id
     kms_key_service_account = google_service_account.test.email
   }
+
+  depends_on = [
+    google_kms_crypto_key_iam_member.crypto_key
+  ]
 }
 
 
@@ -3311,6 +3323,10 @@ resource "google_compute_region_instance_template" "template" {
   network_interface {
     network = "default"
   }
+
+  depends_on = [
+    google_kms_crypto_key_iam_member.crypto_key
+  ]
 }
 `, context)
 }
