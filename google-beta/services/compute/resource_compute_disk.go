2f823676e43dc00052ae703557abec6e6ffb01c2
Modular Magician <magic-modules@google.com>
2024-03-08 20:03:18 +0000

    Move pre_delete code closer to the request (#10143)
    [upstream:b3dd24768e1709f305efff76c197501e7f31ab44]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_disk.go
+++ google-beta/services/compute/resource_compute_disk.go
@@ -1380,6 +1380,12 @@ func resourceComputeDiskDelete(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	var obj map[string]interface{}
+
+	// err == nil indicates that the billing_project value was found
+	if bp, err := tpgresource.GetBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+
 	readRes, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
 		Config:    config,
 		Method:    "GET",
@@ -1440,11 +1446,6 @@ func resourceComputeDiskDelete(d *schema.ResourceData, meta interface{}) error {
 				return err
 			}
 		}
-	}
-
-	// err == nil indicates that the billing_project value was found
-	if bp, err := tpgresource.GetBillingProject(d, config); err == nil {
-		billingProject = bp
 	}
 
 	log.Printf("[DEBUG] Deleting Disk %q", d.Id())
