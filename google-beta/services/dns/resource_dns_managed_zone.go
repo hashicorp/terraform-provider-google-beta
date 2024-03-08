7895ed4dcaa33e62b41004f46034261a503db317
The Magician <magic-modules@google.com>
2024-03-08 12:03:29 -0800

    Move pre_delete code closer to the request (#10143) (#7071)
    [upstream:b3dd24768e1709f305efff76c197501e7f31ab44]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/dns/resource_dns_managed_zone.go
+++ google-beta/services/dns/resource_dns_managed_zone.go
@@ -792,6 +792,12 @@ func resourceDNSManagedZoneDelete(d *schema.ResourceData, meta interface{}) erro
 	}
 
 	var obj map[string]interface{}
+
+	// err == nil indicates that the billing_project value was found
+	if bp, err := tpgresource.GetBillingProject(d, config); err == nil {
+		billingProject = bp
+	}
+
 	if d.Get("force_destroy").(bool) {
 		zone := d.Get("name").(string)
 		token := ""
@@ -861,11 +867,6 @@ func resourceDNSManagedZoneDelete(d *schema.ResourceData, meta interface{}) erro
 			token = resp.NextPageToken
 			paginate = token != ""
 		}
-	}
-
-	// err == nil indicates that the billing_project value was found
-	if bp, err := tpgresource.GetBillingProject(d, config); err == nil {
-		billingProject = bp
 	}
 
 	log.Printf("[DEBUG] Deleting ManagedZone %q", d.Id())
