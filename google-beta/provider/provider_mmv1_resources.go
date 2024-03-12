95156095fd0ef0ccbe9528eef003ea3540fad95d
The Magician <magic-modules@google.com>
2024-03-12 10:10:27 -0700

    Add data source for AppHub discovered workload (#10107) (#7083)
    * Add data source for Apphub discovered workload
    
    * Add data source for Apphub discovered workload
    
    * Add data source for Apphub Discovered Workload
    
    * Resolved comments
    
    * Resolved comments
    
    * Resolved comments
    
    * Resolved comments
    
    * Add tests and documentation for data source discovered workload
    
    * Resolved comments and added tests
    
    * Resolved comments
    
    * Added modifications in tests, data source and documentation
    
    * Added modifications in tests, data source and documentation
    
    * Added modifications in tests, data source and documentation
    
    * Verified tests
    
    * Tests Verification
    
    * Tests Verification
    
    * Updated logic to obtain workload_uri
    
    * Updated logic to obtain workload_uri
    
    * Resolved comments
    
    * Resolved comments
    
    * Add billing account and shorten service project name
    
    * Add header
    
    * Change the project name to start with tf-test
    
    * Resolved comments
    
    * Lint changes
    
    * Removing two sleeps
    
    * Removing two sleeps
    
    * Modifying documentation
    
    * Resolved tests
    
    * Resolved comments
    
    * Resolved comments
    
    * logic modification
    
    * Update Retry logic
    
    * Update Retry logic
    
    * Resolved tests
    
    * Adding workload_uri description
    
    * Adding workload_uri description
    
    [upstream:a6ceb9e3c35210af4ecfaee801958eb7b79ac377]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -151,6 +151,7 @@ var handwrittenDatasources = map[string]*schema.Resource{
 	"google_alloydb_locations":                            alloydb.DataSourceAlloydbLocations(),
 	"google_alloydb_supported_database_flags":             alloydb.DataSourceAlloydbSupportedDatabaseFlags(),
 	"google_artifact_registry_repository":                 artifactregistry.DataSourceArtifactRegistryRepository(),
+	"google_apphub_discovered_workload":                   apphub.DataSourceApphubDiscoveredWorkload(),
 	"google_app_engine_default_service_account":           appengine.DataSourceGoogleAppEngineDefaultServiceAccount(),
 	"google_apphub_discovered_service":                    apphub.DataSourceApphubDiscoveredService(),
 	"google_backup_dr_management_server":                  backupdr.DataSourceGoogleCloudBackupDRService(),
