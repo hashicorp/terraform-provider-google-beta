afcb54a41888eef0ed9da12da6652ecf42913b2a
The Magician <magic-modules@google.com>
2024-04-23 10:42:47 -0700

    add serverTLSpolicy to google_compute_region_target_https_proxy (#9105) (#7280)
    [upstream:a9430683005fe7908171c8e537eb929985964f48]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_region_target_https_proxy_generated_test.go
+++ google-beta/services/compute/resource_compute_region_target_https_proxy_generated_test.go
@@ -49,7 +49,7 @@ func TestAccComputeRegionTargetHttpsProxy_regionTargetHttpsProxyBasicExample(t *
 				ResourceName:            "google_compute_region_target_https_proxy.default",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"ssl_policy", "url_map", "region"},
+				ImportStateVerifyIgnore: []string{"ssl_policy", "url_map", "server_tls_policy", "region"},
 			},
 		},
 	})
@@ -114,6 +114,137 @@ resource "google_compute_region_health_check" "default" {
 `, context)
 }
 
+func TestAccComputeRegionTargetHttpsProxy_regionTargetHttpsProxyMtlsExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckComputeRegionTargetHttpsProxyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeRegionTargetHttpsProxy_regionTargetHttpsProxyMtlsExample(context),
+			},
+			{
+				ResourceName:            "google_compute_region_target_https_proxy.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"ssl_policy", "url_map", "server_tls_policy", "region"},
+			},
+		},
+	})
+}
+
+func testAccComputeRegionTargetHttpsProxy_regionTargetHttpsProxyMtlsExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_project" "project" {
+  provider = google-beta
+}
+
+resource "google_compute_region_target_https_proxy" "default" {
+  provider          = google-beta
+  region           = "us-central1"
+  name              = "tf-test-test-mtls-proxy%{random_suffix}"
+  url_map           = google_compute_region_url_map.default.id
+  ssl_certificates  = [google_compute_region_ssl_certificate.default.id]
+  server_tls_policy = google_network_security_server_tls_policy.default.id
+}
+
+resource "google_certificate_manager_trust_config" "default" {
+  provider    = google-beta
+  location    = "us-central1"
+  name        = "tf-test-my-trust-config%{random_suffix}"
+  description = "sample description for trust config"
+
+  trust_stores {
+    trust_anchors {
+      pem_certificate = file("test-fixtures/ca_cert.pem")
+    }
+    intermediate_cas {
+      pem_certificate = file("test-fixtures/ca_cert.pem")
+    }
+  }
+
+  labels = {
+    foo = "bar"
+  }
+}
+
+resource "google_network_security_server_tls_policy" "default" {
+  provider               = google-beta
+  location               = "us-central1"
+  name                   = "tf-test-my-tls-policy%{random_suffix}"
+  description            = "my description"
+  allow_open             = "false"
+  mtls_policy {
+    client_validation_mode = "REJECT_INVALID"
+    client_validation_trust_config = "projects/${data.google_project.project.number}/locations/us-central1/trustConfigs/${google_certificate_manager_trust_config.default.name}"
+  }
+}
+
+resource "google_compute_region_ssl_certificate" "default" {
+  provider    = google-beta
+  region      = "us-central1"
+  name        = "tf-test-my-certificate%{random_suffix}"
+  private_key = file("test-fixtures/test.key")
+  certificate = file("test-fixtures/test.crt")
+}
+
+resource "google_compute_region_url_map" "default" {
+  provider    = google-beta
+  region      = "us-central1"
+  name        = "tf-test-url-map%{random_suffix}"
+  description = "a description"
+
+  default_service = google_compute_region_backend_service.default.id
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_region_backend_service.default.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_region_backend_service.default.id
+    }
+  }
+}
+
+resource "google_compute_region_backend_service" "default" {
+  provider    = google-beta
+  region      = "us-central1"
+  name        = "tf-test-backend-service%{random_suffix}"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  load_balancing_scheme = "INTERNAL_MANAGED"
+
+  health_checks = [google_compute_region_health_check.default.id]
+}
+
+resource "google_compute_region_health_check" "default" {
+  provider           = google-beta
+  region             = "us-central1"
+  name               = "tf-test-http-health-check%{random_suffix}"
+  check_interval_sec = 1
+  timeout_sec        = 1
+
+  http_health_check {
+    port = 80
+  }
+}
+`, context)
+}
+
 func TestAccComputeRegionTargetHttpsProxy_regionTargetHttpsProxyCertificateManagerCertificateExample(t *testing.T) {
 	t.Parallel()
 
@@ -133,7 +264,7 @@ func TestAccComputeRegionTargetHttpsProxy_regionTargetHttpsProxyCertificateManag
 				ResourceName:            "google_compute_region_target_https_proxy.default",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"ssl_policy", "url_map", "region"},
+				ImportStateVerifyIgnore: []string{"ssl_policy", "url_map", "server_tls_policy", "region"},
 			},
 		},
 	})
