1b7d3a3420869d3986123e2b10206b36cb4a05eb
Modular Magician <magic-modules@google.com>
2024-05-20 19:58:58 +0000

    Add lb route extension resource (#10119)
    Co-authored-by: pweiber <periclesweiber@ciandt.com>
    
    [upstream:0650b2ffcc13cfd0e7e784ed61631c04a785d3cf]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -454,9 +454,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 469
+// Generated resources: 470
 // Generated IAM resources: 270
-// Total generated resources: 739
+// Total generated resources: 740
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                           accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                     accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1036,6 +1036,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_network_services_gateway":                                  networkservices.ResourceNetworkServicesGateway(),
 	"google_network_services_grpc_route":                               networkservices.ResourceNetworkServicesGrpcRoute(),
 	"google_network_services_http_route":                               networkservices.ResourceNetworkServicesHttpRoute(),
+	"google_network_services_lb_route_extension":                       networkservices.ResourceNetworkServicesLbRouteExtension(),
 	"google_network_services_lb_traffic_extension":                     networkservices.ResourceNetworkServicesLbTrafficExtension(),
 	"google_network_services_mesh":                                     networkservices.ResourceNetworkServicesMesh(),
 	"google_network_services_service_binding":                          networkservices.ResourceNetworkServicesServiceBinding(),
