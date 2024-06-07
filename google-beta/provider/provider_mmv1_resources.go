3ee69250ae6a5b7ebf3b756a9537a7203f491a76
The Magician <magic-modules@google.com>
2024-06-07 10:02:51 -0700

    Add Resource Folder Security Health Analytics Custom Module (#10839) (#7483)
    [upstream:dd18002306d2f9d5fc4c062b3b1bc96e149d97a4]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -461,9 +461,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 479
+// Generated resources: 480
 // Generated IAM resources: 279
-// Total generated resources: 758
+// Total generated resources: 759
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                  accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                            accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1128,6 +1128,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_scc_source_iam_binding":                                           tpgiamresource.ResourceIamBinding(securitycenter.SecurityCenterSourceIamSchema, securitycenter.SecurityCenterSourceIamUpdaterProducer, securitycenter.SecurityCenterSourceIdParseFunc),
 	"google_scc_source_iam_member":                                            tpgiamresource.ResourceIamMember(securitycenter.SecurityCenterSourceIamSchema, securitycenter.SecurityCenterSourceIamUpdaterProducer, securitycenter.SecurityCenterSourceIdParseFunc),
 	"google_scc_source_iam_policy":                                            tpgiamresource.ResourceIamPolicy(securitycenter.SecurityCenterSourceIamSchema, securitycenter.SecurityCenterSourceIamUpdaterProducer, securitycenter.SecurityCenterSourceIdParseFunc),
+	"google_scc_management_folder_security_health_analytics_custom_module":    securitycentermanagement.ResourceSecurityCenterManagementFolderSecurityHealthAnalyticsCustomModule(),
 	"google_scc_management_organization_event_threat_detection_custom_module": securitycentermanagement.ResourceSecurityCenterManagementOrganizationEventThreatDetectionCustomModule(),
 	"google_securityposture_posture":                                          securityposture.ResourceSecurityposturePosture(),
 	"google_securityposture_posture_deployment":                               securityposture.ResourceSecurityposturePostureDeployment(),
