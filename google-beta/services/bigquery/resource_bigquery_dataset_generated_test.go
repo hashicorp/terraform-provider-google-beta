f361971cfd12282f8fcc04e3bedc203b86c3ee75
Modular Magician <magic-modules@google.com>
2024-06-17 13:30:47 +0000

    Add resource tags to BigQuery Dataset in the Beta provider (#10971)
    [upstream:bc527bd05d95340af8f53282d470651186279c97]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/bigquery/resource_bigquery_dataset_generated_test.go
+++ google-beta/services/bigquery/resource_bigquery_dataset_generated_test.go
@@ -451,6 +451,77 @@ resource "google_bigquery_dataset" "dataset" {
 `, context)
 }
 
+func TestAccBigQueryDataset_bigqueryDatasetResourceTagsExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckBigQueryDatasetDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccBigQueryDataset_bigqueryDatasetResourceTagsExample(context),
+			},
+			{
+				ResourceName:            "google_bigquery_dataset.dataset",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccBigQueryDataset_bigqueryDatasetResourceTagsExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_project" "project" {
+  provider = "google-beta"
+}
+
+resource "google_tags_tag_key" "tag_key1" {
+  provider = "google-beta"
+  parent = "projects/${data.google_project.project.number}"
+  short_name = "tf_test_tag_key1%{random_suffix}"
+}
+
+resource "google_tags_tag_value" "tag_value1" {
+  provider = "google-beta"
+  parent = "tagKeys/${google_tags_tag_key.tag_key1.name}"
+  short_name = "tf_test_tag_value1%{random_suffix}"
+}
+
+resource "google_tags_tag_key" "tag_key2" {
+  provider = "google-beta"
+  parent = "projects/${data.google_project.project.number}"
+  short_name = "tf_test_tag_key2%{random_suffix}"
+}
+
+resource "google_tags_tag_value" "tag_value2" {
+  provider = "google-beta"
+  parent = "tagKeys/${google_tags_tag_key.tag_key2.name}"
+  short_name = "tf_test_tag_value2%{random_suffix}"
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  provider = google-beta
+
+  dataset_id                  = "dataset%{random_suffix}"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+
+  resource_tags = {
+    "${data.google_project.project.project_id}/${google_tags_tag_key.tag_key1.short_name}" = "${google_tags_tag_value.tag_value1.short_name}"
+    "${data.google_project.project.project_id}/${google_tags_tag_key.tag_key2.short_name}" = "${google_tags_tag_value.tag_value2.short_name}"
+  }
+}
+`, context)
+}
+
 func testAccCheckBigQueryDatasetDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
