d9b504257728bc2d6a564816e6232a31603c55fa
Modular Magician <magic-modules@google.com>
2024-06-27 17:03:16 +0000

    Migrate BigQuery Reservation Assignment from DCL to MMv1 (#10894)
    [upstream:68982811939bc750fb567c339c80b52b67ae569f]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_dcl_resources.go
+++ google-beta/provider/provider_dcl_resources.go
@@ -22,7 +22,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/apikeys"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/assuredworkloads"
-	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/bigqueryreservation"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudbuild"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/clouddeploy"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/compute"
@@ -41,7 +40,6 @@ import (
 var dclResources = map[string]*schema.Resource{
 	"google_apikeys_key":                                        apikeys.ResourceApikeysKey(),
 	"google_assured_workloads_workload":                         assuredworkloads.ResourceAssuredWorkloadsWorkload(),
-	"google_bigquery_reservation_assignment":                    bigqueryreservation.ResourceBigqueryReservationAssignment(),
 	"google_cloudbuild_worker_pool":                             cloudbuild.ResourceCloudbuildWorkerPool(),
 	"google_clouddeploy_delivery_pipeline":                      clouddeploy.ResourceClouddeployDeliveryPipeline(),
 	"google_clouddeploy_target":                                 clouddeploy.ResourceClouddeployTarget(),
