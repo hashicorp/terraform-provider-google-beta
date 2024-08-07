99ebc56a91c5b444e2596c30b5a5fd7300bfa6cc
Modular Magician <magic-modules@google.com>
2024-08-07 18:01:18 +0000

    Updated test configurations to use confidential instances (#11376)
    [upstream:f42a18f6e393189141e3aaece50615ed25d0bf67]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/container/resource_container_node_pool_test.go
+++ google-beta/services/container/resource_container_node_pool_test.go
@@ -4305,6 +4305,9 @@ resource "google_container_node_pool" "with_confidential_boot_disk" {
   cluster            = google_container_cluster.cluster.name
 
   node_config {
+    confidential_nodes {
+      enabled = true
+    }
     image_type = "COS_CONTAINERD"
     boot_disk_kms_key = "%s"
     oauth_scopes = [
@@ -4312,7 +4315,7 @@ resource "google_container_node_pool" "with_confidential_boot_disk" {
       "https://www.googleapis.com/auth/monitoring",
     ]
     enable_confidential_storage = true
-    machine_type = "n2-standard-2"
+    machine_type = "n2d-standard-2"
     disk_type = "hyperdisk-balanced"
   }
 }
