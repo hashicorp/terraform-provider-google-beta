48906d951026c286f25bdf0c24bd7e995f302a9e
Modular Magician <magic-modules@google.com>
2024-09-09 22:37:40 +0000

    Add post create 5s sleep to Topic creation (#11665)
    [upstream:795af5ad00f5ca65530acdaa5e75c7eef64719fe]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/managedkafka/resource_managed_kafka_topic.go
+++ google-beta/services/managedkafka/resource_managed_kafka_topic.go
@@ -176,6 +176,11 @@ func resourceManagedKafkaTopicCreate(d *schema.ResourceData, meta interface{}) e
 	}
 	d.SetId(id)
 
+	// This is useful if the resource in question doesn't have a perfectly consistent API
+	// That is, the Operation for Create might return before the Get operation shows the
+	// completed state of the resource.
+	time.Sleep(5 * time.Second)
+
 	log.Printf("[DEBUG] Finished creating Topic %q: %#v", d.Id(), res)
 
 	return resourceManagedKafkaTopicRead(d, meta)
