bace576c5f7e993a85c653b38935bd183b6d82f5
The Magician <magic-modules@google.com>
2024-09-19 14:06:53 -0700

    Add new resource for creating Whistle Mapping, Reconciliation and Backfill Pipeline Jobs for Healthcare Data Engine  (#11677) (#8222)
    [upstream:1902bacf8e9c72befb6dfb29945bc5effb7355ab]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -485,9 +485,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 526
+// Generated resources: 527
 // Generated IAM resources: 291
-// Total generated resources: 817
+// Total generated resources: 818
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -976,6 +976,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_healthcare_dicom_store":                                              healthcare.ResourceHealthcareDicomStore(),
 	"google_healthcare_fhir_store":                                               healthcare.ResourceHealthcareFhirStore(),
 	"google_healthcare_hl7_v2_store":                                             healthcare.ResourceHealthcareHl7V2Store(),
+	"google_healthcare_pipeline_job":                                             healthcare.ResourceHealthcarePipelineJob(),
 	"google_healthcare_workspace":                                                healthcare.ResourceHealthcareWorkspace(),
 	"google_iam_access_boundary_policy":                                          iam2.ResourceIAM2AccessBoundaryPolicy(),
 	"google_iam_deny_policy":                                                     iam2.ResourceIAM2DenyPolicy(),
