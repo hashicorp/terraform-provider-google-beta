b99050cd55065972bb08d58de08a91a000677534
Modular Magician <magic-modules@google.com>
2024-09-20 18:34:14 +0000

    Adding a new cloud logging resource `LogScope` to Terraform (#11763)
    [upstream:ec8609a33e02287d6d6d90339938efc55865bc02]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -485,9 +485,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 529
+// Generated resources: 530
 // Generated IAM resources: 291
-// Total generated resources: 820
+// Total generated resources: 821
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1045,6 +1045,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_kms_secret_ciphertext":                                               kms.ResourceKMSSecretCiphertext(),
 	"google_logging_folder_settings":                                             logging.ResourceLoggingFolderSettings(),
 	"google_logging_linked_dataset":                                              logging.ResourceLoggingLinkedDataset(),
+	"google_logging_log_scope":                                                   logging.ResourceLoggingLogScope(),
 	"google_logging_log_view":                                                    logging.ResourceLoggingLogView(),
 	"google_logging_log_view_iam_binding":                                        tpgiamresource.ResourceIamBinding(logging.LoggingLogViewIamSchema, logging.LoggingLogViewIamUpdaterProducer, logging.LoggingLogViewIdParseFunc),
 	"google_logging_log_view_iam_member":                                         tpgiamresource.ResourceIamMember(logging.LoggingLogViewIamSchema, logging.LoggingLogViewIamUpdaterProducer, logging.LoggingLogViewIdParseFunc),
