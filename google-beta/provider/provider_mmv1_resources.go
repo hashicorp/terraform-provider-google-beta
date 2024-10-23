5870936c96675b170f0984b974328e562bf75994
The Magician <magic-modules@google.com>
2024-10-22 17:34:16 -0700

    Migrate Network Firewall Policy Association (global and region) resource from DCL to MMv1 (#11366) (#8489)
    [upstream:9a1402bf3fa39860cad1fb258010cbf01450e8d8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -496,9 +496,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 542
+// Generated resources: 544
 // Generated IAM resources: 291
-// Total generated resources: 833
+// Total generated resources: 835
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -733,6 +733,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_compute_network_endpoint_group":                                      compute.ResourceComputeNetworkEndpointGroup(),
 	"google_compute_network_endpoints":                                           compute.ResourceComputeNetworkEndpoints(),
 	"google_compute_network_firewall_policy":                                     compute.ResourceComputeNetworkFirewallPolicy(),
+	"google_compute_network_firewall_policy_association":                         compute.ResourceComputeNetworkFirewallPolicyAssociation(),
 	"google_compute_network_firewall_policy_rule":                                compute.ResourceComputeNetworkFirewallPolicyRule(),
 	"google_compute_network_firewall_policy_with_rules":                          compute.ResourceComputeNetworkFirewallPolicyWithRules(),
 	"google_compute_network_peering_routes_config":                               compute.ResourceComputeNetworkPeeringRoutesConfig(),
@@ -761,6 +762,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_compute_region_network_endpoint":                                     compute.ResourceComputeRegionNetworkEndpoint(),
 	"google_compute_region_network_endpoint_group":                               compute.ResourceComputeRegionNetworkEndpointGroup(),
 	"google_compute_region_network_firewall_policy":                              compute.ResourceComputeRegionNetworkFirewallPolicy(),
+	"google_compute_region_network_firewall_policy_association":                  compute.ResourceComputeRegionNetworkFirewallPolicyAssociation(),
 	"google_compute_region_network_firewall_policy_rule":                         compute.ResourceComputeRegionNetworkFirewallPolicyRule(),
 	"google_compute_region_network_firewall_policy_with_rules":                   compute.ResourceComputeRegionNetworkFirewallPolicyWithRules(),
 	"google_compute_region_per_instance_config":                                  compute.ResourceComputeRegionPerInstanceConfig(),
