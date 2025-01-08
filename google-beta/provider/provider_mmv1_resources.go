3035fc3e76a98a6a8bf581668a0aa415b81b36c7
Modular Magician <magic-modules@google.com>
2025-01-08 21:16:28 +0000

    Add edgenetwork InterconnectAttachment resource. (#12586)
    [upstream:6d54b5142d999bfee3a7570636c4cd322f54733b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -513,9 +513,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 579
+// Generated resources: 580
 // Generated IAM resources: 294
-// Total generated resources: 873
+// Total generated resources: 874
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -959,6 +959,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_edgecontainer_cluster":                                               edgecontainer.ResourceEdgecontainerCluster(),
 	"google_edgecontainer_node_pool":                                             edgecontainer.ResourceEdgecontainerNodePool(),
 	"google_edgecontainer_vpn_connection":                                        edgecontainer.ResourceEdgecontainerVpnConnection(),
+	"google_edgenetwork_interconnect_attachment":                                 edgenetwork.ResourceEdgenetworkInterconnectAttachment(),
 	"google_edgenetwork_network":                                                 edgenetwork.ResourceEdgenetworkNetwork(),
 	"google_edgenetwork_subnet":                                                  edgenetwork.ResourceEdgenetworkSubnet(),
 	"google_essential_contacts_contact":                                          essentialcontacts.ResourceEssentialContactsContact(),
