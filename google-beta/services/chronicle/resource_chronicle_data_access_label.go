fbf2f14df6dff751ba00bf0399f52340f3c8449b
The Magician <magic-modules@google.com>
2025-01-13 08:59:06 -0800

    Updated data_access_label_id description for google_chronicle_data_ac… (#12738) (#9045)
    [upstream:16b37875bc1fc09edf77f284f31bc5a2df9607e4]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/chronicle/resource_chronicle_data_access_label.go
+++ google-beta/services/chronicle/resource_chronicle_data_access_label.go
@@ -59,9 +59,9 @@ func ResourceChronicleDataAccessLabel() *schema.Resource {
 				Required: true,
 				ForceNew: true,
 				Description: `Required. The ID to use for the data access label, which will become the label's
-display name and the final component of the label's resource name. It must
-only contain ASCII lowercase letters, numbers, and dashes; it must begin
-with a letter, and it must not exceed 1000 characters.`,
+display name and the final component of the label's resource name. The
+maximum number of characters should be 63. Regex pattern is as per AIP:
+https://google.aip.dev/122#resource-id-segments`,
 			},
 			"instance": {
 				Type:        schema.TypeString,
