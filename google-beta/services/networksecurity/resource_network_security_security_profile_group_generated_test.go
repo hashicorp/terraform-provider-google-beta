80c94c5bff0162e781fa1e9b3d1867db2a874cd0
Modular Magician <magic-modules@google.com>
2025-01-22 00:51:17 +0000

    Support new Network Security Integration security profile types (#12816)
    [upstream:db31db4510175885374eaa1b051f64396103a02a]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/networksecurity/resource_network_security_security_profile_group_generated_test.go
+++ google-beta/services/networksecurity/resource_network_security_security_profile_group_generated_test.go
@@ -79,6 +79,146 @@ resource "google_network_security_security_profile" "security_profile" {
 `, context)
 }
 
+func TestAccNetworkSecuritySecurityProfileGroup_networkSecuritySecurityProfileGroupMirroringExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id":        envvar.GetTestOrgFromEnv(t),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckNetworkSecuritySecurityProfileGroupDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkSecuritySecurityProfileGroup_networkSecuritySecurityProfileGroupMirroringExample(context),
+			},
+			{
+				ResourceName:            "google_network_security_security_profile_group.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "name", "parent", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccNetworkSecuritySecurityProfileGroup_networkSecuritySecurityProfileGroupMirroringExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_network" "default" {
+  provider                = google-beta
+  name                    = "network%{random_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_network_security_mirroring_deployment_group" "default" {
+  provider                      = google-beta
+  mirroring_deployment_group_id = "tf-test-deployment-group%{random_suffix}"
+  location                      = "global"
+  network                       = google_compute_network.default.id
+}
+
+resource "google_network_security_mirroring_endpoint_group" "default" {
+  provider                      = google-beta
+  mirroring_endpoint_group_id   = "tf-test-endpoint-group%{random_suffix}"
+  location                      = "global"
+  mirroring_deployment_group    = google_network_security_mirroring_deployment_group.default.id
+}
+
+resource "google_network_security_security_profile" "default" {
+  provider    = google-beta
+  name        = "tf-test-sec-profile%{random_suffix}"
+  parent      = "organizations/%{org_id}"
+  description = "my description"
+  type        = "CUSTOM_MIRRORING"
+
+  custom_mirroring_profile {
+    mirroring_endpoint_group = google_network_security_mirroring_endpoint_group.default.id
+  }
+}
+
+resource "google_network_security_security_profile_group" "default" {
+  provider                 = google-beta
+  name                     = "tf-test-sec-profile-group%{random_suffix}"
+  parent                   = "organizations/%{org_id}"
+  description              = "my description"
+  custom_mirroring_profile = google_network_security_security_profile.default.id
+}
+`, context)
+}
+
+func TestAccNetworkSecuritySecurityProfileGroup_networkSecuritySecurityProfileGroupInterceptExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"org_id":        envvar.GetTestOrgFromEnv(t),
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckNetworkSecuritySecurityProfileGroupDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetworkSecuritySecurityProfileGroup_networkSecuritySecurityProfileGroupInterceptExample(context),
+			},
+			{
+				ResourceName:            "google_network_security_security_profile_group.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "location", "name", "parent", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccNetworkSecuritySecurityProfileGroup_networkSecuritySecurityProfileGroupInterceptExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_network" "default" {
+  provider                = google-beta
+  name                    = "network%{random_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_network_security_intercept_deployment_group" "default" {
+  provider                      = google-beta
+  intercept_deployment_group_id = "tf-test-deployment-group%{random_suffix}"
+  location                      = "global"
+  network                       = google_compute_network.default.id
+}
+
+resource "google_network_security_intercept_endpoint_group" "default" {
+  provider                      = google-beta
+  intercept_endpoint_group_id   = "tf-test-endpoint-group%{random_suffix}"
+  location                      = "global"
+  intercept_deployment_group    = google_network_security_intercept_deployment_group.default.id
+}
+
+resource "google_network_security_security_profile" "default" {
+  provider    = google-beta
+  name        = "tf-test-sec-profile%{random_suffix}"
+  parent      = "organizations/%{org_id}"
+  description = "my description"
+  type        = "CUSTOM_INTERCEPT"
+
+  custom_intercept_profile {
+    intercept_endpoint_group = google_network_security_intercept_endpoint_group.default.id
+  }
+}
+
+resource "google_network_security_security_profile_group" "default" {
+  provider                 = google-beta
+  name                     = "tf-test-sec-profile-group%{random_suffix}"
+  parent                   = "organizations/%{org_id}"
+  description              = "my description"
+  custom_intercept_profile = google_network_security_security_profile.default.id
+}
+`, context)
+}
+
 func testAccCheckNetworkSecuritySecurityProfileGroupDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
