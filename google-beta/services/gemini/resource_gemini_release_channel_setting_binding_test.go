44fbab898e509de80d7d30908054c68ad47c0324
Modular Magician <magic-modules@google.com>
2025-02-12 19:47:55 +0000

    Promote Gemini Release Channel Setting Binding from google-beta to google (#13051)
    [upstream:f955290d208a8a996a2048cda774a4d7181487d9]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/gemini/resource_gemini_release_channel_setting_binding_test.go
+++ google-beta/services/gemini/resource_gemini_release_channel_setting_binding_test.go
@@ -22,7 +22,7 @@ func TestAccGeminiReleaseChannelSettingBinding_update(t *testing.T) {
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccGeminiReleaseChannelSettingBinding_basic(context),
@@ -54,11 +54,9 @@ func TestAccGeminiReleaseChannelSettingBinding_update(t *testing.T) {
 func testAccGeminiReleaseChannelSettingBinding_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 data "google_project" "project" {
-    provider = google-beta
 }
 
 resource "google_gemini_release_channel_setting" "basic" {
-    provider = google-beta
     release_channel_setting_id = "%{release_channel_setting_id}"
     location = "global"
     labels = {"my_key" = "my_value"}
@@ -66,7 +64,6 @@ resource "google_gemini_release_channel_setting" "basic" {
 }
 
 resource "google_gemini_release_channel_setting_binding" "basic_binding" {
-    provider = google-beta
     release_channel_setting_id = google_gemini_release_channel_setting.basic.release_channel_setting_id
     setting_binding_id = "%{setting_binding_id}"
 	location = "global"
@@ -78,11 +75,9 @@ resource "google_gemini_release_channel_setting_binding" "basic_binding" {
 func testAccGeminiReleaseChannelSettingBinding_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 data "google_project" "project" {
-    provider = google-beta
 }
 
 resource "google_gemini_release_channel_setting" "basic" {
-    provider = google-beta
     release_channel_setting_id = "%{release_channel_setting_id}"
     location = "global"
     labels = {"my_key" = "my_value"}
@@ -90,7 +85,6 @@ resource "google_gemini_release_channel_setting" "basic" {
 }
 
 resource "google_gemini_release_channel_setting_binding" "basic_binding" {
-    provider = google-beta
     release_channel_setting_id = google_gemini_release_channel_setting.basic.release_channel_setting_id
     setting_binding_id = "%{setting_binding_id}"
 	location = "global"
