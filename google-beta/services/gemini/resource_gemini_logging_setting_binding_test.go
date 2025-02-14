c1e87717de67ac9ae0f4a834d2cba21b8cf4c2dd
The Magician <magic-modules@google.com>
2025-02-14 10:21:05 -0800

    Promote Gemini Logging Setting Binding from google-beta to google (#13050) (#9335)
    [upstream:3eeeb73399e3ab740795415ce7aa5337a50f237d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/gemini/resource_gemini_logging_setting_binding_test.go
+++ google-beta/services/gemini/resource_gemini_logging_setting_binding_test.go
@@ -22,7 +22,7 @@ func TestAccGeminiLoggingSettingBinding_update(t *testing.T) {
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccGeminiLoggingSettingBinding_basic(context),
@@ -54,11 +54,9 @@ func TestAccGeminiLoggingSettingBinding_update(t *testing.T) {
 func testAccGeminiLoggingSettingBinding_basic(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 data "google_project" "project" {
-	provider = google-beta
 }
 
 resource "google_gemini_logging_setting" "basic" {
-    provider = google-beta
     logging_setting_id = "%{logging_setting_id}"
 	location = "global"
 	labels = {"my_key" = "my_value"}
@@ -66,7 +64,6 @@ resource "google_gemini_logging_setting" "basic" {
 }
 
 resource "google_gemini_logging_setting_binding" "basic_binding" {
-    provider = google-beta
     logging_setting_id = google_gemini_logging_setting.basic.logging_setting_id
     setting_binding_id = "%{setting_binding_id}"
 	location = "global"
@@ -78,11 +75,9 @@ resource "google_gemini_logging_setting_binding" "basic_binding" {
 func testAccGeminiLoggingSettingBinding_update(context map[string]interface{}) string {
 	return acctest.Nprintf(`
 data "google_project" "project" {
-	provider = google-beta
 }
 
 resource "google_gemini_logging_setting" "basic" {
-    provider = google-beta
     logging_setting_id = "%{logging_setting_id}"
 	location = "global"
 	labels = {"my_key" = "my_value"}
@@ -90,7 +85,6 @@ resource "google_gemini_logging_setting" "basic" {
 }
 
 resource "google_gemini_logging_setting_binding" "basic_binding" {
-    provider = google-beta
     logging_setting_id = google_gemini_logging_setting.basic.logging_setting_id
     setting_binding_id = "%{setting_binding_id}"
 	location = "global"
