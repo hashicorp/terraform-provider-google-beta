cbb130c3915cae34c523ba4e3583f8a20b6d1450
Modular Magician <magic-modules@google.com>
2025-02-18 22:04:51 +0000

    Add Gemini Data Sharing With Google Setting Binding resource (#12906)
    [upstream:4bd02e1f235de0f47f4cb21d7094093260329120]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -535,9 +535,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 609
+// Generated resources: 610
 // Generated IAM resources: 306
-// Total generated resources: 915
+// Total generated resources: 916
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1042,6 +1042,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_firestore_index":                                                     firestore.ResourceFirestoreIndex(),
 	"google_gemini_code_repository_index":                                        gemini.ResourceGeminiCodeRepositoryIndex(),
 	"google_gemini_data_sharing_with_google_setting":                             gemini.ResourceGeminiDataSharingWithGoogleSetting(),
+	"google_gemini_data_sharing_with_google_setting_binding":                     gemini.ResourceGeminiDataSharingWithGoogleSettingBinding(),
 	"google_gemini_gemini_gcp_enablement_setting":                                gemini.ResourceGeminiGeminiGcpEnablementSetting(),
 	"google_gemini_logging_setting":                                              gemini.ResourceGeminiLoggingSetting(),
 	"google_gemini_logging_setting_binding":                                      gemini.ResourceGeminiLoggingSettingBinding(),
