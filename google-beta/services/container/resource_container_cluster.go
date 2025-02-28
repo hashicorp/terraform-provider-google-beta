15caf221ac61a25b1d860c142902de347097fbd8
Modular Magician <magic-modules@google.com>
2025-02-28 20:43:49 +0000

    Add JOBSET enum to monitoring config (#13196)
    [upstream:a560217bf7627547e87767a3af91266e6a3c39f8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/container/resource_container_cluster.go
+++ google-beta/services/container/resource_container_cluster.go
@@ -1246,7 +1246,7 @@ func ResourceContainerCluster() *schema.Resource {
 							Type:        schema.TypeList,
 							Optional:    true,
 							Computed:    true,
-							Description: `GKE components exposing metrics. Valid values include SYSTEM_COMPONENTS, APISERVER, SCHEDULER, CONTROLLER_MANAGER, STORAGE, HPA, POD, DAEMONSET, DEPLOYMENT, STATEFULSET, WORKLOADS, KUBELET, CADVISOR and DCGM.`,
+							Description: `GKE components exposing metrics. Valid values include SYSTEM_COMPONENTS, APISERVER, SCHEDULER, CONTROLLER_MANAGER, STORAGE, HPA, POD, DAEMONSET, DEPLOYMENT, STATEFULSET, WORKLOADS, KUBELET, CADVISOR, DCGM and JOBSET.`,
 							Elem: &schema.Schema{
 								Type: schema.TypeString,
 							},
