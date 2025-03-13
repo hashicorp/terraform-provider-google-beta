b8bd65bd9908145b2ffab2b4122d09592ae5a829
The Magician <magic-modules@google.com>
2025-03-13 12:34:31 -0700

    Resource list fallback to items index (#13352) (#9545)
    [upstream:6068f8d4da95cc8dda97fe1467ddd21c24257d11]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_region_security_policy_sweeper.go
+++ google-beta/services/compute/resource_compute_region_security_policy_sweeper.go
@@ -111,10 +111,18 @@ func testSweepComputeRegionSecurityPolicy(_ string) error {
 			return err
 		}
 
+		// First try the expected resource key
 		resourceList, ok := res["regionSecurityPolicies"]
-		if !ok {
-			log.Printf("[INFO][SWEEPER_LOG] Nothing found in response.")
-			continue
+		if ok {
+			log.Printf("[INFO][SWEEPER_LOG] Found resources under expected key 'regionSecurityPolicies'")
+		} else {
+			// Next, try the common "items" pattern
+			resourceList, ok = res["items"]
+			if ok {
+				log.Printf("[INFO][SWEEPER_LOG] Found resources under standard 'items' key")
+			} else {
+				continue
+			}
 		}
 		rl := resourceList.([]interface{})
 
