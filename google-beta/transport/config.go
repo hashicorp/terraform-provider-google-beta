2f4215473fa7d1042ff140522240c84030fb4cca
The Magician <magic-modules@google.com>
2025-03-17 15:17:51 -0700

    Add storage_project_management_hub resource. (#12689) (#9570)
    [upstream:6ebedbb8be732616f0be952efc214a268bb6026c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/transport/config.go
+++ google-beta/transport/config.go
@@ -329,6 +329,7 @@ type Config struct {
 	SpannerBasePath                  string
 	SQLBasePath                      string
 	StorageBasePath                  string
+	StorageControlBasePath           string
 	StorageInsightsBasePath          string
 	StorageTransferBasePath          string
 	TagsBasePath                     string
@@ -499,6 +500,7 @@ const SourceRepoBasePathKey = "SourceRepo"
 const SpannerBasePathKey = "Spanner"
 const SQLBasePathKey = "SQL"
 const StorageBasePathKey = "Storage"
+const StorageControlBasePathKey = "StorageControl"
 const StorageInsightsBasePathKey = "StorageInsights"
 const StorageTransferBasePathKey = "StorageTransfer"
 const TagsBasePathKey = "Tags"
@@ -663,6 +665,7 @@ var DefaultBasePaths = map[string]string{
 	SpannerBasePathKey:                  "https://spanner.googleapis.com/v1/",
 	SQLBasePathKey:                      "https://sqladmin.googleapis.com/sql/v1beta4/",
 	StorageBasePathKey:                  "https://storage.googleapis.com/storage/v1/",
+	StorageControlBasePathKey:           "https://storage.googleapis.com/v2/",
 	StorageInsightsBasePathKey:          "https://storageinsights.googleapis.com/v1/",
 	StorageTransferBasePathKey:          "https://storagetransfer.googleapis.com/v1/",
 	TagsBasePathKey:                     "https://cloudresourcemanager.googleapis.com/v3/",
@@ -1453,6 +1456,11 @@ func SetEndpointDefaults(d *schema.ResourceData) error {
 		d.Set("storage_custom_endpoint", MultiEnvDefault([]string{
 			"GOOGLE_STORAGE_CUSTOM_ENDPOINT",
 		}, DefaultBasePaths[StorageBasePathKey]))
+	}
+	if d.Get("storage_control_custom_endpoint") == "" {
+		d.Set("storage_control_custom_endpoint", MultiEnvDefault([]string{
+			"GOOGLE_STORAGE_CONTROL_CUSTOM_ENDPOINT",
+		}, DefaultBasePaths[StorageControlBasePathKey]))
 	}
 	if d.Get("storage_insights_custom_endpoint") == "" {
 		d.Set("storage_insights_custom_endpoint", MultiEnvDefault([]string{
@@ -2607,6 +2615,7 @@ func ConfigureBasePaths(c *Config) {
 	c.SpannerBasePath = DefaultBasePaths[SpannerBasePathKey]
 	c.SQLBasePath = DefaultBasePaths[SQLBasePathKey]
 	c.StorageBasePath = DefaultBasePaths[StorageBasePathKey]
+	c.StorageControlBasePath = DefaultBasePaths[StorageControlBasePathKey]
 	c.StorageInsightsBasePath = DefaultBasePaths[StorageInsightsBasePathKey]
 	c.StorageTransferBasePath = DefaultBasePaths[StorageTransferBasePathKey]
 	c.TagsBasePath = DefaultBasePaths[TagsBasePathKey]
