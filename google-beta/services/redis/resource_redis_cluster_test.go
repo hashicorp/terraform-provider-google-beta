a3e1bf42e53065f5c4a46f7316cdeafe86f59a82
Modular Magician <magic-modules@google.com>
2025-03-28 23:35:22 +0000

    Adding automated backup config to Redis  (#13401)
    [upstream:3ba9ffacd77872416e7a528289c4137fb09f40a4]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/redis/resource_redis_cluster_test.go
+++ google-beta/services/redis/resource_redis_cluster_test.go
@@ -5,6 +5,7 @@ package redis_test
 import (
 	"fmt"
 	"strings"
+
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -266,6 +267,70 @@ func TestAccRedisCluster_createUpdateDeletionProtection(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccRedisCluster_automatedBackupConfig(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckRedisClusterDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccRedisCluster_automatedBackupConfig(context),
+			},
+			{
+				ResourceName:      "google_redis_cluster.cluster_abc",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccRedisCluster_automatedBackupConfigWithout(context),
+			},
+			{
+				ResourceName:      "google_redis_cluster.cluster_abc",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccRedisCluster_automatedBackupConfig(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_redis_cluster" "cluster_abc" {
+  name                           = "tf-test-redis-abc-%{random_suffix}"
+  shard_count                    = 1
+  region                         = "us-central1"
+  deletion_protection_enabled    = false
+  automated_backup_config {
+   retention                     = "259200s"
+   fixed_frequency_schedule {
+    start_time {
+      hours                      = 20
+    }
+   }
+  }
+
+}	
+`, context)
+}
+
+func testAccRedisCluster_automatedBackupConfigWithout(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_redis_cluster" "cluster_abc" {
+  name                           = "tf-test-redis-abc-%{random_suffix}"
+  shard_count                    = 1
+  region                         = "us-central1"
+  deletion_protection_enabled    = false 
+  
+}	
+`, context)
 }
 
 // Validate that persistence is updated for the cluster
