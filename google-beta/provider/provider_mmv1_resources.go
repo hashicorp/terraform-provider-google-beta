70fb2e5fb55a746b34dd63839104ebd07e94df34
Modular Magician <magic-modules@google.com>
2025-04-02 16:55:20 +0000

    Add Gemini Code Tools Setting resource (#13319)
    [upstream:6df9821a827280b831a607db8aafd20e7dedc771]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -554,9 +554,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 636
+// Generated resources: 637
 // Generated IAM resources: 312
-// Total generated resources: 948
+// Total generated resources: 949
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1081,6 +1081,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_firestore_field":                                                     firestore.ResourceFirestoreField(),
 	"google_firestore_index":                                                     firestore.ResourceFirestoreIndex(),
 	"google_gemini_code_repository_index":                                        gemini.ResourceGeminiCodeRepositoryIndex(),
+	"google_gemini_code_tools_setting":                                           gemini.ResourceGeminiCodeToolsSetting(),
 	"google_gemini_data_sharing_with_google_setting":                             gemini.ResourceGeminiDataSharingWithGoogleSetting(),
 	"google_gemini_data_sharing_with_google_setting_binding":                     gemini.ResourceGeminiDataSharingWithGoogleSettingBinding(),
 	"google_gemini_gemini_gcp_enablement_setting":                                gemini.ResourceGeminiGeminiGcpEnablementSetting(),
