250d6dde6f11fc90e929b081065b6de752dbd7d4
Modular Magician <magic-modules@google.com>
2025-04-04 20:49:00 +0000

    Fix failing Managed Kafka Connect resource test failures (#13435)
    [upstream:d297ae2b466f6a26869484dd86bc79eafb79112c]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/managedkafka/resource_managed_kafka_connect_cluster_generated_test.go
+++ google-beta/services/managedkafka/resource_managed_kafka_connect_cluster_generated_test.go
@@ -26,21 +26,28 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
 
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/acctest"
+	"github.com/hashicorp/terraform-provider-google-beta/google-beta/envvar"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
 	transport_tpg "github.com/hashicorp/terraform-provider-google-beta/google-beta/transport"
 )
 
 func TestAccManagedKafkaConnectCluster_managedkafkaConnectClusterBasicExample(t *testing.T) {
+	acctest.SkipIfVcr(t)
 	t.Parallel()
 
 	context := map[string]interface{}{
-		"random_suffix": acctest.RandString(t, 10),
+		"billing_account": envvar.GetTestBillingAccountFromEnv(t),
+		"org_id":          envvar.GetTestOrgFromEnv(t),
+		"random_suffix":   acctest.RandString(t, 10),
 	}
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
-		CheckDestroy:             testAccCheckManagedKafkaConnectClusterDestroyProducer(t),
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"time": {},
+		},
+		CheckDestroy: testAccCheckManagedKafkaConnectClusterDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
 				Config: testAccManagedKafkaConnectCluster_managedkafkaConnectClusterBasicExample(context),
@@ -57,26 +64,50 @@ func TestAccManagedKafkaConnectCluster_managedkafkaConnectClusterBasicExample(t 
 
 func testAccManagedKafkaConnectCluster_managedkafkaConnectClusterBasicExample(context map[string]interface{}) string {
 	return acctest.Nprintf(`
-resource "google_compute_network" "mkc_network" {
-  name                    = "tf-test-my-network%{random_suffix}"
-  auto_create_subnetworks = false
+resource "google_project" "project" {
+  project_id      = "tf-test%{random_suffix}"
+  name            = "tf-test%{random_suffix}"
+  org_id          = "%{org_id}"
+  billing_account = "%{billing_account}"
+  deletion_policy = "DELETE"
+
+  provider = google-beta
 }
 
-resource "google_compute_subnetwork" "mkc_subnet" {
-  name          = "tf-test-my-subnetwork%{random_suffix}"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.mkc_network.id
+resource "time_sleep" "wait_60_seconds" {
+  create_duration = "60s"
+  depends_on = [google_project.project]
 }
 
-resource "google_compute_subnetwork" "mkc_additional_subnet" {
-  name          = "tf-test-my-additional-subnetwork-0%{random_suffix}"
+resource "google_project_service" "compute" {
+  project = google_project.project.project_id
+  service = "compute.googleapis.com"
+  depends_on = [time_sleep.wait_60_seconds]
+
+  provider = google-beta
+}
+
+resource "google_project_service" "managedkafka" {
+  project = google_project.project.project_id
+  service = "managedkafka.googleapis.com"
+  depends_on = [time_sleep.wait_60_seconds]
+
+  provider = google-beta
+}
+
+resource "google_compute_subnetwork" "mkc_secondary_subnet" {
+  project       = google_project.project.project_id
+  name          = "tf-test-my-secondary-subnetwork%{random_suffix}"
   ip_cidr_range = "10.3.0.0/16"
   region        = "us-central1"
-  network       = google_compute_network.mkc_network.id
+  network       = "default"
+  depends_on = [google_project_service.compute]
+
+  provider = google-beta
 }
 
 resource "google_managed_kafka_cluster" "gmk_cluster" {
+  project = google_project.project.project_id
   cluster_id = "tf-test-my-cluster%{random_suffix}"
   location = "us-central1"
   capacity_config {
@@ -86,15 +117,19 @@ resource "google_managed_kafka_cluster" "gmk_cluster" {
   gcp_config {
     access_config {
       network_configs {
-        subnet = "projects/${data.google_project.project.project_id}/regions/us-central1/subnetworks/${google_compute_subnetwork.mkc_subnet.id}"
+        subnet = "projects/${google_project.project.project_id}/regions/us-central1/subnetworks/default"
       }
     }
   }
+  depends_on = [google_project_service.managedkafka]
+
+  provider = google-beta
 }
 
 resource "google_managed_kafka_connect_cluster" "example" {
+  project = google_project.project.project_id
   connect_cluster_id = "tf-test-my-connect-cluster%{random_suffix}"
-  kafka_cluster = "projects/${data.google_project.project.project_id}/locations/us-central1/clusters/${google_managed_kafka_cluster.gmk_cluster.cluster_id}"
+  kafka_cluster = "projects/${google_project.project.project_id}/locations/us-central1/clusters/${google_managed_kafka_cluster.gmk_cluster.cluster_id}"
   location = "us-central1"
   capacity_config {
     vcpu_count = 12
@@ -103,18 +138,18 @@ resource "google_managed_kafka_connect_cluster" "example" {
   gcp_config {
     access_config {
       network_configs {
-        primary_subnet = "projects/${data.google_project.project.project_id}/regions/us-central1/subnetworks/${google_compute_subnetwork.mkc_subnet.id}"
-        additional_subnets = ["${google_compute_subnetwork.mkc_additional_subnet.id}"]
-        dns_domain_names = ["${google_managed_kafka_cluster.gmk_cluster.cluster_id}.us-central1.managedkafka-staging.${data.google_project.project.project_id}.cloud-staging.goog"]
+        primary_subnet = "projects/${google_project.project.project_id}/regions/us-central1/subnetworks/default"
+        additional_subnets = ["${google_compute_subnetwork.mkc_secondary_subnet.id}"]
+        dns_domain_names = ["${google_managed_kafka_cluster.gmk_cluster.cluster_id}.us-central1.managedkafka.${google_project.project.project_id}.cloud.goog"]
       }
     }
   }
   labels = {
     key = "value"
   }
-}
+  depends_on = [google_project_service.managedkafka]
 
-data "google_project" "project" {
+  provider = google-beta
 }
 `, context)
 }
