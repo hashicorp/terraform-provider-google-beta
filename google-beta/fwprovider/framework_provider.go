9bc2fc8d3edb7e757bd43b2e947ff1cf766dc8a4
Modular Magician <magic-modules@google.com>
2025-08-15 02:04:20 +0000

    Enable custom endpoints for all DCL-based resources (#14778)
    [upstream:25a423c917bb808023a8f9da7acc02e3318b4558]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/fwprovider/framework_provider.go
+++ google-beta/fwprovider/framework_provider.go
@@ -1180,7 +1180,7 @@ func (p *FrameworkProvider) Schema(_ context.Context, _ provider.SchemaRequest, 
 				},
 			},
 
-			// dcl
+			// DCL
 			"container_aws_custom_endpoint": &schema.StringAttribute{
 				Optional: true,
 				Validators: []validator.String{
@@ -1188,6 +1188,36 @@ func (p *FrameworkProvider) Schema(_ context.Context, _ provider.SchemaRequest, 
 				},
 			},
 			"container_azure_custom_endpoint": &schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					transport_tpg.CustomEndpointValidator(),
+				},
+			},
+			"apikeys_custom_endpoint": &schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					transport_tpg.CustomEndpointValidator(),
+				},
+			},
+			"assured_workloads_custom_endpoint": &schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					transport_tpg.CustomEndpointValidator(),
+				},
+			},
+			"cloud_resource_manager_custom_endpoint": &schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					transport_tpg.CustomEndpointValidator(),
+				},
+			},
+			"firebaserules_custom_endpoint": &schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					transport_tpg.CustomEndpointValidator(),
+				},
+			},
+			"recaptcha_enterprise_custom_endpoint": &schema.StringAttribute{
 				Optional: true,
 				Validators: []validator.String{
 					transport_tpg.CustomEndpointValidator(),
@@ -1236,8 +1266,6 @@ func (p *FrameworkProvider) Schema(_ context.Context, _ provider.SchemaRequest, 
 			},
 		},
 	}
-
-	transport_tpg.ConfigureDCLCustomEndpointAttributesFramework(&resp.Schema)
 }
 
 // Configure prepares the metadata/'meta' required for data sources and resources to function.
