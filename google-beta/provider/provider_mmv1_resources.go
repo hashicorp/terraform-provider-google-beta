983fbbfc3d87da815b5b837483e64a68a07e3591
The Magician <magic-modules@google.com>
2025-09-22 17:23:28 -0700

    Adds RolloutKind provider for SaasRuntime (#15124) (#10764)
    [upstream:86f5dfe35d418efe71ea92fff6dbe51bd4941c56]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -614,9 +614,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 704
+// Generated resources: 705
 // Generated IAM resources: 348
-// Total generated resources: 1052
+// Total generated resources: 1053
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1498,6 +1498,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_runtimeconfig_config_iam_member":                                     tpgiamresource.ResourceIamMember(runtimeconfig.RuntimeConfigConfigIamSchema, runtimeconfig.RuntimeConfigConfigIamUpdaterProducer, runtimeconfig.RuntimeConfigConfigIdParseFunc),
 	"google_runtimeconfig_config_iam_policy":                                     tpgiamresource.ResourceIamPolicy(runtimeconfig.RuntimeConfigConfigIamSchema, runtimeconfig.RuntimeConfigConfigIamUpdaterProducer, runtimeconfig.RuntimeConfigConfigIdParseFunc),
 	"google_saas_runtime_release":                                                saasruntime.ResourceSaasRuntimeRelease(),
+	"google_saas_runtime_rollout_kind":                                           saasruntime.ResourceSaasRuntimeRolloutKind(),
 	"google_saas_runtime_saas":                                                   saasruntime.ResourceSaasRuntimeSaas(),
 	"google_saas_runtime_unit_kind":                                              saasruntime.ResourceSaasRuntimeUnitKind(),
 	"google_secret_manager_secret":                                               secretmanager.ResourceSecretManagerSecret(),
