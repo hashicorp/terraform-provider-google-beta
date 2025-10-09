7e0045f30db4744a48be8c39366af21b6657340e
The Magician <magic-modules@google.com>
2025-10-09 00:28:48 -0700

    Adds `google_apigee_developer_app resource` and enhance `google_apigee_api_product scopes` to be order-insensitive (#15336) (#10851)
    [upstream:b6c16709ec801b552d56cf69ede46fe304d27d55]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -616,9 +616,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 715
+// Generated resources: 716
 // Generated IAM resources: 348
-// Total generated resources: 1063
+// Total generated resources: 1064
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -667,6 +667,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_apigee_app_group":                                                    apigee.ResourceApigeeAppGroup(),
 	"google_apigee_control_plane_access":                                         apigee.ResourceApigeeControlPlaneAccess(),
 	"google_apigee_developer":                                                    apigee.ResourceApigeeDeveloper(),
+	"google_apigee_developer_app":                                                apigee.ResourceApigeeDeveloperApp(),
 	"google_apigee_dns_zone":                                                     apigee.ResourceApigeeDnsZone(),
 	"google_apigee_endpoint_attachment":                                          apigee.ResourceApigeeEndpointAttachment(),
 	"google_apigee_env_keystore":                                                 apigee.ResourceApigeeEnvKeystore(),
