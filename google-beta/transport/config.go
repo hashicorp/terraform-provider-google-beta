0c4f98327cb1ebd898e42ba9769ab18baf3f7340
Modular Magician <magic-modules@google.com>
2025-10-14 17:34:24 +0000

    Add Observability TraceScope resource and tests (#15304)
    Co-authored-by: Shuya Ma <87669292+shuyama1@users.noreply.github.com>
    
    [upstream:705d5ff1ecedf8ba48871c2e080640ce9c14f38e]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/transport/config.go
+++ google-beta/transport/config.go
@@ -372,6 +372,7 @@ type Config struct {
 	NetworkSecurityBasePath          string
 	NetworkServicesBasePath          string
 	NotebooksBasePath                string
+	ObservabilityBasePath            string
 	OracleDatabaseBasePath           string
 	OrgPolicyBasePath                string
 	OSConfigBasePath                 string
@@ -556,6 +557,7 @@ const NetworkManagementBasePathKey = "NetworkManagement"
 const NetworkSecurityBasePathKey = "NetworkSecurity"
 const NetworkServicesBasePathKey = "NetworkServices"
 const NotebooksBasePathKey = "Notebooks"
+const ObservabilityBasePathKey = "Observability"
 const OracleDatabaseBasePathKey = "OracleDatabase"
 const OrgPolicyBasePathKey = "OrgPolicy"
 const OSConfigBasePathKey = "OSConfig"
@@ -729,6 +731,7 @@ var DefaultBasePaths = map[string]string{
 	NetworkSecurityBasePathKey:          "https://networksecurity.googleapis.com/v1beta1/",
 	NetworkServicesBasePathKey:          "https://networkservices.googleapis.com/v1/",
 	NotebooksBasePathKey:                "https://notebooks.googleapis.com/v1/",
+	ObservabilityBasePathKey:            "https://observability.googleapis.com/v1/",
 	OracleDatabaseBasePathKey:           "https://oracledatabase.googleapis.com/v1/",
 	OrgPolicyBasePathKey:                "https://orgpolicy.googleapis.com/v2/",
 	OSConfigBasePathKey:                 "https://osconfig.googleapis.com/v1beta/",
@@ -1426,6 +1429,11 @@ func SetEndpointDefaults(d *schema.ResourceData) error {
 		d.Set("notebooks_custom_endpoint", MultiEnvDefault([]string{
 			"GOOGLE_NOTEBOOKS_CUSTOM_ENDPOINT",
 		}, DefaultBasePaths[NotebooksBasePathKey]))
+	}
+	if d.Get("observability_custom_endpoint") == "" {
+		d.Set("observability_custom_endpoint", MultiEnvDefault([]string{
+			"GOOGLE_OBSERVABILITY_CUSTOM_ENDPOINT",
+		}, DefaultBasePaths[ObservabilityBasePathKey]))
 	}
 	if d.Get("oracle_database_custom_endpoint") == "" {
 		d.Set("oracle_database_custom_endpoint", MultiEnvDefault([]string{
@@ -2848,6 +2856,7 @@ func ConfigureBasePaths(c *Config) {
 	c.NetworkSecurityBasePath = DefaultBasePaths[NetworkSecurityBasePathKey]
 	c.NetworkServicesBasePath = DefaultBasePaths[NetworkServicesBasePathKey]
 	c.NotebooksBasePath = DefaultBasePaths[NotebooksBasePathKey]
+	c.ObservabilityBasePath = DefaultBasePaths[ObservabilityBasePathKey]
 	c.OracleDatabaseBasePath = DefaultBasePaths[OracleDatabaseBasePathKey]
 	c.OrgPolicyBasePath = DefaultBasePaths[OrgPolicyBasePathKey]
 	c.OSConfigBasePath = DefaultBasePaths[OSConfigBasePathKey]
