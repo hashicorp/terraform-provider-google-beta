6fa4c4708a326f90bf159b27807f408c5048f006
The Magician <magic-modules@google.com>
2025-10-27 15:25:55 -0700

    Revert "cloudsecuritycompliace: add google_cloud_security_compliance_cloud_control resource" (#15487) (#10949)
    [upstream:e84769702aa1f74cbcd277d75d659fd1ae4bf845]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/sweeper/gcp_sweeper_test.go
+++ google-beta/sweeper/gcp_sweeper_test.go
@@ -56,7 +56,6 @@ import (
 	_ "github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudrun"
 	_ "github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudrunv2"
 	_ "github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudscheduler"
-	_ "github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudsecuritycompliance"
 	_ "github.com/hashicorp/terraform-provider-google-beta/google-beta/services/cloudtasks"
 	_ "github.com/hashicorp/terraform-provider-google-beta/google-beta/services/colab"
 	_ "github.com/hashicorp/terraform-provider-google-beta/google-beta/services/composer"
