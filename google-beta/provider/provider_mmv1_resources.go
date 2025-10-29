e935b1bbe9bcedcae757376973b0d1cfd27eecd1
Modular Magician <magic-modules@google.com>
2025-10-29 21:35:28 +0000

    Add a new resource: Control (#15437)
    Co-authored-by: Nick Elliot <nickelliot@google.com>
    
    [upstream:29ad9b6f54ba24da3f667bffc6569c67c2e61de3]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -621,9 +621,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 730
+// Generated resources: 731
 // Generated IAM resources: 348
-// Total generated resources: 1078
+// Total generated resources: 1079
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1147,6 +1147,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_discovery_engine_assistant":                                          discoveryengine.ResourceDiscoveryEngineAssistant(),
 	"google_discovery_engine_chat_engine":                                        discoveryengine.ResourceDiscoveryEngineChatEngine(),
 	"google_discovery_engine_cmek_config":                                        discoveryengine.ResourceDiscoveryEngineCmekConfig(),
+	"google_discovery_engine_control":                                            discoveryengine.ResourceDiscoveryEngineControl(),
 	"google_discovery_engine_data_connector":                                     discoveryengine.ResourceDiscoveryEngineDataConnector(),
 	"google_discovery_engine_data_store":                                         discoveryengine.ResourceDiscoveryEngineDataStore(),
 	"google_discovery_engine_license_config":                                     discoveryengine.ResourceDiscoveryEngineLicenseConfig(),
