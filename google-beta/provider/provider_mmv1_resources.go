6250b003cd35574e26711a101b5a79022ac1f491
Modular Magician <magic-modules@google.com>
2025-11-13 15:08:55 +0000

    Adding Terraform support for OracleDatabase Exascale Db Storage Vault Resource (#15580)
    [upstream:13b33dc105489da2ca96b55738fd46630bc75dd5]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -626,9 +626,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 740
+// Generated resources: 741
 // Generated IAM resources: 348
-// Total generated resources: 1088
+// Total generated resources: 1089
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     accessapproval.ResourceAccessApprovalFolderSettings(),
 	"google_organization_access_approval_settings":                               accessapproval.ResourceAccessApprovalOrganizationSettings(),
@@ -1492,6 +1492,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_oracle_database_cloud_exadata_infrastructure":                        oracledatabase.ResourceOracleDatabaseCloudExadataInfrastructure(),
 	"google_oracle_database_cloud_vm_cluster":                                    oracledatabase.ResourceOracleDatabaseCloudVmCluster(),
 	"google_oracle_database_db_system":                                           oracledatabase.ResourceOracleDatabaseDbSystem(),
+	"google_oracle_database_exascale_db_storage_vault":                           oracledatabase.ResourceOracleDatabaseExascaleDbStorageVault(),
 	"google_oracle_database_odb_network":                                         oracledatabase.ResourceOracleDatabaseOdbNetwork(),
 	"google_oracle_database_odb_subnet":                                          oracledatabase.ResourceOracleDatabaseOdbSubnet(),
 	"google_org_policy_custom_constraint":                                        orgpolicy.ResourceOrgPolicyCustomConstraint(),
