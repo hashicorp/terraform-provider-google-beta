d2423cf0ab7d8b6cbcd3526b71812799c87d6891
Modular Magician <magic-modules@google.com>
2025-11-24 21:53:03 +0000

    Display nat ip (#15675)
    Co-authored-by: Nick Elliot <nickelliot@google.com>
    
    [upstream:cb379c917bd4a42088b76c09603fef95cdc392b9]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_service_attachment_generated_test.go
+++ google-beta/services/compute/resource_compute_service_attachment_generated_test.go
@@ -69,7 +69,7 @@ func TestAccComputeServiceAttachment_serviceAttachmentBasicExample(t *testing.T)
 				ResourceName:            "google_compute_service_attachment.psc_ilb_service_attachment",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"region", "tunneling_config"},
+				ImportStateVerifyIgnore: []string{"region", "show_nat_ips", "tunneling_config"},
 			},
 		},
 	})
@@ -178,7 +178,7 @@ func TestAccComputeServiceAttachment_serviceAttachmentExplicitProjectsExample(t 
 				ResourceName:            "google_compute_service_attachment.psc_ilb_service_attachment",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"region", "tunneling_config"},
+				ImportStateVerifyIgnore: []string{"region", "show_nat_ips", "tunneling_config"},
 			},
 		},
 	})
@@ -294,7 +294,7 @@ func TestAccComputeServiceAttachment_serviceAttachmentExplicitNetworksExample(t 
 				ResourceName:            "google_compute_service_attachment.psc_ilb_service_attachment",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"region", "tunneling_config"},
+				ImportStateVerifyIgnore: []string{"region", "show_nat_ips", "tunneling_config"},
 			},
 		},
 	})
@@ -421,7 +421,7 @@ func TestAccComputeServiceAttachment_serviceAttachmentReconcileConnectionsExampl
 				ResourceName:            "google_compute_service_attachment.psc_ilb_service_attachment",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"region", "tunneling_config"},
+				ImportStateVerifyIgnore: []string{"region", "show_nat_ips", "tunneling_config"},
 			},
 		},
 	})
@@ -521,7 +521,7 @@ func TestAccComputeServiceAttachment_serviceAttachmentTunnelingConfigExample(t *
 				ResourceName:            "google_compute_service_attachment.psc_ilb_service_attachment",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"region", "tunneling_config"},
+				ImportStateVerifyIgnore: []string{"region", "show_nat_ips", "tunneling_config"},
 			},
 		},
 	})
@@ -633,7 +633,7 @@ func TestAccComputeServiceAttachment_serviceAttachmentCrossRegionIlbExample(t *t
 				ResourceName:            "google_compute_service_attachment.psc_ilb_service_attachment",
 				ImportState:             true,
 				ImportStateVerify:       true,
-				ImportStateVerifyIgnore: []string{"region", "tunneling_config"},
+				ImportStateVerifyIgnore: []string{"region", "show_nat_ips", "tunneling_config"},
 			},
 		},
 	})
