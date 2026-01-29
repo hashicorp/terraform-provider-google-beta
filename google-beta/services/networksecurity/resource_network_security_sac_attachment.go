637d1f8c01d213610fb1cf32c4fe1731bb38097e
The Magician <magic-modules@google.com>
2026-01-29 12:00:01 -0800

    feat(firestore): Support for skip wait on Firestore index resource. (#16203) (#11476)
    [upstream:0ebb67dfc6977bafd2f24d99033dfc44e5856649]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/networksecurity/resource_network_security_sac_attachment.go
+++ google-beta/services/networksecurity/resource_network_security_sac_attachment.go
@@ -408,7 +408,7 @@ func resourceNetworkSecuritySacAttachmentRead(d *schema.ResourceData, meta inter
 }
 
 func resourceNetworkSecuritySacAttachmentUpdate(d *schema.ResourceData, meta interface{}) error {
-	// Only the root field "labels" and "terraform_labels" are mutable
+	// Only the root field "labels", "terraform_labels", and virtual fields are mutable
 	return resourceNetworkSecuritySacAttachmentRead(d, meta)
 }
 
--- google-beta/services/saasruntime/resource_saas_runtime_unit_operation.go
+++ google-beta/services/saasruntime/resource_saas_runtime_unit_operation.go
@@ -673,7 +673,7 @@ func resourceSaasRuntimeUnitOperationRead(d *schema.ResourceData, meta interface
 }
 
 func resourceSaasRuntimeUnitOperationUpdate(d *schema.ResourceData, meta interface{}) error {
-	// Only the root field "labels" and "terraform_labels" are mutable
+	// Only the root field "labels", "terraform_labels", and virtual fields are mutable
 	return resourceSaasRuntimeUnitOperationRead(d, meta)
 }
 
