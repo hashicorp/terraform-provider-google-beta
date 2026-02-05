1e4d30f18f0cfd75622de9e68402bc28d909c98a
Modular Magician <magic-modules@google.com>
2026-02-05 03:43:45 +0000

    Add support for dynamic nic to GCE instance/template (#16303)
    [upstream:4352529b05d8a18964060548668e5c8718e611f2]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_instance_template_test.go
+++ google-beta/services/compute/resource_compute_instance_template_test.go
@@ -1705,8 +1705,6 @@ func TestAccComputeInstanceTemplate_sourceImageEncryptionKey(t *testing.T) {
 func TestAccComputeInstanceTemplate_NetworkAttachment(t *testing.T) {
 	t.Parallel()
 
-	var instanceTemplate compute.InstanceTemplate
-
 	testNetworkName := acctest.BootstrapSharedTestNetwork(t, "attachment-network")
 	subnetName := acctest.BootstrapSubnet(t, "tf-test-subnet", testNetworkName)
 	networkAttachmentName := acctest.BootstrapNetworkAttachment(t, "tf-test-attachment", subnetName)
@@ -1727,11 +1725,6 @@ func TestAccComputeInstanceTemplate_NetworkAttachment(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: testAccComputeInstanceTemplate_network_attachment(context),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckComputeInstanceTemplateExists(
-						t, "google_compute_instance_template.foobar", &instanceTemplate),
-					testAccCheckComputeInstanceTemplateHasNetworkAttachment(&instanceTemplate, fmt.Sprintf("https://www.googleapis.com/compute/beta/%s", fullFormNetworkAttachmentName)),
-				),
 			},
 			{
 				ResourceName:      "google_compute_instance_template.foobar",
@@ -2057,6 +2050,88 @@ func TestAccComputeInstanceTemplate_schedulingSkipGuestOSShutdown(t *testing.T) 
 			},
 		},
 	})
+}
+
+func TestAccComputeInstanceTemplate_dynamicNic(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"instance_name": fmt.Sprintf("tf-test-%s", acctest.RandString(t, 10)),
+		"region":        "us-central1",
+		"zone":          "us-central1-a",
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
+		CheckDestroy:             testAccCheckComputeInstanceTemplateDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeInstanceTemplate_dynamicNic(context),
+			},
+			{
+				ResourceName:            "google_compute_instance_template.foobar",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"labels", "terraform_labels"},
+			},
+		},
+	})
+}
+
+func testAccComputeInstanceTemplate_dynamicNic(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+data "google_compute_image" "my_image" {
+  family  = "debian-11"
+  project = "debian-cloud"
+}
+
+resource "google_compute_network" "network1" {
+  name                    = "%{instance_name}-network1"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnetwork1" {
+  name    = "%{instance_name}-subnetwork1"
+  network = google_compute_network.network1.id
+  region  = "%{region}"
+
+  ip_cidr_range = "10.1.0.0/16"
+}
+
+resource "google_compute_network" "network2" {
+  name                    = "%{instance_name}-network2"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnetwork2" {
+  name    = "%{instance_name}-subnetwork2"
+  network = google_compute_network.network2.id
+  region  = "%{region}"
+
+  ip_cidr_range = "10.2.0.0/16"
+}
+
+resource "google_compute_instance_template" "foobar" {
+  name           = "%{instance_name}"
+  machine_type   = "e2-micro"
+
+  disk {
+    source_image = data.google_compute_image.my_image.self_link
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface { # nic0
+    subnetwork = google_compute_subnetwork.subnetwork1.id
+  }
+
+  network_interface { # nic0.2
+    subnetwork      = google_compute_subnetwork.subnetwork2.id
+    vlan            = 2
+  }
+}
+`, context)
 }
 
 func TestUnitComputeInstanceTemplate_IpCidrRangeDiffSuppress(t *testing.T) {
