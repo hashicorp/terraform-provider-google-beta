1df69211cef47b699b57be6ecfe0213d29018054
Modular Magician <magic-modules@google.com>
2026-02-12 23:03:15 +0000

    deletion policy generation backupdr-blockchainnodeengine (#16325)
    [upstream:0134df47072e9b29546cee5b8172f083248f1eef]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_data_exchange_subscription.go
+++ google-beta/services/bigqueryanalyticshub/resource_bigquery_analytics_hub_data_exchange_subscription.go
@@ -333,6 +333,18 @@ For Data Exchange subscriptions, this map may contain multiple entries if the Da
 				Computed: true,
 				ForceNew: true,
 			},
+			"deletion_policy": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Whether Terraform will be prevented from destroying the instance. Defaults to "DELETE".
+When a 'terraform destroy' or 'terraform apply' would delete the instance,
+the command will fail if this field is set to "PREVENT" in Terraform state.
+When set to "ABANDON", the command will remove the resource from Terraform
+management without updating or deleting the resource in the API.
+When set to "DELETE", deleting the resource is allowed.
+`,
+				Default: "DELETE",
+			},
 		},
 		UseJSONNumber: true,
 	}
@@ -523,6 +535,11 @@ func resourceBigqueryAnalyticsHubDataExchangeSubscriptionRead(d *schema.Resource
 			return fmt.Errorf("Error setting refresh_policy: %s", err)
 		}
 	}
+	if _, ok := d.GetOkExists("deletion_policy"); !ok {
+		if err := d.Set("deletion_policy", "DELETE"); err != nil {
+			return fmt.Errorf("Error setting deletion_policy: %s", err)
+		}
+	}
 	if err := d.Set("project", project); err != nil {
 		return fmt.Errorf("Error reading DataExchangeSubscription: %s", err)
 	}
@@ -621,6 +638,13 @@ func resourceBigqueryAnalyticsHubDataExchangeSubscriptionDelete(d *schema.Resour
 	}
 
 	headers := make(http.Header)
+	if d.Get("deletion_policy").(string) == "PREVENT" {
+		return fmt.Errorf("cannot destroy BigqueryAnalyticsHubDataExchangeSubscription without setting deletion_policy=\"DELETE\" and running `terraform apply`")
+	}
+	if d.Get("deletion_policy").(string) == "ABANDON" {
+		log.Printf("[DEBUG] deletion_policy set to \"ABANDON\", removing DataExchangeSubscription %q from Terraform state without deletion", d.Id())
+		return nil
+	}
 
 	log.Printf("[DEBUG] Deleting DataExchangeSubscription %q", d.Id())
 	res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
