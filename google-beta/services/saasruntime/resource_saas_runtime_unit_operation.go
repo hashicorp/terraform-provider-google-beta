2986a2512e17ffc232d91ab1ce9b5df92678f0e6
The Magician <magic-modules@google.com>
2026-02-12 12:11:44 -0800

    deletion policy generation accessapproval-artifactregistry (#16324) (#11549)
    [upstream:59e2f9f34e85d114deda4554f3c30943fc525960]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/saasruntime/resource_saas_runtime_unit_operation.go
+++ google-beta/services/saasruntime/resource_saas_runtime_unit_operation.go
@@ -683,7 +683,7 @@ func resourceSaasRuntimeUnitOperationRead(d *schema.ResourceData, meta interface
 }
 
 func resourceSaasRuntimeUnitOperationUpdate(d *schema.ResourceData, meta interface{}) error {
-	// Only the root field "labels", "terraform_labels", and virtual fields are mutable
+	// Only the root field "deletion_policy", "labels", "terraform_labels", and virtual fields are mutable
 	return resourceSaasRuntimeUnitOperationRead(d, meta)
 }
 
