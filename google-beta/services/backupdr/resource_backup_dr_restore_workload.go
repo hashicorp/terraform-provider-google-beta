10998701939d1bc4aa982fe1f817dc84718b4671
The Magician <magic-modules@google.com>
2026-02-12 15:03:25 -0800

    deletion policy generation backupdr-blockchainnodeengine (#16325) (#11552)
    [upstream:0134df47072e9b29546cee5b8172f083248f1eef]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/backupdr/resource_backup_dr_restore_workload.go
+++ google-beta/services/backupdr/resource_backup_dr_restore_workload.go
@@ -100,6 +100,7 @@ func ResourceBackupDRRestoreWorkload() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceBackupDRRestoreWorkloadCreate,
 		Read:   resourceBackupDRRestoreWorkloadRead,
+		Update: resourceBackupDRRestoreWorkloadUpdate,
 		Delete: resourceBackupDRRestoreWorkloadDelete,
 
 		Importer: &schema.ResourceImporter{
@@ -1402,6 +1403,19 @@ the request if it has already been completed.`,
 					},
 				},
 			},
+
+			"deletion_policy": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Whether Terraform will be prevented from destroying the instance. Defaults to "DELETE".
+When a 'terraform destroy' or 'terraform apply' would delete the instance,
+the command will fail if this field is set to "PREVENT" in Terraform state.
+When set to "ABANDON", the command will remove the resource from Terraform
+management without updating or deleting the resource in the API.
+When set to "DELETE", deleting the resource is allowed.
+`,
+				Default: "DELETE",
+			},
 		},
 		UseJSONNumber: true,
 	}
@@ -2419,6 +2433,11 @@ func resourceBackupDRRestoreWorkloadCreate(d *schema.ResourceData, meta interfac
 func resourceBackupDRRestoreWorkloadRead(d *schema.ResourceData, meta interface{}) error {
 	// This resource could not be read from the API.
 	return nil
+}
+
+func resourceBackupDRRestoreWorkloadUpdate(d *schema.ResourceData, meta interface{}) error {
+	// Only the root field "deletion_policy", "labels", "terraform_labels", and virtual fields are mutable
+	return resourceBackupDRRestoreWorkloadRead(d, meta)
 }
 
 func resourceBackupDRRestoreWorkloadDelete(d *schema.ResourceData, meta interface{}) error {
