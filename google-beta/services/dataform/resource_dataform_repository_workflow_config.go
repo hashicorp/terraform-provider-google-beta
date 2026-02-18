b94b9a02717a50d2b79b6f05556d5b5bc51c77e8
The Magician <magic-modules@google.com>
2026-02-18 09:49:53 -0800

    deletion policy generation databasemigrationservice-documentaiwarehouse (#16383) (#11582)
    [upstream:618ffb2414d2c6774d3bb0ccb781c2b99ab1f89d]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/dataform/resource_dataform_repository_workflow_config.go
+++ google-beta/services/dataform/resource_dataform_repository_workflow_config.go
@@ -257,6 +257,18 @@ func ResourceDataformRepositoryWorkflowConfig() *schema.Resource {
 				Computed: true,
 				ForceNew: true,
 			},
+			"deletion_policy": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Whether Terraform will be prevented from destroying the instance. Defaults to "DELETE".
+When a 'terraform destroy' or 'terraform apply' would delete the instance,
+the command will fail if this field is set to "PREVENT" in Terraform state.
+When set to "ABANDON", the command will remove the resource from Terraform
+management without updating or deleting the resource in the API.
+When set to "DELETE", deleting the resource is allowed.
+`,
+				Default: "DELETE",
+			},
 		},
 		UseJSONNumber: true,
 	}
@@ -385,6 +397,12 @@ func resourceDataformRepositoryWorkflowConfigRead(d *schema.ResourceData, meta i
 		return transport_tpg.HandleNotFoundError(err, d, fmt.Sprintf("DataformRepositoryWorkflowConfig %q", d.Id()))
 	}
 
+	// Explicitly set virtual fields to default values if unset
+	if _, ok := d.GetOkExists("deletion_policy"); !ok {
+		if err := d.Set("deletion_policy", "DELETE"); err != nil {
+			return fmt.Errorf("Error setting deletion_policy: %s", err)
+		}
+	}
 	if err := d.Set("project", project); err != nil {
 		return fmt.Errorf("Error reading RepositoryWorkflowConfig: %s", err)
 	}
@@ -513,6 +531,13 @@ func resourceDataformRepositoryWorkflowConfigDelete(d *schema.ResourceData, meta
 	}
 
 	headers := make(http.Header)
+	if d.Get("deletion_policy").(string) == "PREVENT" {
+		return fmt.Errorf("cannot destroy DataformRepositoryWorkflowConfig without setting deletion_policy=\"DELETE\" and running `terraform apply`")
+	}
+	if d.Get("deletion_policy").(string) == "ABANDON" {
+		log.Printf("[DEBUG] deletion_policy set to \"ABANDON\", removing RepositoryWorkflowConfig %q from Terraform state without deletion", d.Id())
+		return nil
+	}
 
 	log.Printf("[DEBUG] Deleting RepositoryWorkflowConfig %q", d.Id())
 	res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
