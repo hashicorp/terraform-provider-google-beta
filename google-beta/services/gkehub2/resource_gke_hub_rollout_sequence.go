ceb973396d482da8d9d6d8faadff053774decc39
Modular Magician <magic-modules@google.com>
2026-02-26 20:10:06 +0000

    deletion policy generation gemini - healthcare (#16511)
    [upstream:b1b0797f6f1eef5947eb21b38fb354ff7f533cc8]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/gkehub2/resource_gke_hub_rollout_sequence.go
+++ google-beta/services/gkehub2/resource_gke_hub_rollout_sequence.go
@@ -229,6 +229,18 @@ Please refer to the field 'effective_labels' for all of the labels present on th
 				Computed: true,
 				ForceNew: true,
 			},
+			"deletion_policy": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Whether Terraform will be prevented from destroying the instance. Defaults to "DELETE".
+When a 'terraform destroy' or 'terraform apply' would delete the instance,
+the command will fail if this field is set to "PREVENT" in Terraform state.
+When set to "ABANDON", the command will remove the resource from Terraform
+management without updating or deleting the resource in the API.
+When set to "DELETE", deleting the resource is allowed.
+`,
+				Default: "DELETE",
+			},
 		},
 		UseJSONNumber: true,
 	}
@@ -355,6 +367,12 @@ func resourceGKEHub2RolloutSequenceRead(d *schema.ResourceData, meta interface{}
 		return transport_tpg.HandleNotFoundError(err, d, fmt.Sprintf("GKEHub2RolloutSequence %q", d.Id()))
 	}
 
+	// Explicitly set virtual fields to default values if unset
+	if _, ok := d.GetOkExists("deletion_policy"); !ok {
+		if err := d.Set("deletion_policy", "DELETE"); err != nil {
+			return fmt.Errorf("Error setting deletion_policy: %s", err)
+		}
+	}
 	if err := d.Set("project", project); err != nil {
 		return fmt.Errorf("Error reading RolloutSequence: %s", err)
 	}
@@ -397,6 +415,18 @@ func resourceGKEHub2RolloutSequenceRead(d *schema.ResourceData, meta interface{}
 }
 
 func resourceGKEHub2RolloutSequenceUpdate(d *schema.ResourceData, meta interface{}) error {
+	clientSideFields := map[string]bool{"deletion_policy": true}
+	clientSideOnly := true
+	for field := range ResourceGKEHub2RolloutSequence().Schema {
+		if d.HasChange(field) && !clientSideFields[field] {
+			clientSideOnly = false
+			break
+		}
+	}
+	if clientSideOnly {
+		log.Print("[DEBUG] Only client-side changes detected. Cancelling update operation.")
+		return resourceGKEHub2RolloutSequenceRead(d, meta)
+	}
 
 	config := meta.(*transport_tpg.Config)
 	userAgent, err := tpgresource.GenerateUserAgentString(d, config.UserAgent)
@@ -523,6 +553,13 @@ func resourceGKEHub2RolloutSequenceDelete(d *schema.ResourceData, meta interface
 	}
 
 	headers := make(http.Header)
+	if d.Get("deletion_policy").(string) == "PREVENT" {
+		return fmt.Errorf("cannot destroy GKEHub2RolloutSequence without setting deletion_policy=\"DELETE\" and running `terraform apply`")
+	}
+	if d.Get("deletion_policy").(string) == "ABANDON" {
+		log.Printf("[DEBUG] deletion_policy set to \"ABANDON\", removing RolloutSequence %q from Terraform state without deletion", d.Id())
+		return nil
+	}
 
 	log.Printf("[DEBUG] Deleting RolloutSequence %q", d.Id())
 	res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
