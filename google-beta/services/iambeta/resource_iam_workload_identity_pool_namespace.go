468c0af5475d889f422c2cd35f08fa930e60e95c
Modular Magician <magic-modules@google.com>
2026-02-26 20:03:46 +0000

    deletion policy generation iam2-kms (#16529)
    [upstream:e58cd37e43cc078724472e5131e8d3011c9fba99]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/iambeta/resource_iam_workload_identity_pool_namespace.go
+++ google-beta/services/iambeta/resource_iam_workload_identity_pool_namespace.go
@@ -235,6 +235,18 @@ until it is permanently deleted.`,
 				Computed: true,
 				ForceNew: true,
 			},
+			"deletion_policy": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: `Whether Terraform will be prevented from destroying the instance. Defaults to "DELETE".
+When a 'terraform destroy' or 'terraform apply' would delete the instance,
+the command will fail if this field is set to "PREVENT" in Terraform state.
+When set to "ABANDON", the command will remove the resource from Terraform
+management without updating or deleting the resource in the API.
+When set to "DELETE", deleting the resource is allowed.
+`,
+				Default: "DELETE",
+			},
 		},
 		UseJSONNumber: true,
 	}
@@ -367,6 +379,12 @@ func resourceIAMBetaWorkloadIdentityPoolNamespaceRead(d *schema.ResourceData, me
 		return nil
 	}
 
+	// Explicitly set virtual fields to default values if unset
+	if _, ok := d.GetOkExists("deletion_policy"); !ok {
+		if err := d.Set("deletion_policy", "DELETE"); err != nil {
+			return fmt.Errorf("Error setting deletion_policy: %s", err)
+		}
+	}
 	if err := d.Set("project", project); err != nil {
 		return fmt.Errorf("Error reading WorkloadIdentityPoolNamespace: %s", err)
 	}
@@ -391,6 +409,18 @@ func resourceIAMBetaWorkloadIdentityPoolNamespaceRead(d *schema.ResourceData, me
 }
 
 func resourceIAMBetaWorkloadIdentityPoolNamespaceUpdate(d *schema.ResourceData, meta interface{}) error {
+	clientSideFields := map[string]bool{"deletion_policy": true}
+	clientSideOnly := true
+	for field := range ResourceIAMBetaWorkloadIdentityPoolNamespace().Schema {
+		if d.HasChange(field) && !clientSideFields[field] {
+			clientSideOnly = false
+			break
+		}
+	}
+	if clientSideOnly {
+		log.Print("[DEBUG] Only client-side changes detected. Cancelling update operation.")
+		return resourceIAMBetaWorkloadIdentityPoolNamespaceRead(d, meta)
+	}
 
 	config := meta.(*transport_tpg.Config)
 	userAgent, err := tpgresource.GenerateUserAgentString(d, config.UserAgent)
@@ -507,6 +537,13 @@ func resourceIAMBetaWorkloadIdentityPoolNamespaceDelete(d *schema.ResourceData, 
 	}
 
 	headers := make(http.Header)
+	if d.Get("deletion_policy").(string) == "PREVENT" {
+		return fmt.Errorf("cannot destroy IAMBetaWorkloadIdentityPoolNamespace without setting deletion_policy=\"DELETE\" and running `terraform apply`")
+	}
+	if d.Get("deletion_policy").(string) == "ABANDON" {
+		log.Printf("[DEBUG] deletion_policy set to \"ABANDON\", removing WorkloadIdentityPoolNamespace %q from Terraform state without deletion", d.Id())
+		return nil
+	}
 
 	log.Printf("[DEBUG] Deleting WorkloadIdentityPoolNamespace %q", d.Id())
 	res, err := transport_tpg.SendRequest(transport_tpg.SendRequestOptions{
