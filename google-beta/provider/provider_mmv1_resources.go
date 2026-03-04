38cd239c5185b5ece83efa16919745e8f4445dc8
Modular Magician <magic-modules@google.com>
2026-03-04 18:16:17 +0000

    Add Config resource to Dataform (#16500)
    [upstream:3140a1fa0dc88cb1d3085edbe49562cd8abfd75b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/provider/provider_mmv1_resources.go
+++ google-beta/provider/provider_mmv1_resources.go
@@ -649,9 +649,9 @@ var handwrittenIAMDatasources = map[string]*schema.Resource{
 }
 
 // Resources
-// Generated resources: 790
+// Generated resources: 791
 // Generated IAM resources: 357
-// Total generated resources: 1147
+// Total generated resources: 1148
 var generatedResources = map[string]*schema.Resource{
 	"google_folder_access_approval_settings":                                     registry.Resource("google_folder_access_approval_settings"),
 	"google_organization_access_approval_settings":                               registry.Resource("google_organization_access_approval_settings"),
@@ -1097,6 +1097,7 @@ var generatedResources = map[string]*schema.Resource{
 	"google_data_catalog_taxonomy_iam_binding":                                   registry.Resource("google_data_catalog_taxonomy_iam_binding"),
 	"google_data_catalog_taxonomy_iam_member":                                    registry.Resource("google_data_catalog_taxonomy_iam_member"),
 	"google_data_catalog_taxonomy_iam_policy":                                    registry.Resource("google_data_catalog_taxonomy_iam_policy"),
+	"google_dataform_config":                                                     registry.Resource("google_dataform_config"),
 	"google_dataform_folder":                                                     registry.Resource("google_dataform_folder"),
 	"google_dataform_repository":                                                 registry.Resource("google_dataform_repository"),
 	"google_dataform_repository_iam_binding":                                     registry.Resource("google_dataform_repository_iam_binding"),
