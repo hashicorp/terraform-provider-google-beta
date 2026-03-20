8935ddb1929011b4d2b833c28f1841df49ddd75a
Modular Magician <magic-modules@google.com>
2026-03-20 00:15:37 +0000

    Add field loadBalancingScheme to resources compute_target_tcp_proxy (beta) (#16681)
    [upstream:acf002fa27243a825d4bed105ee2295a0c6c7db5]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_target_tcp_proxy_generated_test.go
+++ google-beta/services/compute/resource_compute_target_tcp_proxy_generated_test.go
@@ -102,6 +102,98 @@ resource "google_compute_health_check" "default" {
 `, context)
 }
 
+func TestAccComputeTargetTcpProxy_targetTcpProxyBasicBetaExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckComputeTargetTcpProxyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeTargetTcpProxy_targetTcpProxyBasicBetaExample(context),
+			},
+			{
+				ResourceName:            "google_compute_target_tcp_proxy.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"backend_service"},
+			},
+		},
+	})
+}
+
+func testAccComputeTargetTcpProxy_targetTcpProxyBasicBetaExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_target_tcp_proxy" "default" {
+  provider              = google-beta
+  name                  = "tf-test-test-proxy%{random_suffix}"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  backend_service       = google_compute_backend_service.default.id
+}
+
+resource "google_compute_backend_service" "default" {
+  provider              = google-beta
+  name                  = "tf-test-backend-service%{random_suffix}"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  protocol              = "TCP"
+  timeout_sec           = 10
+
+  health_checks = [google_compute_health_check.default.id]
+}
+
+resource "google_compute_health_check" "default" {
+  provider           = google-beta
+  name               = "tf-test-health-check%{random_suffix}"
+  timeout_sec        = 1
+  check_interval_sec = 1
+
+  tcp_health_check {
+    port = "443"
+  }
+}
+`, context)
+}
+
+func TestAccComputeTargetTcpProxy_targetTcpProxyBackendlessExample(t *testing.T) {
+	t.Parallel()
+
+	context := map[string]interface{}{
+		"random_suffix": acctest.RandString(t, 10),
+	}
+
+	acctest.VcrTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		CheckDestroy:             testAccCheckComputeTargetTcpProxyDestroyProducer(t),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccComputeTargetTcpProxy_targetTcpProxyBackendlessExample(context),
+			},
+			{
+				ResourceName:            "google_compute_target_tcp_proxy.default",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"backend_service"},
+			},
+		},
+	})
+}
+
+func testAccComputeTargetTcpProxy_targetTcpProxyBackendlessExample(context map[string]interface{}) string {
+	return acctest.Nprintf(`
+resource "google_compute_target_tcp_proxy" "default" {
+  provider              = google-beta
+  name                  = "tf-test-test-proxy%{random_suffix}"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+}
+`, context)
+}
+
 func testAccCheckComputeTargetTcpProxyDestroyProducer(t *testing.T) func(s *terraform.State) error {
 	return func(s *terraform.State) error {
 		for name, rs := range s.RootModule().Resources {
