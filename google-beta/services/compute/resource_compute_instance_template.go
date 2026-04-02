a79da8026bfdf2f99d7fd78a421ecdc0b36b89d4
Modular Magician <magic-modules@google.com>
2026-04-02 17:11:56 +0000

    Universal Deletion Handwritten Part 1 (#16814)
    [upstream:569178ba0cda8ee43056a334205e887de97d3fd0]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_instance_template.go
+++ google-beta/services/compute/resource_compute_instance_template.go
@@ -79,6 +79,7 @@ func ResourceComputeInstanceTemplate() *schema.Resource {
 		},
 		SchemaVersion: 1,
 		CustomizeDiff: customdiff.All(
+			tpgresource.DefaultProviderDeletionPolicy("DELETE"),
 			tpgresource.DefaultProviderProject,
 			resourceComputeInstanceTemplateSourceImageCustomizeDiff,
 			resourceComputeInstanceTemplateScratchDiskCustomizeDiff,
