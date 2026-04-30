2770cc6e096b65e5cb6178ae313f0d58d7e4e041
Modular Magician <magic-modules@google.com>
2026-04-30 23:17:40 +0000

    mark disk encryption key fields as sensitive in compute resources (#17366)
    [upstream:e7fe1ef420018c1b8ab52cab4eba02c77829b10b]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_disk.go
+++ google-beta/services/compute/resource_compute_disk.go
@@ -827,6 +827,7 @@ If absent, the Compute Engine Service Agent service account is used.`,
 							ForceNew: true,
 							Description: `Specifies a 256-bit customer-supplied encryption key, encoded in
 RFC 4648 base64 to either encrypt or decrypt this resource.`,
+							Sensitive: true,
 						},
 						"sha256": {
 							Type:     schema.TypeString,
@@ -883,6 +884,7 @@ If absent, the Compute Engine Service Agent service account is used.`,
 							ForceNew: true,
 							Description: `Specifies a 256-bit customer-supplied encryption key, encoded in
 RFC 4648 base64 to either encrypt or decrypt this resource.`,
+							Sensitive: true,
 						},
 						"sha256": {
 							Type:     schema.TypeString,
