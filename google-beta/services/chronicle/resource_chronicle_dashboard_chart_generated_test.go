28d2788bb2818c50338f7d491f1f18dad8833866
Modular Magician <magic-modules@google.com>
2026-05-07 21:16:48 +0000

    Add Terraform support for Dashboard Chart for v1 endpoint (#17404)
    [upstream:8890d5b9d48abbbf3592da6afe402f4e73cbd187]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/chronicle/resource_chronicle_dashboard_chart_generated_test.go
+++ google-beta/services/chronicle/resource_chronicle_dashboard_chart_generated_test.go
@@ -65,7 +65,7 @@ func TestAccChronicleDashboardChart_chronicleDashboardchartBasicExample(t *testi
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		CheckDestroy:             testAccCheckChronicleDashboardChartDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
@@ -91,7 +91,6 @@ func testAccChronicleDashboardChart_chronicleDashboardchartBasicExample(context 
 	return acctest.Nprintf(`
 # A Native Dashboard is required to create a Dashboard Chart.
 resource "google_chronicle_native_dashboard" "my_dashboard" {
-  provider     = google-beta
   location     = "us" 
   instance     = "%{chronicle_id}"
   display_name = "tf-test-dashboard-1%{random_suffix}"
@@ -114,7 +113,6 @@ resource "google_chronicle_native_dashboard" "my_dashboard" {
 }
 
 resource "google_chronicle_dashboard_chart" "my_chart" {
-  provider         = google-beta
   location         = "us" # Example region, adjust as necessary
   instance         = "%{chronicle_id}"
   # This parameter creates an implicit dependency: Terraform will create
@@ -191,7 +189,7 @@ func TestAccChronicleDashboardChart_chronicleDashboardchartFullExample(t *testin
 
 	acctest.VcrTest(t, resource.TestCase{
 		PreCheck:                 func() { acctest.AccTestPreCheck(t) },
-		ProtoV5ProviderFactories: acctest.ProtoV5ProviderBetaFactories(t),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories(t),
 		CheckDestroy:             testAccCheckChronicleDashboardChartDestroyProducer(t),
 		Steps: []resource.TestStep{
 			{
@@ -217,7 +215,6 @@ func testAccChronicleDashboardChart_chronicleDashboardchartFullExample(context m
 	return acctest.Nprintf(`
 # A Native Dashboard is required to create a Dashboard Chart.
 resource "google_chronicle_native_dashboard" "my_dashboard" {
-  provider     = google-beta
   location     = "us" # Example region, adjust as necessary
   instance     = "%{chronicle_id}"
   display_name = "%{dashboard_name}"
@@ -244,7 +241,6 @@ resource "google_chronicle_native_dashboard" "my_dashboard" {
 }
 
 resource "google_chronicle_dashboard_chart" "my_chart" {
-  provider         = google-beta
   location         = google_chronicle_native_dashboard.my_dashboard.location
   instance         = google_chronicle_native_dashboard.my_dashboard.instance
   native_dashboard = google_chronicle_native_dashboard.my_dashboard.name
@@ -345,7 +341,6 @@ resource "google_chronicle_dashboard_chart" "my_chart" {
 }
 
 resource "google_chronicle_dashboard_chart" "button_tile" {
-  provider         = google-beta
   location         = google_chronicle_native_dashboard.my_dashboard.location
   instance         = google_chronicle_native_dashboard.my_dashboard.instance
   native_dashboard = google_chronicle_native_dashboard.my_dashboard.name
@@ -377,7 +372,6 @@ resource "google_chronicle_dashboard_chart" "button_tile" {
 }
 
 resource "google_chronicle_dashboard_chart" "markdown_tile" {
-  provider         = google-beta
   location         = google_chronicle_native_dashboard.my_dashboard.location
   instance         = google_chronicle_native_dashboard.my_dashboard.instance
   native_dashboard = google_chronicle_native_dashboard.my_dashboard.name
