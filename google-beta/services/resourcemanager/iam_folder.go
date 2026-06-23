7bec6ae31431b80d1087e9cffd6aad68d53edba5
Modular Magician <magic-modules@google.com>
2026-06-23 20:33:11 +0000

    Folder iam member list resource (#18049)
    [upstream:e4e9f0d8c3f0b4ed40dcd5d0a65d57dac96bea18]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/resourcemanager/iam_folder.go
+++ google-beta/services/resourcemanager/iam_folder.go
@@ -21,8 +21,10 @@ import (
 	"strings"
 
 	"github.com/hashicorp/errwrap"
+	"github.com/hashicorp/terraform-plugin-framework/list"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/registry"
+
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/services/resourcemanagerv3"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgiamresource"
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
@@ -167,6 +169,30 @@ func GetFolderIamPolicyByFolderName(folderName, userAgent string, config *transp
 	return v1Policy, nil
 }
 
+func FolderIamMemberResource() *schema.Resource {
+	return tpgiamresource.ResourceIamMember(
+		IamFolderSchema,
+		NewFolderIamUpdater,
+		FolderIdParseFunc,
+		tpgiamresource.IamWithBatching,
+		tpgiamresource.IamWithParentResourceIdentity(FolderIamParentResourceIdentityParser),
+	)
+}
+
+// NewFolderIamMemberListResource returns the list implementation for google_folder_iam_member
+func NewFolderIamMemberListResource() list.ListResource {
+	return tpgiamresource.NewIamMemberListResource(
+		"google_folder_iam_member",
+		FolderIamMemberResource(),
+		NewFolderIamUpdater,
+		tpgiamresource.IamMemberListCallConfig{
+			ParentResourceField: "folder",
+			EnableRoleFilter:    true,
+			EnableMemberFilter:  true,
+		},
+	)
+}
+
 func init() {
 	registry.Schema{
 		Name:        "google_folder_iam_audit_config",
@@ -184,7 +210,12 @@ func init() {
 		Name:        "google_folder_iam_member",
 		ProductName: "resourcemanager",
 		Type:        registry.SchemaTypeIAMResource,
-		Schema:      tpgiamresource.ResourceIamMember(IamFolderSchema, NewFolderIamUpdater, FolderIdParseFunc, tpgiamresource.IamWithBatching, tpgiamresource.IamWithParentResourceIdentity(FolderIamParentResourceIdentityParser)),
+		Schema:      FolderIamMemberResource(),
+	}.Register()
+	registry.FrameworkListResource{
+		Name:        "google_folder_iam_member",
+		ProductName: "resourcemanager",
+		Func:        NewFolderIamMemberListResource,
 	}.Register()
 	registry.Schema{
 		Name:        "google_folder_iam_policy",
