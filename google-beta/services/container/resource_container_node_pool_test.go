68eba0c4998f3565a84a2a4d697ae7dee21b135f
Modular Magician <magic-modules@google.com>
2026-07-27 16:56:51 +0000

    test: fix flaky ANY_RESERVATION_THEN_FAIL node pool test (#18406)
    [upstream:199dca4418bb639ef1f36a3b235a88ba60298762]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/container/resource_container_node_pool_test.go
+++ google-beta/services/container/resource_container_node_pool_test.go
@@ -4058,13 +4058,15 @@ resource "google_compute_reservation" "gce_reservation" {
   zone = "us-central1-a"
 
   specific_reservation {
-    count = 1
+    count = 3
     instance_properties {
-      machine_type     = "n1-standard-1"
+      machine_type     = "n1-standard-2"
     }
   }
 
   specific_reservation_required = false
+
+  depends_on = [google_container_cluster.cluster] 
 }
 
 resource "google_container_node_pool" "with_reservation_affinity" {
@@ -4073,7 +4075,7 @@ resource "google_container_node_pool" "with_reservation_affinity" {
   cluster            = google_container_cluster.cluster.name
   initial_node_count = 1
   node_config {
-    machine_type    = "n1-standard-1"
+    machine_type    = "n1-standard-2"
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
@@ -4082,6 +4084,7 @@ resource "google_container_node_pool" "with_reservation_affinity" {
       consume_reservation_type = "ANY_RESERVATION_THEN_FAIL"
     }
   }
+  depends_on = [google_compute_reservation.gce_reservation]
 }
 `, cluster, networkName, subnetworkName, reservation, np)
 }
