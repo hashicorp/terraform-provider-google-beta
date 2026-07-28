ef2b08d7d1485d102bf68e4b0b53c946cfa9d761
The Magician <magic-modules@google.com>
2026-07-28 14:01:42 -0700

    compute: add default_value to consumer_accept_lists fields to prevent… (#18276) (#12792)
    [upstream:988e40b1934f0bade7498ba5b858225e014f7731]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/compute/resource_compute_service_attachment.go
+++ google-beta/services/compute/resource_compute_service_attachment.go
@@ -437,6 +437,7 @@ create.`,
 				DiffSuppressFunc: tpgresource.CompareSelfLinkRelativePaths,
 				Description: `The endpoint that is allowed to connect to this service attachment.
 Only one of project_id_or_num, network_url and endpoint_url may be set.`,
+				Default: "",
 			},
 			"network_url": {
 				Type:             schema.TypeString,
@@ -444,12 +445,14 @@ Only one of project_id_or_num, network_url and endpoint_url may be set.`,
 				DiffSuppressFunc: tpgresource.CompareSelfLinkRelativePaths,
 				Description: `The network that is allowed to connect to this service attachment.
 Only one of project_id_or_num and network_url may be set.`,
+				Default: "",
 			},
 			"project_id_or_num": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Description: `A project that is allowed to connect to this service attachment.
 Only one of project_id_or_num and network_url may be set.`,
+				Default: "",
 			},
 		},
 	}
