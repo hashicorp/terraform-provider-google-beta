90780595cea41891ad577103178aabb44ceb3b2a
Modular Magician <magic-modules@google.com>
2026-08-20 18:22:08 +0000

    Feature branch major release 8.0.0 main sync August 20 (#18730)
    [upstream:5a970ee2063f8fc87d61e74fd6d35295c6ff0033]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/fwprovider/framework_provider.go
+++ google-beta/fwprovider/framework_provider.go
@@ -1313,6 +1313,12 @@ func (p *FrameworkProvider) Schema(_ context.Context, _ provider.SchemaRequest, 
 					transport_tpg.CustomEndpointValidator(),
 				},
 			},
+			"vertex_ai_aad_custom_endpoint": &schema.StringAttribute{
+				Optional: true,
+				Validators: []validator.String{
+					transport_tpg.CustomEndpointValidator(),
+				},
+			},
 			"vmwareengine_custom_endpoint": &schema.StringAttribute{
 				Optional: true,
 				Validators: []validator.String{
