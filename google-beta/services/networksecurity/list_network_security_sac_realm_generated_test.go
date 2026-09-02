2a63e45dd7c8687866875434897f49d525588bef
The Magician <magic-modules@google.com>
2026-09-01 19:52:08 -0700

    identityplatform: add list resources (#18813) (#13050)
    [upstream:f5f8b480cdcee4ae232d81d8c781e65a8c048fda]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/networksecurity/list_network_security_sac_realm_generated_test.go
+++ google-beta/services/networksecurity/list_network_security_sac_realm_generated_test.go
@@ -22,6 +22,7 @@ package networksecurity_test
 import (
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/querycheck"
@@ -36,6 +37,7 @@ import (
 var (
 	_ = strings.Trim
 	_ = envvar.TestEnvVar
+	_ = time.Now
 )
 
 func TestAccNetworkSecuritySacRealmListQuery_generated(t *testing.T) {
