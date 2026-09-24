89ebd2d3dab8f9227f083b314e2a256814d436f6
The Magician <magic-modules@google.com>
2026-09-24 15:45:48 -0700

    Made it so the test reader can tell the difference between serial tests and table-driven tests (#19150) (#13236)
    [upstream:aa0406235329d24b5937a4b059c8d850abbd25c3]
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- google-beta/services/datalossprevention/resource_data_loss_prevention_stored_info_type_internal_test.go
+++ google-beta/services/datalossprevention/resource_data_loss_prevention_stored_info_type_internal_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/hashicorp/terraform-provider-google-beta/google-beta/tpgresource"
 )
 
-func TestAccDataLossPreventionStoredInfoType_dlpStoredInfoTypeCustomDiffFuncForceNew(t *testing.T) {
+func TestDataLossPreventionStoredInfoType_dlpStoredInfoTypeCustomDiffFuncForceNew(t *testing.T) {
 	t.Parallel()
 
 	cases := map[string]struct {
